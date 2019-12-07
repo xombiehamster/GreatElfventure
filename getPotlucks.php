bd349bdd345f3d9771e5bd90334235f698f4c3b4
xombiehamster <58495678+xombiehamster@users.noreply.github.com>
2019-12-06 21:39:36 -0600

    Update getPotlucks.php

--- getPotlucks.php
+++ getPotlucks.php
@@ -1,19 +1,19 @@
 <?php
 function getPotlucks($year){
     $str = "<tr>";
-    $ind = 0;
-    $arr = array();
+    $ind = 1;
+    $arr = array("");
     while ($ind < $year){
         $arr[$ind] = 'Events/'.getStringYear($ind).'_0.php';
         $ind++;
     }
     #BLADE
     $str .= '<td>';
-    if(!is_null($arr[0])){
-        $str .= '<ul><li><a href="'.$arr[0].'">Year 1: Goodbye Potluck</a></li>';
-    }
     if(!is_null($arr[1])){
-        $str .= '<li><a href="'.$arr[1].'">Year 2: A New Year</a></li></ul></td>';
+        $str .= '<ul><li><a href="'.$arr[1].'">Year 1: Goodbye Potluck</a></li>';
+    }
+    if(!is_null($arr[2])){
+        $str .= '<li><a href="'.$arr[2].'">Year 2: A New Year</a></li></ul></td>';
     }else{
         $str .= '</ul></td>';
     }
@@ -27,14 +27,14 @@ function getPotlucks($year){
     $str .= '<td></td>';
     #FIRE
     $str .= '<td>';
-    if(!is_null($arr[2])){
-        $str .= '<ul><li><a href="'.$arr[2].'">Year 3: Summer Potluck</a></li></ul>';
+    if(!is_null($arr[3])){
+        $str .= '<ul><li><a href="'.$arr[3].'">Year 3: Summer Potluck</a></li></ul>';
     }
     $str .= '</td>';
     #RIVER
     $str .= '<td>';
-    if(!is_null($arr[3])){
-        $str .= '<ul><li><a href="'.$arr[3].'">Year 4: A Peaceful Potluck</a></li></ul>';
+    if(!is_null($arr[4])){
+        $str .= '<ul><li><a href="'.$arr[4].'">Year 4: A Peaceful Potluck</a></li></ul>';
     }
     $str .= '</td>';
     #WINGED
