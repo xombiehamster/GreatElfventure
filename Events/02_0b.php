d4a3872fa6daeed7228c1952d1b90d8dc714a9df
xombiehamster <58495678+xombiehamster@users.noreply.github.com>
2019-12-08 21:06:23 -0600

    Update 02_0b.php

--- Events/02_0b.php
+++ Events/02_0b.php
@@ -9,7 +9,183 @@
 </head>
     <h1>A War of Words</h1>
 
-<!-- insert a lot of text here -->
+    <p>Night has fallen on Fort Alfyr, but the training grounds are abuzz with activity. The training grounds are a large open area  between the two barracks buildings lined with a sandy floor. In the middle of training grounds, Terje appears to have set up an improvised ring. The circle is lined with torches, illuminating the field in a flickering glow. On one end of the ring, Terje appears to have set up five chairs on a raised platform. The chairs are from the mess hall and the platform is made of milk crates, but it works. </p>
+    <p>Around the ring, a couple dozen off-duty blade elves have gathered. Terje moves through the group holding several slips of paper over his head. He calls out among the group like an auctioneer, apparently taking bets. As the group from the party begins to arrive, Terje shushes the crowd. Katiin politely stands to one side and attends as the various groups move to their positions. </p>
+    <p>At one end of the ring, Embebi sits, cross legged.  Her eyes are closed, but her face is confident.  For the past several minutes, she has just been sitting and inhaling softly.  As the crowd begins to settle, she can sense that it is nearly time to begin. </p>
+    <p>The Hunger stood at the other end, warming his hands inside one of the torch-flames. </p>
+    <p>No more movement.  Yes.  Now was the time.</p>
+    <p>In one smooth motion, Embebi rises to her feet.  She tilts her head from side to side, receiving a satisfying <i>crunch</i>.  She paces towards the center of the ring, crosses her arms, and looks out over the assembled judges.</p>
+    <p>“Since you are all Lowlanders,” she shouts, deep voice cutting through the night air, “I’ll explain this like it is your first time hearing it.  The Rite of Refusal has been invoked.  The bids have been placed.  The Numbered have been chosen.  The only way either of us will now leave this ring, this Circle of Equals,” smirk , “is when one of us collapses, or until the Numbered feel they have seen and heard enough to reach a simple majority!”</p>
+    <p>She now turns her gaze on The Hunger.</p>
+    <p>“And So! I, Stalker Embebi, one of the Aggro, make myself known to all will see! I stand in this Circle of Equals! I will honor my bid, and will not disgrace myself! In this solemn matter, let none interfere!” </p>
+    <p>She waits, poised, anticipating his response.</p>
+    <p>The Hunger spun back around. His face, hands, and arms glowed red-hot, and smoke twirled from his fingers. He gestured towards himself, and then to the audience. "And so I, the Hunger, Magister of the Triumvirate, make myself known to you all, and as I stand here in this Circle of Equals, I pledge to honor my bid and not disgrace myself. In this solemn matter, let no one interfere." </p>
+    <p>Embebi inclines her head slightly.  “As you saw fit to grant a handicap to match my handicap, it is only reasonable that I will make first move.  A gesture of <i>appreciation</i>.”</p>
+    <p>With that, she folds into a crouch.  Perched on tiptoes, she rests her elbows on her knees, bending slightly forward.  A look of concentration settles on her face.</p>
+    <p>A low drone, originating deep in her throat, hums forth. </p>
+    <p>Before long, it is then joined by a second tone- a higher, whistling noise- which overlays the first. </p>
+    <p>The polyphonic tones clash against eachother in the open air- two noises, one throat. They dip and rise together, then separately, and then stop.</p>
+    <p>Embebi draws another deep breath.  When she starts again, it is in the low drone, which echoes and buzzes from deep within.</p>
+    <p>“<i>From distant lands, I have travelled Far/<br/>
+        Guided by land, by water, by Star/<br/>
+        To arrive here, in this place so Low/<br/>
+        For it was my wish, my call to Go</i>”<br/>
+        <br/>
+        “<i>In this place, so Low, so Strange/<br/>
+        Before me, now, you all Arrange/<br/>
+        For of you all, but one did Dare/<br/>
+        To Believe him ready for struggle Fair</i>”<br/>
+        <br/>
+        “<i>To this elf, who bears his Mask/<br/>
+        I shall but one pointed question Ask/<br/>
+        For if he Earned to be Hunger Named/<br/>
+        Then why of himself… would he be Ashamed?</i>”</p>
+    <p>The droning ends, and she takes a few quick breaths before stilling herself.  She opens her eyes to gaze at her opponent.</p>
+    <p>There is a collective “Oooh!” from the assembled blade elves. Terje deals with a flurry of bet changes. Terje is grinning like a nutcase.  Smilisca elbows Terje repeatedly with an excited grin.</p>
+    <p>Anasatri’s wings dip in surprise as she trades a wide-eyed look with Pebble. </p>
+    <p>The Hunger clearly does not have the throat to match Embebi's impressive singing... so he doesn't try to. His voice is sharp, clear, and nuanced; a lord's voice, rather than a singer's.</p>
+    <p>"<i>I bear this mask to remember the dead/<br/>
+        It's my burden, my curse for the life I have lead/<br/>
+        Forged from my oppressor's sweetest of treasure/<br/>
+        I tally sweeter revenge within every ledger/<br/>
+        <br/>
+        Ashamed? Ha! I'll save that for the Crag/<br/>
+        Those hiding in the mountains really shouldn't brag/<br/>
+        We've reforged a nation while you hid far above/<br/>
+        They have no room to stand when they send the likes of/<br/>
+        <br/>
+        YOU, gooseplop! You rock-covered jackass/<br/>
+        Was that singing I heard, or were you passing gas?/<br/>
+        I'll try to educate, 'cause you have a lot to learn/<br/>
+        Mess with a Fire Elf and you're likely to burn./</i>"</p>
+    <p>The Hunger's glow increased steadily throughout his monologue, cresting right at the end of "burn", before returning to his normal mild red. As soon as he finished, he gestured back to Embebi.</p>
+    <p>There is another surprised murmur from the blade elf crowd, and more slips passing to Terje. The crowd almost seems dividing, with groups moving toward either end. Roughly equal, for now. More elves are streaming in from the barracks, some still in night clothes.</p>
+    <p>Embebi, for her part, remains crouched, parsing The Hunger’s rebuttal.  A look of confusion crosses her face.</p>
+    <p>She tilts her head in a “well, okay” gesture, then closes her eyes again.  Her low drone resumes.</p>
+    <p>“<i>A strange reply, to blame the Dead/<br/>
+        For that you now bear a curse in their Stead/<br/>
+        The Dead do not Curse, so it must be Said/<br/>
+        I believe I know from whence your Curse is Fed.</i>”<br/>
+        <br/>
+        “<i>You speak dearly of Treasure, and from what I Hear/<br/>
+        You feel it your Measure, your shield from your Fear/<br/>
+        That you know no Pleasure, None else will Endear/<br/>
+        Unless it’s your Leisure, Nothing else you hold Dear.</i>”<br/>
+        <br/>
+        “<i>Your crass insults mean nothing, they are but pumice Stone/<br/>
+        Coarse and Abrasive, but cuts no flesh, breaks no Bone/<br/>
+        They serve only to salve you from what you fear when Alone/<br/>
+        That in counting your treasures, you can never hope… to Atone.</i>”</p>
+    <p>She settles back.  Her breaths are even, deep.  Her eyes snap open.</p>
+    <p>She has trained for this.</p>
+    <p>There are a few outright whoops from blade elves at the second stanza especially. Embebi’s group is definitely growing, but it’s far from decisive yet. </p>
+    <p>The Hunger sighed contentedly, and the light rose back out of him.</p>
+    <p>"<i>You think I measure myself by the little I own?/<br/>
+        My atonement is built out of every Surt stone/<br/>
+        The Grand Surtian Gardens are my victory arch/<br/>
+        And a gravestone crushing every Lycan exarch/<br/>
+        <br/>
+        I care nothing for treasure save what it can rebuild/<br/>
+        When our elf nation rises, <b>then</b> my atonement's fulfilled/<br/>
+        I swore with Fury and Terror to end our captivity/<br/>
+        Wealth is for tyrants; my <b>salve</b> is liberty/<br/>
+        <br/>
+        Whereas your salve is to defend a fanatic/<br/>
+        With rhymes about as fresh as they are phlegmatic/<br/>
+        Put some heat in your song! I'm owning this ring!/<br/>
+        At home I'm a functionary but in here I'm the KING!</i>"</p>
+    <p>Again, the Hunger stepped back, letting himself cool down to his normal tones. He nodded to Embebi.</p>
+    <p>The last couple stanzas definitely even the blade elf crowd back out. The crowd is still growing, and becoming rowdy. It has nearly tripled in size, and they follow up every turn with jeers and cheering alike. Several officers can be seen dragging a handful of uniformed guardselves back to their posts from the rear of the crowd.</p>
+    <p>Terje has backed out of the main crowd and slides in next to Smilisca with a smile. Smilisca points toward Acris at the far side of the ring.  The young apprentice is absolutely enraptured.</p>
+    <p>Embebi spares a glance over to the Numbered, but looks back to The Hunger when it is clear they have reached no judgement yet.  </p>
+    <p>She visibly chews over his response, considering her own.</p>
+    <p>She closes her eyes, and begins once more.</p>
+    <p>“<i>An Aggro’s whole Life is Defense, this is True/<br/>
+        Protect all Crag from Strife, this is what Aggro Do/<br/>
+        By Word or by Knife, Our Duty’s seen Through/<br/>
+        But though your words seem Rife, I wonder what you do.<br/>
+        <br/>
+        You claim to Rebuild, but what walls have you Raised?/<br/>
+        You order others to do this, should I be Amazed?/<br/>
+        Curse Tyrants, and yet in same breath, kings are Praised/<br/>
+        Not freedom, but Rule, your ambitions have Gazed.<br/>
+        <br/>
+        To give me Command, you must first Earn Respect/<br/>
+        I’ve seen you Demand, and these, I Reject/<br/>
+        For though you have Planned, Action you Neglect/<br/>
+        Your Dreams may be Grand, but your Use?  Is… Suspect.</i>”</p>
+    <p>She settles back, a small tired frown on her face.  Her breathing hitches, for but a moment.</p>
+    <p>This strike riles up the crowd, but oddly doesn't seem to alter the balance of power. Instead both groups of elves are just a little more entrenched in who they support, though it's all still pretty friendly for the blade elves. </p>
+    <p>Anasatri leans over to Barrabus, “You’re the bard; what are we supposed to be judging here?” </p>
+    <p>Barrabus shrugged. "Different music for different cultures, I'm afraid. That said... I'd guess cadence, rhythm, and rhyme? And if either of them choke? Either way, I'm impressed; I would have expected the Hunger to tap out a long time ago. Kudos to Embibi, too; maintaining a stable tone over three separate groups of stanzas without support is not easy. </p>
+    <p>“...And to be honest, we should cut them off soon. Try to end this on a high note."</p>
+    <p>The Hunger stepped forward, pulling the hot glow back into his flesh.</p>
+    <p>"<i>All that belching you're doing must have blown out your mind/<br/>
+        Do you think strong walls are 'built' without being designed?/<br/>
+        I stand strong by my peers in building great Surt/<br/>
+        I may have an office, but I'm still carving through dirt/</i>"</p>
+    <p>The Hunger sucked in a breathe. A bit of sweat rolled down his ears.</p>
+    <p>"<i>But you want more? Fine. Let's talk of the war/<br/>
+        In my duty to our people I shattered more than a score/<br/>
+        Of Lycan supply lines, their infrastructure, their economic back-end/<br/>
+        Where were you, Embebi? What did you <b>defend</b>?/<br/>
+        <br/>
+        Your people have hidden for more than a year/<br/>
+        Was it shame that kept you? Or perhaps it was fear?/<br/>
+        You accuse me and my leadership of having no use/<br/>
+        But what have you been doing, Crag Elf? What's your excuse?</i>"</p>
+    <p>The light pouring from him faded a little more slowly this time. He turned and gestured to Embibi.</p>
+    <p>That connects with the crowd. Over the last two stanzas, the cheers and whoops from the blade elves easily drown out the jeers and boos. Despite the volume Hunger is loud enough to be heard, and a solid portion of the crowd is swayed. Embebi stil has quite a few on her side of the ring though, and they are far from cowed. </p>
+    <p>Embebi’s eyes narrow. She bares her teeth, grinding them.  Looks like that hit a sore spot.</p>
+    <p>She rises from her crouch.  She begins droning once more, but before long, it becomes a growl.</p>
+    <p>“<i>You! You! You! YOU! You braggartly child!/<br/>
+        The memory of fallen are thusly defiled!/<br/>
+        Spare dead and others one breath, yourself a Typhoon!/<br/>
+        If it’s Deeds that you want, in </i><b>Flesh</b><i> mine are Hewn!<br/>
+        <br/>
+        Aggro Defend Crag! Our Duty to the Core!/<br/>
+        Our people, whose peace was Sundered in War!/<br/>
+        You Fought For your </i><b>Freedom</b><i>?! Here’s something you Missed!/<br/>
+        You Fought For your Freedom, Crag fought </i><b>To Exist</b><i>!<br/>
+        <br/>
+        When to our Home the </i><span class="animal">Animal</span><i> forces were Led/<br/>
+        We did not appease them! They wanted us Dead!/<br/>
+        You Won because now to Empire you Strive!/<br/>
+        The Aggro won here, </i><b>because</b> we’re Alive!<br/>
+        <br/>
+        <i>I fought with my eyes! I fought with my Hands!/<br/>
+        I fought them till we were in Distant Lands!/<br/>
+        I fought with my Kin! I fought while they Died!/<br/>
+        We chose to Die There, Instead of Inside/<br/>
+        Because those were the Choices Fate handed our Way/<br/>
+        We Died in those Fields so Crag Live to this Day!</i>”</p>
+    <p>She pauses, gasping, her hands clenching and unclenching.</p>
+    <p>“<i>...Speak to me more, of your Wealth and your Walls.<br/>
+        But I need not </i><b>Speak</b><i> when Ancestors Call.</i>”</p>
+    <p>She stops here, eyes full of fury.  As her breath stills slowly, she returns to her crouch.</p>
+    <p>She closes her eyes.  She is disappointed.</p>
+    <p>Anasatri elbowed Barrabus and Pebble, whispering, “Someone’s going to get killed if this goes on much longer.”</p>
+    <p>Indeed, following Anasatri's words, it sounds like a fight breaks out in the crowd on Embebi's side of the ring. The first two stanzas earned some boos, but then as Embebi continued there was straight up yelling from the blade elves. The crowd is getting extremely rowdy, causing Katiin to have to step forward to shout orders and calm the group. She succeeds, but only just. From the edge of the ring she shoots Hunger a very pointed look.</p>
+    <p>“<i>While we were still drying in fields, the Crag went Away.</i>” Smilisca mutters under his breath, low enough for Terje’s ears only.</p>
+    <p>Terje gently places his hand in Smilisca's, but says nothing. </p>
+    <p>The light glaring out of the Hunger's mask suddenly rose to an eye-watering brightness, and smoke rose around him as the shoulders of his tunic started to smolder threateningly.</p>
+    <p>Barrabus nodded. "You're right," he whispered. "The old bastard's losing it. We--"</p>
+    <p>"ENOUGH," the Hunger roared [for reference, that's an Intimidate check totaling to 29, directed towards pacifying the crowd]. </p>
+    <p>He paused, glaring down at the crowd. "Enough," he whispered. "I cannot -- will not -- try to trump the Crag Elves' losses with words. Or any of ours; we have all suffered enough." He stopped for a second, and sucked in another labored breath. "Embibi... I forfeit this match to you. You are... quite good. I look forward to seeing more of your work in the future."</p>
+    <p>He turned back to Tsun. "Ashwalker. I will leave you..." He glanced through the other judges. "...In Leafstorm's recognizance. I'm sure you'll get along splendidly. Rest assured that if you're not on the boat by the time we leave, I have no qualms about leaving you here." </p>
+    <p>"Give my regards to Ehra, Katiin. I think I will take a rest on our boat for now." And with that, the Hunger turned, and stepped slowly out of the ring. </p>
+    <p>Embebi, for her part, just looks… stunned.</p>
+    <p>At the Hunger's roar, the crowd falls silent. For a few brief moments, the loudest noise other than the Hunger's words is the crackling of the torches.</p>
+    <p>The moment the Hunger's foot touches the outside of  the ring, the crowd erupts. Not with violence, but with cheering, applause, cursing, yelling, and laughing alike. Whatever violence was brewing has been defused and replaced with raucous enthusiasm.</p>
+    <p>Katiin shouts for a few moments, unable to be heard.</p>
+    <p>She takes a deep breath before bellowing, "WHAT DOES A BLADE DO!?"</p>
+    <p>The crowd nearly as one snaps to attention and answers in a chorus of "KILL! KILL! KILL!" before falling silent.</p>
+    <p>Anyone looking at Pebble notices her flinch, arms tightening around Namib.</p>
+    <p>"Show is over, soldiers!" Katiin orders, "As you were!"</p>
+    <p>Still rumbling but obedient, the blade elves begin to return to their barracks.</p>
+    <p>Terje leans to Smilisca, "I just made so much money."</p>
+    <p><span class="center">-FIN-</span></p>
+
 
     <?php
     include '../footer.php';
