c3aec86a008071df5cc06a2aacb132f45c04ef7f
xombiehamster <58495678+xombiehamster@users.noreply.github.com>
2019-12-10 11:13:37 -0600

    Update 01_2_Winged.php

--- Winged/Year/01_2_Winged.php
+++ Winged/Year/01_2_Winged.php
@@ -3,37 +3,143 @@
 
 <head>
     <title>
-        River Elf Year 1 - Fall
+        Winged Elf Year 1 - Fall
     </title>
 </head>
 
 <body>
-    <h1>Year 1 - Fall</h1>
+    <h1>Year 1 - Spring</h1>
     <table style="width:100%">
         <tr>
             <th>Stress</th>
             <td>Economic 0/5</td>
             <td>Morale 0/5</td>
             <td>Property 0/4</td>
-            <td>Military 0/5</td>
+            <td>Military 0/4</td>
         </tr>
         <tr>
             <th>Fate Points</th>
-            <td>3/3 Refresh</td>
+            <td>3/4 Refresh</td>
             <td>0 Bonus</td>
-        </tr>
-        <tr>
-            <th>Parager</th>
-            <td>(-12,8)</td>
-            <td>Goldfall River</td>
-        </tr>
+        </tr>   
     </table>
-    <p>Staurois paces uncomfortably across the porch of the Hylidae raft.  Smilisca sits on a stump that’s been bolted to the deck boards and works on tying a complicated knot in the map braid for the Goldfall River.  Acris watches his master’s hands intently from where he sits at his feet, practicing his own knots by tying them in fishing line next to Eina with her feet dangling in the cool clear water.  Litoria stands in the shallow water next to the raft, leaning gently on her hippopotamus while she grazes.</p>
-    <p><b>Economic:</b> With Eina motivating the work efforts with the blessings of Calestros, work begins on building a lift - a “fall hopper” - made of pulleys and sturdy ropes with boulders for counterbalance.  They put to use the extra materials on hand that were left over from the original raft construction projects.  This makes use of the Investment trapping of Infrastructure with 2 Ability, 1 Proficiency, and 2 Boost dice: 5 advantage, 3 successes, 0 triumphs</p>
+
+    <p><b>Misc:</b>
+<p>Shadimon stared at the copper feather sitting on the table. It was the size of a dagger and lovingly detailed, to the individual fibers and the sculpted puff of down at the base. Two polished beads had been fitted onto the shaft, one blue, one white. He went to pick it up and paused, looking at the play of light on one edge, “Is this <i>sharp</i>?”</p>
+
+<p>The artist grinned wider, “It is. We thought that was a good symbol.”</p>
+
+<p>“Goddess is it.” He picked it up, and noticed a small string of tiny metal beads. “What’s this?”</p>
+
+<p>“For attaching a real feather.”</p>
+
+<p>“Ah,” Shadimon drew the sound out, running the short chain through his fingers. “Yes, yes that’s important. This is tremendous. You’re up for doing more?”</p>
+
+<p>She folded her arms and grinned wider. “I already have the next couple started. That one was a successful prototype, and is meant to be ours,” Shadimon glanced up as she continued, “Wouldn’t be right to give them to everyone else and not have one ourselves.” She laughed, “And it was easier to practice, knowing it wasn’t meant as a gift.</p>
+
+<p>“Satri and I have been discussing ideas, but I also wanted to talk with you. Make sure I’m doing the right number, and see if you have any ideas on what might be important to the others.”</p>
+
+<p>Shadimon picked up a tablet and stylus, “Let’s see...us, the blade elves, the fire elves, dark elves, dreamdust elves, the cryptid elves, river elves, ah...oh, totem elves. I believe that’s everyone who stayed. So that’s eight.” He tapped the stylus, leaving small impressions in the wax, “I don’t know that I’d know more than anyone else does. Though...for the blade elves. One of the beads should be taken from a piece of armor, or a weapon. That’s how they remember people.” He grimaced, “Your guess is as good as mine for the cryptid elves.”</p>
+
+<p>“Yes, that one might be, ah, generic. But black and white goes well with anything.”</p>
+
+<p>“Maybe a cloth or leather wrapping, too. That’s what they seem to wear most often.”</p>
+
+<p>“Ohh, good idea…”</p>
+
+They talked for a little longer about colors and styles, and the artist cheerfully returned to her makeshift studio, leaving the finished feather with Shadimon. He tilted one wing forward and ran his hand through the smaller covert feathers until he found one that was ready to come out. Plucking the feather free, he clipped it to the little chain and smoothed out the fibers. Lacking anything better for now, he set it in the center of the table. It could be hung up later to mark whatever the official town center ended up being.</p>
+
+    <p><b>Economic:</b></p>
+<p>Spring is a good time for planting, let’s make another stab at getting that garden established!</p>
+
+<p>Using the Investment trapping of the Infrastructure skill. Rolling 1 ability: 2 advantage, 0 successes, 0 triumphs</p>
+
+    <p><b>Political:</b></p>
+<p>"The scout corp hasn't gotten this much work since the War ended."</p>
+<p>"Keeps them sharp, and we can't afford many risks, as marginal as we still are."</p>
+<p>"Also keeps them <i>busy</i>."</p>
+<p>"Heh, yes. The youmg the boisterous, and the need-something-to-do."</p>
+
+<p>Cloak and Dagger trapping of Spycraft. Rolling 1 ability: 1 advantage, 0 successes, 0 triumphs</p>
+
+    <p><b>Military:</b></p>
+<p>"Has anyone actually gone to the falls?"</p>
+<p>Varna nodded, "Sure! Plenty of people have gone to check it out. The wind coming off of it is <i>great</i>!"</p>
+<p>"I'd believe that. I mean has anyone <i>mapped</i> the area around it?"</p>
+<p>Varna's ears dipped, "Ah...no." He suddenly grinned and gave a messy salute, "Right, orders received!"</p>
+
+<p>Pathfinder trapping of Reconnaissance. Rolling 2 ability and 1 proficiency: 2 advantage, 2 successes, 0 triumphs</p>
+
+    <p><b>Mental:</b></p>
+<p>“You’re nervous; your feathers are fluffed.”</p>
+
+<p>Shadimon resettled his wings and grimaced. The table in front of him was scattered with small slates and tablets, reports from the scouts and Harrier patrols. “On edge, yes. We haven’t found anyone else in this area at all.”</p>
+
+<p>Baijani sat down across from him, “And that worries you.”</p>
+
+<p>“We haven’t been exactly quiet and stealthy here. Anyone in the area should have come to see what’s going on.”</p>
+
+<p>“And we haven’t seen any such thing.”</p>
+
+<p>Shadimon squinted at her, “No, which is what worries me.”</p>
+
+<p>“Maybe there’s no one here anymore.” She raised her eyebrows at his scowl, “But that’s too easy.”</p>
+
+<p>“And we didn’t just survive the most vicious war in recorded history to rely on ‘maybe’. But we aren’t finding <i>anything</i>.”</p>
+
+<p>Baijani tilted her head, “Have you considered a magic approach? A large-scale ward around our area of the canyon, further if we can make it work.”</p>
+
+<p>“That would interfere with us, wouldn’t it?”</p>
+
+<p>“No, not a barricade. Just a spider web, say. Set it to detect people other than winged elves, maybe. But we have our three dreamdust elves, so it would be going off all the time.” She picked up one of the blank tablets and a stylus, “We’re looking for people who shouldn’t be here. But how do we define that? Do we care if people are just looking? Curious?” She looked up, “You’re the security head.”</p>
+
+<p>Shadimon drummed his fingers on the table, “No, we don’t have a reason to discourage any locals yet. Those are still potential allies or trade opportunities, and not automatically threatening. How specific can this kind of ward be? Can you set it to detect...I don’t know, intent?”</p>
+
+<p>Baijani rolled the stylus through her fingers, “Possibly. This kind of scale isn’t my area of expertise. I’ll have to check with our other mages. What do you have in mind?”</p>
+
+<p>“Malicious intent. Anyone, or thing I suppose, meaning harm to the village or any of our people.”</p>
+
+<p>She jotted a few scribbled notes on the tablet, “I’ll start the idea through the gossip mill and see what shakes out.</p>
+
+<p>Magic trapping of Academics to develop ward ritual: detect intent to do harm to Asavardi or its people. Rolling 3 ability: 2 advantage, 4 successes, 0 triumphs</p>
+
+    <p><b>Results:</b></p>
+    <p><b>Economic:</b></p>
+<p>Once again, the hard stone of the canyon walls proves unyielding. While a few small test gardens are growing, they're struggling to get the layout and irrigation right. Despite this, your leadership notices something odd among the elves assigned to work on the garden and canyon wall project. Instead of getting frustrated, they mostly laugh off the problems they are having. Even though they've been struggling to improve the walls for most of the year, morale is still fine. In fact, the setbacks they encounter seem to bolster them more. It seems that they are finally feeling comfortable here, and well… they're elves. They have plenty of time to get it right.</p>
+
+<p>Due to the unwavering morale of your stonemasons, your next attempt on the gardens project gets a bonus die.</p>
+
+    <p><b>Political:</b></p>
+<p>Hey, guess what happened this time? (No spies. Again.)</p>
+
+    <p><b>Political:</b></p>
+<p>Your pathfinders come back with some interesting results. The territory containing the falls obviously has the Massive Waterfall aspect, but interestingly a smaller river feeds into the main one just after the waterfall basin. Following this smaller river leads to a small pool concealed in the adjacent canyons, fed by a small fifteen foot tall or so waterfall coming out of the canyon wall. That waterfall was apparently coming out of some kind of cave system. Feeling adventurous, your pathfinders entered the cave system.</p>
+
+<p>Apparently, this cave system goes for some ways. They ended up several miles into the cave system following the underground cave system before turning back as they weren't equipped for an expedition like that. The square directly to the west of the falls has the Underground River aspect, and it appears to continue further west.</p>
+
+<p>They return to your planned route and explore the square directly to the south of the falls. This area is well and truly the Southern Mountains, and has the High Mountains aspect. It's difficult to function even for flappies at that kind of altitude, but they're able to safely map the area and return on time. For once.</p>
+
+    <p><b>Mental:</b></p>
+<p>Congratulations! You have successfully created the ritual <u>Detect Interlopers</u>!</p>
+
+<p>Risk- Low</p>
+<p>Duration- 1d4 turns</p>
+<p>Target- One settlement</p>
+<p>Difficulty- 2</p>
+<p>Effect- This ritual places a subtle ward on the targeted settlements. If any hostile faction attempts an Espionage check on the warded settlement, their Espionage roll is automatically compared to the total number of successes the ritual received (not net.) If the ritual has more successes, the ward is discharged. The settlement leaders immediately become aware that there is a spy in their midst and may roll a Cloak and Dagger check to find them. If the settlement had already rolled cloak and dagger, they may instead roll a proficiency die and add those results to their opposed check.</p>
+
+<p>Note that the specific wording of the ward states 'intent to harm the settlement or its people' and the ward will not trigger if these conditions are not met.</p>
+
+<p></p>
+<p>‘Home’ is only a word. To the winged elves, it's something of a loaded term. The last place that they called home was put to the torch, taken from them for reasons they scarcely understood at the time. For over a century since they have known nothing but war and hardship. No place has ever felt like home, hardly ever lasting more than a few weeks or months. There are adult elves who do not even remember what it is to have a home or be at peace. That, however, is beginning to change. After so many weeks and  months of quiet tension, the winged elves are beginning to settle. For the first time in many years, you begin to hear the word ‘home’ spoken aloud.</p>
+
+<p>You are refugees no more.</p>
+
 
     <?php
     include '../../settarget.php'; 
-    settarget("Winged",3,null);
+    settarget("Winged",5,null);
+    if($_SESSION["YEAR"]<2){$_SESSION["YEAR"]=2;}
     include '../footer.php';
-    echo getarrows("01_1_Winged.php","01_3_Winged.php");?>
+    echo getarrows("01_3_Winged.php","02_1_Winged.php");?>
 </body>
