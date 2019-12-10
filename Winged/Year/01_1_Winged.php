ced048f7a7812d87dfdedf11f8805d325a406954
xombiehamster <58495678+xombiehamster@users.noreply.github.com>
2019-12-09 19:34:35 -0600

    Update 01_1_Winged.php

--- Winged/Year/01_1_Winged.php
+++ Winged/Year/01_1_Winged.php
@@ -3,12 +3,20 @@
 
 <head>
     <title>
-        River Elf Year 1 - Fall
+        Winged Elf Year 1 - Fall
     </title>
 </head>
 
 <body>
-    <h1>Year 1 - Fall</h1>
+<h1>Year 1 - Fall</h1>
+<p>“Yes we’re ready for the party, and yes we’re ready to go tomorrow.” Baijani leaned down to shift one of the carrying baskets. “Now come. Satri took care of food, and everything will still be here when we get back.”</p>
+
+<p>The enormous farewell feast had been a huge hit; the winged elves went where they pleased, usually carrying samples of food with them to share with friends. They clustered together with each other as well as others, handling cheerful - or tearful - goodbyes.</p>
+
+<p>Three of their leaders - Shadimon, Baijani, and Anasatri - attended a smaller gathering of leaders from all the different factions of elves. They had all been busy, even the blade elves who weren’t planning to leave, and many of them hadn’t seen or spoken to each other for days, too embroiled in their own preparations.</p>
+
+<p>The meeting had been good. They were all able to speak with their counterparts, making last-minute arrangements and agreements. Anasatri also had an actual conversation with the leader(?) of the cryptid elves, an event she wouldn’t stop recounting and boasting about.</p>
+
     <table style="width:100%">
         <tr>
             <th>Stress</th>
@@ -19,18 +27,92 @@
         </tr>
         <tr>
             <th>Fate Points</th>
-            <td>3/3 Refresh</td>
+            <td>4/4 Refresh</td>
             <td>0 Bonus</td>
         </tr>
-        <tr>
-            <th>Parager</th>
-            <td>(-12,8)</td>
-            <td>Goldfall River</td>
-        </tr>
     </table>
-    <p>Staurois paces uncomfortably across the porch of the Hylidae raft.  Smilisca sits on a stump that’s been bolted to the deck boards and works on tying a complicated knot in the map braid for the Goldfall River.  Acris watches his master’s hands intently from where he sits at his feet, practicing his own knots by tying them in fishing line next to Eina with her feet dangling in the cool clear water.  Litoria stands in the shallow water next to the raft, leaning gently on her hippopotamus while she grazes.</p>
-    <p><b>Economic:</b> With Eina motivating the work efforts with the blessings of Calestros, work begins on building a lift - a “fall hopper” - made of pulleys and sturdy ropes with boulders for counterbalance.  They put to use the extra materials on hand that were left over from the original raft construction projects.  This makes use of the Investment trapping of Infrastructure with 2 Ability, 1 Proficiency, and 2 Boost dice: 5 advantage, 3 successes, 0 triumphs</p>
 
+    <p><b>Economic:</b>The winged elves are born travelers, but their specialty is swift, lightly-burdened journeys. The task of moving hundreds of people and the supplies to sustain them was a challenge, and they were more than grateful to the river elves and dreamdust elves for their help in moving bulky and heavier things (and handling the horses and mules, which, if we’re being honest, kinda scare some of the winged elves). Each winged elf carried their own personal baggage, and teams rotated carrying the small nets and larger baskets loaded with things for the community as a whole.</p> 
+
+<p>Varna, the scoutmaster, and his fastest flyers sped ahead upstream, coming back each night to the river elf floatilla to report on conditions ahead. Eventually, all the groups split, and the winged elves struck out on their own, aiming for the stone monuments they could see rising out of the low forest.</p>
+
+<p>Using the Establishment trapping of the Infrastructure skill they establish a village in Goldfall Canyon. Rolling 1 ability: 0 advantage, 0 successes, 0 triumphs</p>
+
+    <p><b>Political:</b> The odds of being followed are very low, but Shadimon isn’t willing to take chances when their future hangs by a thread. He details some of the scouts to watch for movement where there shouldn’t be any.</p>
+
+<p>Using the Cloak and Dagger trapping of the Spycraft skill, they watch for trackers or saboteurs. Rolling 1 sad ability: 0 advantage, 1 successes, 0 triumphs</p>
+
+    <p><b>Military:</b> “We’re getting closer to the southern border, and we’re in unfamiliar terrain. We need to know what kind of wind we’re flying.”</p>
+
+<p>Using the Pathfinders trapping of the Reconnaissance skill, Shadimon sends their veteran scouts ahead to get the lay of the land.  Rolling 3 abilities: 2 advantage, 2 successes, 0 triumphs</p>
+    <p><b>Mental:</b> Knowing what they were planning, the winged elves spoke with engineers from all the other races for any shreds of information on building with stone. As they fly and begin to get a look at the actual terrain they’ll be claiming, they pool their recent knowledge, and anything useful they can glean from their long history of lore and storytelling.</p>
+
+<p>Using the Research trapping of the Academic skill, they try to anticipate problems and develop plans for as many different scenarios as they can imagine. Rolling 4 shiny abilities: 2 advantage, 1 successes, 0 triumphs</p>
+
+<p><b>Results</b></p>
+
+    <p><b>Economic:</b> Congratulations! The foundation of Asavardi is a success! The trip up the river was surprisingly pleasant, for the most part. The first leg was quite nice, with all of the elves travelling together and feeling optimistic it had almost a party feel to it. In fact, you were even able to witness a River elf river bond ceremony. One by one, the other elf groups peeled off though, and eventually it was just you and the river elves. The river elves were having some difficulty moving at their usual pace, so your respective communities parted ways and you moved up the river to the head of the canyon.</p>
+
+<p>Oddly, where the two surrounding mountains make a narrow pass around the river (-17, -17) you found a narrow palisade crossing the river. It was partially collapsed and heavily damaged. Your scouts moved further up the river just to be safe, but found no one.</p>
+
+<p>You moved further up the river and found a good location- high caves up the cliff faces, barely accessible from the ground, formed good cover from the elements. The caves aren’t deep, most only extending thirty or forty feet into the cliff, but it’s enough for now. A few weeks of setting up living spaces in the most spacious of them, and it feels like home. It’s still a pretty bare bones basic setup, but it’s safe enough. Even better, as your people spread out into the shallow caves they discover that the cliffs here are rich in copper. Copper isn’t the most glorious or valuable metal, but it definitely has its uses.</p>
+
+<p>Asavardi has the following statistics:</p>
+<p><ul>
+<li>Name: Asavardi</li>
+<li>Description: Asavardi is built on cliff faces of the canyon system surrounding the Goldfall River. Nearly inaccessible from the ground, homes and buildings are cut directly into the rock using natural caves as a starting point.</li>
+<li>Size: Village</li></ul>
+<p><ul>
+<li>Territory Aspects:</li>
+<li>Copper Veins: The cliffs that Asavardi are built into are rich in copper.</li>
+<li>Riverside: The cliffs that Asavardi are built into run along the Goldfall river.</li></ul>
+<p><ul>
+<li>Settlement Aspects:</li>
+<li>Cliff Homes: The dwellings in Asavardi are largely built into a shallow cave system along the canyon walls.</li>
+<li>Empty Slot</li>
+<li>Garrison: Currently full with the Harriers</li></ul></p>
+
+    <p><b>Political:</b> Things seem pretty quiet on this front. Scouts keep a close eye on the routes leading into the canyon and towards your caves, but they don’t see in anything. One small group of watchers returns late, but it turns out one of their members had just pocketed some Dreamdust elf confections and gotten lost after having a bit too much fun on patrol.</p>
+
+    <p><b>Mental:</b> Turns out, assigning a bunch of easily excitable highly enthusiastic people an extremely vague task involving the words 'as many different scenarios as you can imagine' is kind of risky. It turns out that your researchers can imagine <i>quite a few</i> scenarios. In fact, they get so hung up imagining fresh, exciting new ways that tragedy could strike the winged elves they kind of forget to actually come up with any solutions. You have a really, <i>really</i> long list of potential problems, though. You kind of doubt the ones like 'Lycans were never defeated, have been hiding the whole time' and 'the planet is actually a dragon egg' are at all likely or even anything you could do anything about, and you think more of those smuggled confections may have been involved with that last one.</p>
+
+<p>Still, it’s not a total loss. Apparently your fledgling researchers had a blast coming up with wild unlikely scenarios and got along quite well. A small core of clever elves has come together and appear to be quite happy to be a think tank.</p>
+
+<p>You will not get a setback die from Overwhelming Enthusiasm if you change Research topics after this failure, and indeed your next Research check will get a boost die.</p>
+
+    <p><b>Military:</b> This is where things get interesting.</p>
+
+<p>Your Pathfinders arrive, weeks behind schedule. They are nearly out of supplies, exhausted, and nearly crash land coming home. They don’t appear hurt, and they’re all accounted for. After a day or so of recovery, they are able to give a full debriefing.</p>
+
+<p>They were able to map most of the large canyon that your settlement was in. They headed towards the waterfall and started working their way around.</p>
+
+<p>Immediately, they found something extremely disturbing. On the other side of the river nestled against the other side of the canyon wall is the remains of another settlement- and it is a disturbing sight.</p>
+
+<p>They estimate the village held several hundred people. It appears to have been mostly smaller shanty-style buildings, though there were...well, were the remains of a few larger structures. Whatever happened to this settlement, it was ugly - and involved fire. A lot of fire.</p>
+
+<p>Your Pathfinders surmise that during the fall, someone- probably elves, humans, or a combination thereof judging by the skeletons- attempted to flee from whatever chaos was happening in this region and build a safe haven in this canyon. Whatever they were fleeing from, it found them. The palisade at the canyon mouth was probably theirs- and someone got through it.</p>
+
+<p>The settlement was burned, and it appears the majority of the occupants were killed. Slaughtered might be a better word. There are hundreds of skeletons in the city, burned to ash apparently inside the buildings they lived in. The whole settlement was put to the sword and the torch, though they estimate it was at least a few years ago.</p>
+
+<p>The Pathfinders didn’t stay there long. It gave them the, and I quote, ‘heebie jeebies’. They continued north towards the mouth of the canyon.</p>
+
+<p>They had just finished mapping the sector containing the palisade when they took a break. The squad went to the top of the highest peak they could get to in the sector to ‘get some fresh air’. The view is apparently breathtaking from up there.</p>
+
+<p>From there, they noticed a heavy storm system moving up the river. They watched it for a couple days before one of the more eagle-eyed scouts noticed something- further down the river, an embankment was collapsing under the weight of the storm.</p>
+
+<p>The Pathfinders correctly guessed that the embankment’s collapse would cause a sudden flash flood- right down the river towards Partager and the unsuspecting river elves.</p>
+
+<p>Your lead Pathfinder apologizes for abandoning his mission. He lead his squad downriver, as fast as their wings would carry them. They dropped any gear they could live without and flew day and night. They were able to warn the river elves, but only moments before the flood hit. They stayed in the area for several days assisting the river elves in recovering and in finding the missing. He reports that they sustained damage, but apparently few casualties.</p>
+
+<p>The head Pathfinder accepts responsibility for abandoning their original mission. They were, however, able to map 4 squares of territory surrounding your settlement, including both the gates and the destroyed settlement.</p>
+
+<p>Speaking of that settlement…</p>
+
+<p><b>Event: Are We Safe?</b></p>
+
+<p>News of the destroyed settlement leaks quickly and spreads like wildfire in your population. Gawkers fly out to the destroyed settlement to see it for themselves. They come back worried. The more cautious members of your settlement begin to openly question whether you made the right choice by coming here. There are quite a few talking about abandoning Asavardi and heading for those coastal forests. The fact that fire was involved in the other settlement’s destruction only makes people more nervous. This canyon didn’t protect its previous occupants, why will it protect you?</p>
+
+<p>Take one point of Morale stress, and you will take one point of morale stress per round until your people are confident that they are safe and that the fate of that settlement will not befall them as well.</p>
     <?php
     include '../../settarget.php'; 
     settarget("Winged",2,null);
