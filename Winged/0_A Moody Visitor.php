f8a44aa0a3e79caf806ab4622548164ffd0fd330
xombiehamster <58495678+xombiehamster@users.noreply.github.com>
2019-12-10 13:54:02 -0600

    Create 0_A Moody Visitor.php

---
+++ Winged/0_A Moody Visitor.php
@@ -0,0 +1,153 @@
+<html>
+
+<p><b>A Moody Visitor</b></p>
+
+<p>“...Say again?”</p>
+
+<p>“There’s what appears to be a dark elf coming up the canyon.”</p>
+
+<p>“A dark elf. One...dark elf? Alone?”</p>
+
+<p>“As far as I could tell. He wasn’t really even trying to hide, just kind of...wandering this way.”</p>
+
+<p>Varna scratched his head, “Go talk to him, I guess? See what he wants? They don’t usually travel above ground, do they? Take someone else to keep watch, just in case there’s something weird going on.”</p>
+
+<p>~~</p>
+
+<p>Tinsin landed on a boulder while Alenwe circled up above. With some muttering and skittering noises, the lone dark elf came around the rock, looking around distractedly from under his hood.</p>
+
+<p>Tinsin cleared her throat, “Ah, hello?”</p>
+
+<p>The dark elf jumped, nearly falling back out of sight before catching himself. It took him a moment to find her against the glare of the sky, “Oh, hello!”</p>
+
+<p>“Are you uh, lost?”</p>
+
+<p>“Well, I would be if I knew exactly where I was going. Is your village this way?”</p>
+
+<p>Tinsin fought the urge to cover her face, and hoped he couldn’t quite see her expression. “You’re heading in the right direction, yes. Are you a messenger?”</p>
+
+<p>"Uh...I mean, I do have a message but it wasn't why I was sent out?"</p>
+
+<p>“...Right, all right then. I assume you’ll need to pass that message on to one of our leaders, then?” She spread her wings at his enthusiastic nod, “Follow us; it’s still a ways away.”</p>
+
+<p>Tinsin and Alenwe took turns dropping back to lead the dark elf. The poor thing seemed to be struggling, but they weren’t sure what they could do to help other than scoping out the easiest paths between the rocks. It took several hours to make it back to the village, where the young elf collapsed on the soft sand, going flat on his back. “Ok, I think I’m just going to live here.” He looked the very long way up the cliffs and squirmed a little deeper into the sand. “Did I mention I’m not a great climber?”</p>
+
+<p>“At least there are ladders?” Alenwe offered.</p>
+
+<p>“Ladders are good. I like ladders.”</p>
+
+<p>“Oh, sorry, I’m Tinsin, by the way. This is Alenwe.”</p>
+
+<p>“I’m Amenidal. Probably. Ask me again when I’ve had a nap.”</p>
+
+<p>Tinsin squinted up to the canyon rim, “You may not have to climb afterall.” A puff of dust preceded four elves with a carry basket launching off the cliff. Amenidal watched with curiosity and dawning terror as the team glided down and landed further down the canyon. </p>
+
+<p>“Welcome!” one of the carriers called as Tinsin and Alenwe helped their visitor to his feet. “We aren’t exactly ready for ground-bound visitors yet, I’m afraid, and we couldn’t expect you to try to actually climb the cliffs.”</p>
+
+<p>“Though it’s very good you’re a dark elf,” one of the others added. “Anyone else would probably be too heavy.”</p>
+
+<p>“Right, great…” Amenidal let himself be led to the basket, where one of the carriers tied a safety line around his narrow waist. </p>
+
+<p>“All right, you can sit or lay down, whichever is more comfortable to you. Holding onto the sides usually helps people feel more stable?”</p>
+
+<p>The dark elf clambered into the basket and knelt down, “Is this ok?” The basket suddenly looked awfully flimsy...</p>
+
+<p>“Perfect! Just hang on to the sides until we get in the air. Taking off can be bumpy.”</p>
+
+<p>Tinsin and Alenwe took off first, getting out of the way. Amenidal had a brief moment of questioning all of this life choices before the carriers spread their wings. “Oh Echoes…” On some cue he missed, all four leaped upward and slashed their wings down. Amenidal clutched at the basket and squeaked as it suddenly lurched. The thundering wind kicked up huge clouds of dust, making him duck his head. The vertigo of closed eyes was worse than the dust, he decided, and cracked his eyes open again. The view from the front of the basket showed the ground racing by and receding at an alarming pace, so he looked to the side, at one of the walls. The rumpled striations of color in the rock were very pretty, he decided, and significantly more interesting than the now-very-distant canyon floor. </p>
+
+<p>Just when he thought he had adjusted, they burst up out of the canyon and into the open air. The carriers heard a brief muffled shriek, and started a long, broad turn back toward the village. Amenidal gave up and closed his eyes; as alarming as the ground receding was, it was much worse to see it racing back toward him. There was another hurricane of wind, during which he braced himself for a collision, and then the basket set down with a surprisingly gentle thump. The ensuing silence was very loud, broken briefly by the snap and rustle of folding feathers. </p>
+
+<p>“Amenidal? Did you make it?”</p>
+
+<p>“I think so? Are we down? Am I dead?”</p>
+
+<p>There was laughter and then several hands helping pull him upright. Someone undid the safety line and they helped him step out of the basket. He wobbled a few steps, “I’m good, I think, can I just…” His knees started to give out and they guided him to a rock. Tinsin stood nearby, one of her wings spread to give him some shade while he caught his breath. Someone else came by with a cup of water.</p>
+
+<p>After a few moments, Amenidal looked around. The top of the cliff was broad and flat. There were farm plots back away from the edge, and a few buildings under construction. “Are we there yet?”</p>
+
+<p>“Almost,” Tinsin said apologetically. “The village itself is on the cliff. So from here...we go down. This time there are ladders!” she hastened to add. “And paths! The worst is over?”</p>
+
+<p>The ladders down were gratifyingly sturdy wood, not rope like Amenidal had dreaded. He kept his eyes very firmly fixed on the wall in front of him as he went down, but at the bottom, he turned and got an uninterrupted view of the vast canyon spreading out below him. He squeaked and put his back against the wall. Tinsin came down behind him and put herself between him and the drop. The path was smooth and reinforced by wood in places. One more ladder and another, wider path, and eventually they came out onto a broad, smooth ledge that led into a cave mouth.</p>
+
+<p>Amenidal bolted into the cave and went face-down on the floor, sighing with profound relief. The other elves in the room looked slightly concerned before Tinsin flashed a reassuring hand-sign. Someone came over anyway and set a cup of water next to the downed dark elf. He squeaked, and she assumed that was gratitude.</p>
+
+<p>A few hours later, Shadimon and Baijani landed on the ledge, intent on heading to the office space off of the main cave. By that point, Amenidal had acquired a blanket, a plate with some gnawed-on food, and a small sign reading ‘Do Not Disturb’.</p>
+
+<p>Shadimon blinked and Baijani burst out laughing. “I see our guest made it. More or less.”</p>
+
+<p>“He woke up and ate a little bit earlier,” one of the workers called. “Poor brancher’s just had a long day; we’re keeping an eye on him.”</p>
+
+<p>“We don’t really have anything resembling guest quarters…”</p>
+
+<p>Baijani waved a hand, “Nonsense, I’ll take him home with me. It’s an easy walk from here. We have space, and dark elves don’t do any better alone than we do.” She crouched down next to him, bracing herself with one wing. “Amenidal?”</p>
+
+<p>No response.</p>
+
+<p>She lightly tugged on one of his enormous ears, “Time to wake up; sleeping on the floor all night won’t do you any favors, no matter how young you are.”</p>
+
+<p>He mumbled and tilted his head, cracking open one eye. </p>
+
+<p>“There you are. Come on, let’s get you some dinner and a real bed.”</p>
+
+<p>He blinked owlishly at her, then slowly sat up, holding the blanket around his narrow shoulders. “That...sounds really nice.”</p>
+
+<p>“Up you get then, son.” She helped him to his feet and paused for him to collect his things. When he hesitated over the random things people had put on or near him, she patted his shoulder, “Don’t worry about it.” She led him out of the hall, once again keeping him to the safer inside of the path. The walk to her home was relatively short, and she pushed open the door onto a cheerfully lit room filled with interesting smells. </p>
+
+<p>“My children! We have a guest!”</p>
+
+<p>Amenidal balked at the door as several pairs of bright eyes turned to him, but Baijani kept a comforting hand on his back. “This is Amenidal. He’s visiting from the Caldera, and will be staying with us for now.”</p>
+
+<p>“Welcome!” one of the others said. She was smaller than Amenidal, and had striking red and cream wings. “Here, come have a seat. We’re almost ready for dinner.” She led him over to a pit in the floor, lined with blankets and pillows, and with a low table in the middle. “You came all this way alone? You must be exhausted!”</p>
+
+<p>His ears turned an exciting shade of pink.  “It wasn’t that bad!  The way to the Fort might not have cliffs but it… uh… lacks shade and I’m not the best at - ah… physical exertion?”  As he spoke, he seemed to get lost looking around the room.  The stone work wasn’t bad… ish.</p>
+
+<p>“This isn’t the easiest territory to cross on the ground, either. I’m Astalance, by the way. The lady who adopted you is Baijani, my grandmother.” She patted his shoulder sympathetically, “I do mean adopted. She does this to everyone.” </p>
+
+<p>Amenidal’s attention snapped back at the pat.  He blinked, a confused expression graced his face,  “Adopted?  Do you have a lot of siblings then?” </p>
+
+<p>She laughed, “Not blood-siblings. All of us here are blood - except Kelani, but she’s also been adopted. No, it’s just what she does. She likes people, you know? Likes taking care of them. Having a hungry young elf show up all alone? You’re a goner.” She grinned, “It’s not official or whatever. For you, just means that you’re welcome to stay here with us as long as you need to.”</p>
+
+<p>He looked down for a second.  It didn’t seem that far from how dark elves worked, but Mother Echo that sounded like a lot of people to stuff into one house.  “She sounds like Uncle.”  Amenidal looked back up with a wide smile on his face, “Thank you, Astalance!  I think I like the thought of being a goner.” </p>
+
+<p>“Very good,” Baijani said. “The house is full of granddaughters and needs at least one grandson to even it out. I’m sure your daily schedule is all thrown off, but for us it’s almost time to go to bed. We have this last big meal and then wind down for the night. You’re welcome to keep whatever schedule suits you best.” </p>
+
+<p>Amenidal flushed lightly.  Right, Baijani was still there.</p>
+
+<p>The rest of the family crowded into the sitting area, giving Amenidal a bit of space but generally being on top of each other. Dinner was some sort of...pastry? Filled with meat and vegetables and maybe eggs? “Ah, are there any things you can’t or shouldn’t eat? Or things you should?”</p>
+
+<p>“Um… we mostly eat meats but I think vegetables are ok?”  He floundered for a few moments at there being so many other bodies.  He usually took meals alone, so this was excitingly new.</p>
+
+<p>Curiously, he poked a pastry before a piece is torn off.  “Not sure I’ll be able to finish the whole thing, but it looks really good!” </p>
+
+<p>“Don’t worry about that. Last meal of the day is usually the heaviest for us, and anything we don’t finish now will be there for breakfast.” Baijani broke one of the pastries in half, “Do let us know if you need a break. There are quite a few of us, and I know we can be a little much sometimes.” </p>
+
+<p>Though the group babbled and chatted amongst themselves, they refrained from pelting Amenidal with questions. Not that they excluded him, by any means, but gave him the option of when and how much he wanted to engage. Surprisingly, he engaged quite a bit and even fired off a few questions of his own.  After dinner things were tidied away, one of the others showed Amenidal around the home, especially important things like the bathroom.</p>
+
+<p>“So, I’m not entirely caught up on how dark elves do things,” Baijani said, “but we sleep in family groups. Even if that is how you do things too, we’re still technically strangers. You can join us if you like, or sleep separate if that’s more comfortable.”</p>
+
+<p>“We also sleep in family groups!”  He paused, ears drooped slightly.  “I uh… haven’t done that in a while, so if it’s ok if I...joined?” </p>
+
+<p>“Absolutely! I think I remember that it’s cold to you up on the surface too, yes? We’ve got plenty of blankets and it’s never cold with a group.” Baijani led him to the cozy bedroom, where the others were undressing for bed. Shrike paused in the act of kicking her pants off, “Ah...do dark elves have problems with nudity?” </p>
+
+<p>“The surface is so cold, but some caves also get pretty chilly too when you’re out on scouting patrols and stuff.”  He tilted his head at Shrike’s question.  “Not really?  You guys are kinda…” he made a wobbly hand gesture, “naked in a different way to us?”</p>
+
+<p>Amenidal glanced around the room with interest as he continued, “Skin contact is more of an issue but you are all family and if you roll me up in blankets I doubt there will be any issues.”</p> 
+
+<p>Shrike eyed him with interest, “Ahh, the tattoos? I guess that must be strange. You start getting those pretty young, don’t you?” One of her nieces leaned around her to toss a blanket to Amenidal, “We’ll let you bundle up and squish you in the middle then!”</p>
+
+<p>A squeak left him as he fumbled to get a hold on the blanket.  It carried over into his words before evening out, “Ah!  Yes, we start to get them in our teens usually.”  He promptly wrapped the blanket around himself, an excited bounce starting up. </p>
+
+<p>Shrike chuckled and scooped him up once he’s wrapped up, tossing him lightly into the nest of pillows. Someone else put out the lamp, and the rest of them settled in what appeared to be usual places, many of which seem to be partially on top of Amenidal. A wing ended up arched over his head, keeping in the warmth. The feathers had an almost musty smell, but not in a bad way, and the room was warm and quiet as everyone settled. </p>
+
+<p>The weight of other people causes a tiny bubble of tension to release from Amenidal’s chest.  He wiggled happily in the set up and let himself sink into it.</p>
+
+<p>Time passed and still sleep does not claim him.  He knew this was due to how backwards his sleep schedule was because of traveling, but still that climb had taken a lot out of him and the meal he had just shared was heavy in his stomach.  It was plenty to make even more physically fit dark elves fall asleep.</p>
+
+<p>He stared at the feathers above him and listened to the simple noises of others breathing around him.</p>
+
+<p>Amenidal’s eyes stung slightly.  It had been so long… he had forgotten how much he missed this.</p>
+
+<p>He was not entirely sure why he thought the winged elves would be more rigid to his presence than they were turning out to be.  They were a warm people with a warm culture to go with it.  He made a quiet vow to himself to rectify his misgivings and learn more of who they were while he was staying in the city.</p>
+
+<p>The night was long, but eventually he forced himself to at least doze for a short period of time. </p>
