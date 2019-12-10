5e21a070b9f051067969749f11307bb2a3c7d532
xombiehamster <58495678+xombiehamster@users.noreply.github.com>
2019-12-10 11:14:31 -0600

    Update 01_3_Winged.php

--- Winged/Year/01_3_Winged.php
+++ Winged/Year/01_3_Winged.php
@@ -3,7 +3,7 @@
 
 <head>
     <title>
-        River Elf Year 1 - Fall
+        Winged Elf Year 1 - Fall
     </title>
 </head>
 
@@ -13,23 +13,98 @@
         <tr>
             <th>Stress</th>
             <td>Economic 0/5</td>
-            <td>Morale 0/5</td>
+            <td>Morale 1/5, healing</td>
             <td>Property 0/4</td>
             <td>Military 0/5</td>
         </tr>
         <tr>
             <th>Fate Points</th>
-            <td>3/3 Refresh</td>
+            <td>3/4 Refresh</td>
             <td>0 Bonus</td>
         </tr>
-        <tr>
-            <th>Parager</th>
-            <td>(-12,8)</td>
-            <td>Goldfall River</td>
-        </tr>
     </table>
-    <p>Staurois paces uncomfortably across the porch of the Hylidae raft.  Smilisca sits on a stump that’s been bolted to the deck boards and works on tying a complicated knot in the map braid for the Goldfall River.  Acris watches his master’s hands intently from where he sits at his feet, practicing his own knots by tying them in fishing line next to Eina with her feet dangling in the cool clear water.  Litoria stands in the shallow water next to the raft, leaning gently on her hippopotamus while she grazes.</p>
-    <p><b>Economic:</b> With Eina motivating the work efforts with the blessings of Calestros, work begins on building a lift - a “fall hopper” - made of pulleys and sturdy ropes with boulders for counterbalance.  They put to use the extra materials on hand that were left over from the original raft construction projects.  This makes use of the Investment trapping of Infrastructure with 2 Ability, 1 Proficiency, and 2 Boost dice: 5 advantage, 3 successes, 0 triumphs</p>
+
+    <p><b>Economic:</b></p>
+<p>Working stone continued to be hard. Some of their artisans had done stone-carving before, but never on such a huge scale. The most they were experienced with was sculpting small pieces, and they all lamented that the cliffs were a different type of stone that didn’t behave at all like they expected. Fortunately, through all the trial and error, there were mercifully few injuries, and no deaths. A few people sported patches of mismatched feathers, where broken ones had to be replaced by donations from friends, but most wore those as a mark of chagrined pride.</p>
+
+<p>The first attempts at establishing a garden on the cliff walls had failed. Some patches that had been seeded were still under close observation, but the locations with live plants had mostly fallen apart. Flying through the canyon, Shadimon could see the darker streaks where people were lining ledges with richer soil to try to get plants to take. Ledges around developing house fronts were quickly acquiring soil, and some people were beginning to carve planters under windows, or plant climbing vines at the bases of door posts.</p>
+
+<p>A knot of people were clustered around a native patch of greenery, and Shadimon glided closer for a better look. One of them waved him in, and he found a narrow edge to land on. “Shadi! Still trying to be everywhere I see.” </p>
+
+<p>Shadimon snorted, leaning against the wall, “Since there never seems to be a lack of work to be done, and <i>someone</i> decided to leave it all to me.”</p>
+
+<p>Kirien batted his eyelashes, “I was sure you could handle it. I deserved my retirement.”</p>
+
+<p>“Yes, ‘retirement’ in the form of a very large rock, if I recall…”</p>
+
+<p>Kirien laughed and patted his leg. It had been healed magically, but the shattering damage that had taken him out of the war had left it weakened. “Yes indeed. A perfect excuse. Look here, we’re looking at these plants that are already growing for ideas on how to do it ourselves. Half of them are just growing out of bare rock, it looks like. But some of them have made their own soil, as they die back for the winter. It doesn’t seem to get as cold here, so the growing season might be better. We want to try again in the spring, see if we can get some things established as everything already here starts growing again. Could protect some of our shade-loving plants down here too.”</p>
+
+<p>“Good to know you have it in hand. Plants were never my specialty.”</p>
+
+<p>“Mhm, Tassimir has mostly taken charge of it. He was the one that grew the canopy orchids before, you remember. He’s good with difficult plants. But speaking of...well, it was his idea, and I’ll let him propose it formally, but it’s a good one.” He grinned at Shadimon’s aggrieved sigh, “It’s an important one, so you’ll have to receive it properly as the wingleader.”</p> 
+
+<p>“Fine, but now I’m curious. Make sure he finds me soon.”</p>
+
+<p>Kirien and his helpers waved as Shadimon stepped off the ledge. Kirien was right that the canyon walls could be an ideal place to grow some of their plants, but it was going to take time. They’d had canopy gardens in the forest, but growing plants on living trees was significantly easier than on bare rock. They were, once again, having to learn new techniques.</p>
+
+<p>Investment trapping of Infrastructure to continue making improvements to the settlement. Rolling 1 ability die: 2 advantage, 0 successes, 0 triumphs</p>
+
+    <p><b>Politlca:</b></p>
+<p>Still no known issues, but worth keeping an eye out. </p>
+<p>Cloak and Dagger trapping of Spycraft to keep looking for the spies we're sure are there somewhere. Rolling 1 ability: 2 advantage, 0 successes, 0 triumphs</p>
+
+    <p><b>Military:</b></p>
+<p>More exploration is always needed, especially to see if there are any more unpleasant surprises waiting for us. Now we're going to investigate the eastern side of the canyon.</p>
+
+<p>Pathfinder trapping of Reconnaissance to continue exploring and getting to know our new home. Rolling 2 ability and 1 proficiency: 3 advantage, 1 successes, 0 triumphs</p>
+
+    <p><b>Mental:</b></p>
+<p>Improving the living spaces is all well and good and needs to be done, but for more to happen, we’ve got to have something to trade or sell. We know we have these copper veins, but we know nothing about mining, and we can’t keep waiting on help from the dark elves. We’re going to get our think tank back together with a bit more direction this time: what have we learned about working stone in the past months, and how the hell do you convert ore to useable metal?</p>
+
+<p>Using Research trapping of Academics to get us ready to start making use of those copper veins. Rolling 4 ability and that shiny boost die from the first turn: 2 advantage, 4 successes, 0 triumphs</p>
+
+    <p><b>Results:</b></p>
+    <p><b>Misc:</b></p>
+<p>Your patrols find a Dark Elf named Amenidal wandering around one of the side canyons near Asavardi. He seems quite pleasant and says he just wanted to meet some winged elves and see how you’re doing. Oh, and tell you that the dark elves’ new home of Hilt’Inn is nearly overrun by hundreds of skeletons. They’re probably fine though, Amenidal explains.</p>
+
+    <p><b>Economic:</b></p>
+<p>Wow, stone is <i>considerably</i> more difficult to work with than wood. The winged elves have been throwing themselves at improving the stonework around Asavardi from ‘literally living in caves’ to ‘actual homes’ and so far, it’s been slow going. It hasn’t been hopeless or anything, but there’s been quite a lot of broken tools, stubbed thumbs, and falling rock injuries.</p>
+
+<p>The winged elves themselves, though, remain irrepressible. While the stone has been unyielding so far, they are nothing but enthusiastic. It seems the more difficult the stone is to carve, the more enthusiastic they become at working with it.</p>
+
+<p>At break times, communal meals, or evening fireside chats it seems all they can talk about is whatever creative way they have managed to mess it all up this time. Indeed, with time, this actually works to your advantage. More so than with a simple job completed efficiently, the sheer variety of clever ways the winged elves have mucked up actually turns into something of an advantage.</p>
+
+<p>Your engineers have become intimately familiar with the rocks, walls, and caves of Asavardi. Though they haven’t made much progress this season, the creative ways they begin to approach their problems result in many novel solutions that might only work for a people like yours.</p>
+
+<p>All future checks to improve the cliffs or caves in Asavardi will have their difficulty reduced by one.</p>
+
+    <p><b>Political:</b></p>
+<p>Well, still no spies this turn. All is quiet. There’s a slight air of paranoia as some elves seem to think things have been <i>too</i> quiet. No one’s freaking out or anything, but it’s a general opinion that it’s very strange that despite all the interesting stuff going on in this canyon no one’s come poking around. Several of your watchelves voluntarily start pulling extra duty, just to be safe.</p>
+
+<p>The next Espionage check taken against you will receive a setback die due to these volunteers.</p>
+
+    <p><b>Military:</b></p>
+<p>Your Pathfinders return a little sheepishly, though they appear in high spirits. The sector directly east of Asavardi is more mesa, with deep ravines. Most excitingly, it seems like most of the mesa area above the canyon is covered in high pine forests. This square has the aspects High Forest and Twisting Canyons.</p>
+
+<p>To the southeast is where it got exciting. Apparently hydrated by the water from the upper Goldfall, there is a large dense Darkwood forest overlooking the canyon! The trees are much smaller than the old growth trees back home, but the forest is healthy and covers much of the canyon rim. This location provides a stunning view of the canyon and the falls.</p>
+
+<p>That’s actually where problems started. Your pathfinders got a bit too excited scouting this area. They admit that they spent at least a week just… enjoying being in a forest, for the first time in years. Darkwood trees are beautiful and rare, and no one has seen one in decades much less dozens of them. They lost track of time before making the trek northeast, and thus it wasn’t explored. Square 2 hast the aspects Darkwood Forest and Stunning Views.</p>
+
+    <p><b>Mental:</b></p>
+<p>Your research team has been gathering information and sources about stoneworking and digging since you decided that the canyons would be your new home. It stands to reason that a few of the sources they had gathered would also contain some information about mining. Your think tank goes to work digging through their sources to find useful information.</p>
+
+<p>About twenty minutes into that, your head researcher returns furious. Someone has been messing with his file system! He had them all set aside in neat stacks, just where he would remember where they were. Someone came along and… and… organized  them! With files! And labels! And <i>SHELVES</i>!</p>
+
+<p>After being told to take it down a notch, he returns to the business at hand. The rest of the team reports that the new file organization system is quite helpful! Not only are they able to find helpful information they knew they had, the system helps them find more information buried in the random sources they had already gathered.</p>
+
+<p>Aside from the obvious construction, art, currency, and decorative purposes copper actually has quite a few useful attributes. It is a key component in bronze. It is highly conductive, being useful for electricity magic as well as mundane items like lightning rods. It does not rust, and can be used to protect other metals or wood from humidity. It makes excellent cookware. </p>
+
+<p>As far as mining, most of the copper you are finding is native metallic, meaning it does not require any kind of ore smelting and can be just directly chipped out of the rock. Your think tank has quite a few methods for extracting and processing it, most of which should be quite applicable even with the minimum amount of infrastructure you have at the moment. The plan they come up with for mining it indeed seems almost easy.</p>
+
+<p>The first check you make to make use of the Copper Veins aspect will have an Upgrade and the difficulty reduced by one.</p>
+
+
+
 
     <?php
     include '../../settarget.php'; 
