82e3ebf62d567caf04e8f42a5dbc4759490b181e
xombiehamster <58495678+xombiehamster@users.noreply.github.com>
2019-12-10 19:38:43 -0600

    oops, hand slipped.

---
+++ River/6_A Hitchhiker in Partager.php
@@ -0,0 +1,18 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+
+<head>
+    <title>
+        A Hitchhiker in Partager
+    </title>
+    <link rel="stylesheet" type="text/css" href="../style.css">
+</head>
+<body>
+    <h1>A Hitchhiker in Partager</h1>
+    <p>A placeholder</p>
+    <?php 
+        include '../footer.php';
+        echo getarrows("Year/02_1_River.php#jump","Year/02_1_River.php#jump");
+    ?>
+</body>
+</html>
