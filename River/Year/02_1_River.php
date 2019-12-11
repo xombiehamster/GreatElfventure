6a0d0c707eb7327012b61fe2e92ea5b6a825d4b7
xombiehamster <58495678+xombiehamster@users.noreply.github.com>
2019-12-10 19:45:09 -0600

    Update 02_1_River.php

--- River/Year/02_1_River.php
+++ River/Year/02_1_River.php
@@ -39,7 +39,8 @@
         <li>
             How can I help?
         </li>
-    </ui>
+    </ul>
+    <br>
     <table style="width:100%">
         <tr>
             <th>Stress</th>
@@ -61,7 +62,7 @@
     </table>
     <p>Litoria and Sauver had previously planned to stay behind on the journey back to Partager, but with Smilisca having gone ahead, she can’t leave Acris to try and find it on his own.  Separated from his Master, the Apprentice becomes enmeshed with the Pathfinders they meet at the chosen location for Winter Quarters.</p>
     <p>Partager, with Smilisca there to direct, manages to avoid the Crag Elf danger zone.  While traversing the swampy terrain, they keep an eye out for any signs that the Totem Elves had been in the area.</p>
-    <h2 id="jump">Roleplay Event 2.1.1: <a href="../6_A Hitchhiker in Partager.php">A Hitchhiker in Partager</a></h2>
+    <h3 id="jump">Roleplay Event 2.1.1: <a href="../6_A Hitchhiker in Partager.php">A Hitchhiker in Partager</a></h3>
     <p>Ink, returning with Smilisca’s Spellbook, also comes with Barrabus in tow.  Barrabus, of course, is absolutely adamant that Ink just randomly kidnapped him. Ink comments little on this before teleporting away, leaving poor Barry apparently stranded.  During his stay, Barrabus learns of Ny’s pregnancy and promises to keep it a secret for the time being.</p>
     <p><b>Economic:</b> Some quick thinking and significant effort drive Smilisca and Staurois to reorganize the hauling capacity of the flotilla to accept the trade goods given in exchange by the Crag Elves for what food they can spare.  Doing this work without the effort of actually delivering their cargo seems odd, but they respect the Crag Elves’ privacy by meeting at the requested drop point and no closer.  Due to the dire circumstances of this request, they delay the construction of Winter Quarters to aid their fellow elves.</p>
     <p>This uses the <i>Merchants</i> trapping of the <b>Trade</b> skill, and their shiny new stunt: Elf Express, and they roll 2 Ability and 1 Proficiency die: 3 advantage, 0 successes, 0 triumphs  Due to the dire nature of this need, they spend a Fate point to give that another try, and get: <kbd>2 advantage, 3 successes, 0 triumphs</kbd></p>
@@ -70,7 +71,7 @@
     <p><b>Military: </b>To work out some of their bitterness and keep them distracted, Litoria asks the pathfinders to scout the terrain to the North of the designated location for Winter Quarters.  This is a good distraction for her too.  The Crag Elves’ sitting in their homeland, comfortable or not, is a bitter pill to swallow when so much vast, uncrossable desert lies between herself and hers.  On Fort Alfyr’s map, they scout (-3, 4) and (-3, 3).  This uses the <i>Pathfinders</i> trapping of <b>Reconnaissance</b> with 2 Ability dice: 1 advantage, 1 successes, 0 triumphs</p>
     <p><b>Mental:</b> Despite remembering her time as a young acolyte fondly, Eina thinks that if Calestros calls adults to the Service, she’s certainly willing to have their help.  But it’s an unusual situation to be developing a priesthood basically from scratch.  Eina wants to take into account the life experiences of the new volunteers and not treat them as children entering Calestros’s service in their youth as she did.  She opts to research how other elves handle religious devotion to see if there are any existing solutions to the question that they could adapt to their own uses.</p>
     <p>With help from those who will be most affected by this, she uses the <i>Research</i> trapping of <b>Spirituality</b> with 1 Proficiency die: <kbd>0 advantage, 2 successes, 0 triumphs</kbd> + a Setback die to be rolled by GM.</p>
-    <h2 id="jump2">Roleplay Event 2.1.2: <a href="../7_A Special Delivery.php">A Special Delivery</a></h2>
+    <h3 id="jump2">Roleplay Event 2.1.2: <a href="../7_A Special Delivery.php">A Special Delivery</a></h3>
     <h2>Results</h2>
     <p><b>Economic:</b> <i>GOAL REWARD: Blood in the Elven body.</i> Due to the river elves’ absolute devotion to serving as the lifeblood of this new elven land, even at the cost of their own plans, the difficulty of this trade check has gone down by one.</p>
     <p>Oh, those poor poor crag elves. They have managed to scrounge up some amount of iron ore and supplies, but it’s quite obvious it’s wildly unorganized. Different qualities and types of ore, all staggered around haphazardly with no measurements of type or kind, no real way to tell exactly what they have. And they’re so stiff, so completely unsure of how to handle themselves during a little friendly bartering.</p>
