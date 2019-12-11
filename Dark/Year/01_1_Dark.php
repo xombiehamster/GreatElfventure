51299f93562772f4fe3a4a89503166ccd7379272
xombiehamster <58495678+xombiehamster@users.noreply.github.com>
2019-12-10 21:53:14 -0600

    dark elves wat wat

--- Dark/Year/01_1_Dark.php
+++ Dark/Year/01_1_Dark.php
@@ -3,12 +3,12 @@
 
 <head>
     <title>
-        River Elf Year 1 - Fall
+        Dark Elf Year 1 - Summer
     </title>
 </head>
 
 <body>
-    <h1>Year 1 - Fall</h1>
+    <h1>Year 1 - Summer</h1>
     <table style="width:100%">
         <tr>
             <th>Stress</th>
@@ -22,14 +22,55 @@
             <td>3/3 Refresh</td>
             <td>0 Bonus</td>
         </tr>
-        <tr>
-            <th>Parager</th>
-            <td>(-12,8)</td>
-            <td>Goldfall River</td>
-        </tr>
     </table>
-    <p>Staurois paces uncomfortably across the porch of the Hylidae raft.  Smilisca sits on a stump that’s been bolted to the deck boards and works on tying a complicated knot in the map braid for the Goldfall River.  Acris watches his master’s hands intently from where he sits at his feet, practicing his own knots by tying them in fishing line next to Eina with her feet dangling in the cool clear water.  Litoria stands in the shallow water next to the raft, leaning gently on her hippopotamus while she grazes.</p>
-    <p><b>Economic:</b> With Eina motivating the work efforts with the blessings of Calestros, work begins on building a lift - a “fall hopper” - made of pulleys and sturdy ropes with boulders for counterbalance.  They put to use the extra materials on hand that were left over from the original raft construction projects.  This makes use of the Investment trapping of Infrastructure with 2 Ability, 1 Proficiency, and 2 Boost dice: 5 advantage, 3 successes, 0 triumphs</p>
+    <p><u><b>Economic: </b></u></p>
+    <p><i>Establishment</i> trapping of the <b>Infrastructure</b> skill to establish a settlement in the Caldera.<br/>
+        <kbd>3 advantages, 1 successes, 0 triumphs</kbd></p>
+    <p><u><b>Political: </b></u></p>
+    <p><i>Cloak and Dagger</i> trapping of the <b>Spycraft</b> skill to protect against possible external agents.<br/>
+        <kbd>0 advantages, 0 successes, 0 triumphs</kbd> ( :p )</p>
+    <p><u><b>Military: </b></u></p>
+    <p><i>Pathfinders</i> trapping of the <b>Reconnaissance</b> skill to try and scout out the abandoned town as well as look for tunnels.<br/>
+        <kbd>3 advantages, 5 successes, 0 triumphs</kbd></p>
+    <p><u><b>Mental: </b></u></p>
+    <p><i>Research</i> trapping of the <b>Academics</b> skill to try and learn more about the aspects of this territory they have landed in.<br/>
+        <kbd>0 advantages, 2 successes, 0 triumphs</kbd></p>
+    <h2>Results</h2>
+    <p><b>Economic: </b> Congratulations! The settlement of [whatever name you want] has been founded!... Sort of! More on that in a second. The trip up the river was surprisingly pleasant, for the most part. The first leg was quite nice, with all of the elves travelling together and feeling optimistic it had almost a party feel to it. In fact, you were even able to witness a River elf river bond ceremony. The dreamdust elves peeled of first, with the dark elves breaking off to head north for the Caldera a week or so later. So far so good.</p>
+    <p>Things actually get <i>interesting</i> as soon as you arrive a the former tourist attraction.</p>
+    <p>You arrive during the night, of course. The settlement is completely dark, with no torches or lamps visible. As your settlers enter the small town, it quickly becomes apparent that the town has been abandoned for some time. Entering the lodge itself, it quickly becomes apparent why.</p>
+    <p>The town is infested with skeletons - apparently of the previous inhabitants. Thankfully, you have a good sense of planning - the first elves to enter the town were soldiers. The skeletons, individually, aren’t too tough but there are quite a few of them. Your soldiers are able to clear out most of the skeletons from the town itself and make a safe place to stay in the lodge.</p>
+    <p>The lodge itself is a large, two story wooden building built into the side of a large cliff that goes right up to the shores of Crater Lake itself. Inside, the building has decayed significantly. Doors have fallen off, windows are broken, and eerie wind howls at all hours. It’s perfectly habitable once the skeletons are cleared out, though. The arcane runes painted on the wall in faded blood aren’t helping your property values though. Between the lodge itself and the surrounding bungalows and outbuildings, there’s enough room for everyone at least for now. Still, it’s a bit unnerving. The good news is that one of the attractions of this location is that the inside of the lodge contains a cave entrance! Apparently the “healing mineral waters” of some caverns containing hot springs were a tourist draw here. The bad news is the arcane runes are even more common on the cave entrances…</p>
+    <p>Regardless, your new settlement has the following statistics:</p>
+    <ul>
+        <li><b>Name:</b> The Hilt’Inn</li>
+        <li><b>Description:</b> Up to you!</li>
+        <li><b>Size:</b> Village</li>
+        <li><b>Territory Aspects:</b>
+            <ul><li><i>Crater Lake:</i> The settlement sits on the edge of crater lake, a large hot spring.</li>
+            <li><i>Mineral caves:</i> The settlement contains a cave system with warm pools, along with connections to a deeper cave system under the caldera.</li>
+            <li><i>Disturbing Danger:</i> Oh… yeah, right the skeletons and bloody arcane runes are kinda hurting that whole tourism potential.</li></ul>
+        </li>
+        <li><b>Settlement Aspects</b><ul>
+            <li><i>Old Lodge:</i> The old Crater Lake Lodge has seen better days, but her bones are good - okay, maybe not the best way to phrase that.</li>
+            <li><i>Empty Slot</i></ul>
+        </li>
+        <li><b>Garrison:</b> Currently full with the Rough Claws division.</li>
+    </ul>
+    <p>In addition, since you were able to settle directly in the abandoned town you have used only a small amount of the building supplies you brought. You may choose to use these supplies on one future Investment or Establishment check to downgrade the difficulty by one</p>
+    <p><b>Political: </b>Your spyhunters are a bit distracted by all the skeletons all over the damned place. They do their best to keep an eye out during the day for possible interlopers, but they’re so busy finding creative ways to keep skeletons from getting in the building they’ve got their hands full. The good news is they successfully keep anyone from getting hurt during the day, but you’re unsure that if there were any spies you would have noticed.</p>
+    <p><b>Military: </b>Hoo boy, those Pathfinders. First of all, their planned scouting of the surrounding areas quickly turns into a cave expedition. They’re able to map the surrounding area of the former tourist town pretty easily, but they can’t safely leave that cave entrance in the middle of town with no idea what dangers could be back there. Rigged for trouble (and with a shiny bonus die due to your Tunnel Fighting stunt) they set out.</p>
+    <p>They return late in the season, exhausted and injured but triumphant. After several months in the caves, they were able to scout a wide range of squares surrounding your settlement- in fact, all 5 of the squares of cave systems adjacent to the cave system behind the lodge. They report that the cave system indeed goes much further back, but they were only able to scout so far.</p>
+    <p>The caves are, for now, extremely dangerous. The skeletons get more dense further in. Worse, more skeleton creatures lurk in the twisted tiny side passages. Apparently the area used to be home to kobolds. More on that notes, almost the entire tunnel system is filled with ancient decaying traps both magical and practical.</p>
+    <p>Your pathfinders, however, are masters of the dark. They are able to navigate the extremely dangerous terrain and map these areas. It wasn’t easy and they had to avoid or bypass most of the traps and skeletons. The areas are still dangerous. However, they found two very interesting things in the caves.</p>
+    <p>The square to the northwest of the lodge is the likely source of the kobold skeletons. It apparently used to be a thriving kobold warren. Unfortunately for them, the warren suffered the same fate as the town. It’s now swarming with probably hundreds of kobold skeletons, as well as many of the same runes you saw in the lodge. Worse, the kobold traps are most dense here.</p>
+    <p>To the northeast they find something much, much more positive. There is a large cavern absolutely filled with crystals and volcanic glass. They bring back samples of what they can find. It contains common obsidian, as well as beautiful geodes, stunningly blue Andara volcanic glass, and a few strange types of crystal none of your people recognize. The cavern is apparently breathtakingly beautiful, if extremely dangerous to reach right now.</p>
+    <p>Speaking of, your Pathfinders are exhausted. The trek was extremely dangerous and stretched their caving skills and endurance to the limit. They suffered many injuries from traps and skeletons, but amazingly avoided any fatalities. They are still completely exhausted. Any use of Pathfinders next turn will take a <b>setback die</b>.</p>
+    <p><b>Mental: </b>While your researchers were planning on maybe examining some potential tourist locations or maybe “investigating” some hot springs… well, these bloody arcane runes are <i>probably</i> more important for the time being. Everyone’s afraid to touch them as they have no idea what they do.</p>
+    <p>You set your researchers on learning what these runes are for, and whether or not its safe to remove them. The good news is that they’re able to get a good idea on what most of these runes are! They’re mostly written in a derivative of Draconic. Unsurprisingly, they mostly concern necromancy. Shocking. The good news is that they are mostly not overtly magical and can be safely removed. It seems that whoever made them was practicing, and refining their grim craft. The bad news is that translating one of the runes in detail caused one of your researchers to start screaming in an unknowable foul language and bleeding from several orifices.</p>
+    <p>He seems OK now, after a case or so of wine, but it’s put a severe dent in the morale of your research team. Any Mental skill used next turn will take a setback die.</p></p>
+    <h3>Event - Tourism Disaster</h3>
+    <p>Needless to say, this isn’t the best situation for your plans. Take <b>one point of Morale stress</b> due to the severe situation. Thankfully, your excellent pre-planning has stopped this from being a total disaster and your people are safe for the moment. However, you will be unable to safely create any settlements inside the Caldera caves until the skeletons and traps are dealt with.</p>
 
     <?php
     include '../../settarget.php'; 
