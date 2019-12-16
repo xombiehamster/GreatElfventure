86ce5c46bf497c827a3de3c2789fb4f1940a7eda
wizzardman <58559951+wizzardman@users.noreply.github.com>
2019-12-16 10:31:54 -0600

    Update 01_2_Fire.php

--- Fire/Year/01_2_Fire.php
+++ Fire/Year/01_2_Fire.php
@@ -1,36 +1,132 @@
 <?php session_start(); ?>
 <!DOCTYPE html>
 
-<head>
-    <title>
-        River Elf Year 1 - Fall
-    </title>
-</head>
-
-<body>
-    <h1>Year 1 - Fall</h1>
-    <table style="width:100%">
-        <tr>
-            <th>Stress</th>
-            <td>Economic 0/5</td>
-            <td>Morale 0/5</td>
-            <td>Property 0/4</td>
-            <td>Military 0/5</td>
-        </tr>
-        <tr>
-            <th>Fate Points</th>
-            <td>3/3 Refresh</td>
-            <td>0 Bonus</td>
-        </tr>
-        <tr>
-            <th>Parager</th>
-            <td>(-12,8)</td>
-            <td>Goldfall River</td>
-        </tr>
-    </table>
-    <p>Staurois paces uncomfortably across the porch of the Hylidae raft.  Smilisca sits on a stump that’s been bolted to the deck boards and works on tying a complicated knot in the map braid for the Goldfall River.  Acris watches his master’s hands intently from where he sits at his feet, practicing his own knots by tying them in fishing line next to Eina with her feet dangling in the cool clear water.  Litoria stands in the shallow water next to the raft, leaning gently on her hippopotamus while she grazes.</p>
-    <p><b>Economic:</b> With Eina motivating the work efforts with the blessings of Calestros, work begins on building a lift - a “fall hopper” - made of pulleys and sturdy ropes with boulders for counterbalance.  They put to use the extra materials on hand that were left over from the original raft construction projects.  This makes use of the Investment trapping of Infrastructure with 2 Ability, 1 Proficiency, and 2 Boost dice: 5 advantage, 3 successes, 0 triumphs</p>
-
+<h2><strong>Fall, Year 1</strong></h2>
+<table>
+<tbody>
+<tr>
+<td>
+<p><strong>Stress</strong></p>
+</td>
+<td>
+<p>Economic 0/5</p>
+</td>
+<td>
+<p>Morale 1/5</p>
+</td>
+<td>
+<p>Property 0/4</p>
+</td>
+<td>
+<p>Military 0/5</p>
+</td>
+</tr>
+<tr>
+<td>
+<p><strong>Fate Points</strong></p>
+</td>
+<td>
+<p>2/2 refresh</p>
+</td>
+<td>
+<p>0 bonus</p>
+</td>
+</tr>
+</tbody>
+</table>
+<p><em>Fire Elves problems:</em></p>
+<p><strong>Event: Wolf Hunt</strong></p>
+<p>Your people are afraid, and fear makes them angry. The old monsters are back. Fire elves tend to cover up fear with rage, and this is no different. Average fire elves are taking to the street in large numbers, &ldquo;hunting&rdquo; for hidden werewolves. These groups are starting to become large and organized, and aren&rsquo;t accomplishing anything other than wrecking up local businesses looking for wolves and taking people away from vital jobs.</p>
+<p>You take one point of Morale stress from the howl. In addition starting next turn you will take one point of Economic stress damage every turn until the populace is placated.</p>
+<p><strong>Economic Action</strong>:&nbsp;</p>
+<p>Infrastructure (Investment): Green Flame [the Hunger]</p>
+<p><em>Includes Bonus Die from Mental action (Summer Year 1)&nbsp;</em></p>
+<p><em>Invoking Surt Territory Aspect Sulphur [Free] [Reroll]</em></p>
+<p>As evidenced by the local terrain, ash-heavy soil is not effective farmland. However, as the Hunger knows from his own research, there are a lot of components within ash -- nitrogen, methane, sulfur&hellip; -- that are known to increase plant growth with proper controlled exposure. And while immediately accessible water isn&rsquo;t really available from ground level, the mountains and geysers provide plenty of potential for unusually sourced aqua. So why not try something unusual?</p>
+<p>Coordinating with his architects, the Hunger proposes an &lsquo;aqua via&rsquo; -- an addition to the existing walls and structures that will function as an irrigation system to stacked plants and gardens built directly onto the roofs of local buildings. The water would be sourced from both mountain runoff and nearby geysers, and filtered via fresh, low-sulfur ash. Combined with fertilizers enhanced with the local ash, this should result in a permanent source of water and farmland that can expand along with the city.</p>
+<p><a href="https://dev71687.service-now.com/nav_to.do?uri=x_339494_mdalpha_rolls.do?sys_id=31217364db4d3300e7a0a08a4896190e">Roll</a>: 3 advantage, 1 successes, 0 triumphs | Reroll: 4 advantage, 2 successes, 0 triumphs</p>
+<p><strong>Political Action</strong>:&nbsp;</p>
+<p>Recovery (Leadership): Swords and Plowshares [Ashwalker Auburn Tsun]</p>
+<p><em>Counter to event Wolf Hunt</em></p>
+<p><em>Invoking aspect: Birth Pyre for +1 Prof die [FP -1]</em></p>
+<p>Eager to cement the political strength of the Ashen after a failure to establish a new Ashkin militia, Ashwalker Auburn Tsun seizes the opportunity provided by the fear and paranoia now coursing through the streets of Surt. &ldquo;We have faced harsher winds than this and not been dimmed,&rdquo; she said, in a speech in the Agora at sunrise. &ldquo;This is not the first time Lycans have prowled our streets, and as last time, we will not sit back and let our bravest warriors fight alone. We did not defeat the Empire by prowling the streets at night and hunting our neighbors; we beat them with organization, with tactics, and above all with preparation. No Fire Elf alone can stop this wolf. If we are to keep them out, we must fortify. We must build infrastructure. We must build armies. And above all, we must entrench our hearts and minds. We cannot and must not let down our guard after a mere moment&rsquo;s peace.&rdquo;</p>
+<p>The Ashen push for a series of public works projects, designed to help establish and fortify the city against future incursions. Rather than pushing for individual military units, they instead aim to support rapid response militias -- providing an outlet for the anger and fear of the common citizen, and giving them incentive to build and think in military terms.&nbsp;</p>
+<p><a href="https://dev71687.service-now.com/nav_to.do?uri=x_339494_mdalpha_rolls.do?sys_id=10c7ff28db4d3300e7a0a08a4896195b">Roll</a>: 2 advantage, 4 successes, 1 triumphs</p>
+<p><strong>Military Action:</strong></p>
+<p>Reconnaissance (Pathfinders): Mountains West [The Fury]</p>
+<p>While the Terror and the Hunger were facing Surt&rsquo;s newest threat, the Fury was coordinating with a team of expert Fire Elf Pathfinders to explore the mountains directly to the west of the village (i.e. the square to the immediate west of Surt, and the square north of that -- a two-square Recon check).</p>
+<p><a href="https://dev71687.service-now.com/nav_to.do?uri=x_339494_mdalpha_rolls.do?sys_id=3c6fff6cdb4d3300e7a0a08a48961996">Roll</a>: 0 advantage, 1 success</p>
+<p><strong>Mental Action:</strong></p>
+<p>Academics (Research): Mineralogical Survey [Phoenix Lord Barrabus Leafstorm]</p>
+<p>Now that Surt has an established presence in the Burning Maar, the more economically-minded Fire Elves are eager to identify what mineral riches the Maar can provide. Barrabus is called forth by the Phoenix Imperialis, and charges the geologists and academics of the Fire Elves with identifying what mineral aspects are likely within the Burning Maar, and what potential the Sulfur of Surt provides.</p>
+<p><a href="https://dev71687.service-now.com/nav_to.do?uri=x_339494_mdalpha_rolls.do?sys_id=db747364db4d3300e7a0a08a489619b3">Roll</a>: 1 advantage, 2 successes</p>
+<p><strong>End of Turn&nbsp;</strong></p>
+<h3>Fall Year 1 Results</h3>
+<p><strong>Results-</strong></p>
+<p><strong>Economic- </strong>Despite the slowly increasing chaos in the city, the urban planning committee is dedicated to the potential crisis they are trying to resolve. Thankfully, the committee appears to have been burned down to those who are fully devoted to the long-term goal of making Surt a sustainable location. Even despite the&hellip; <em>rumors</em> about the Hunger&rsquo;s encounter with the werewolf. Indeed, the Hunger throws himself into this work completely, and doing so seems to ease his mind and allow him to put the experience behind him. Mostly, anyway.</p>
+<p>The plans for the Aqua Via are drafted, and the system begins to be constructed. The Hunger has some truly lofty plans for the system, but for the moment only the basics are worked out. The Hunger&rsquo;s basic knowledge of alchemy seems to work out, however- one of the most important components of the system, the filtration, seems to work. Research reveals that the waters of the Burning Maar are mildly toxic. Not enough that brief exposure is a concern, but decades of exposure could be dangerous even to the fire elves.</p>
+<p>The Aqua Via is constructed. Two large waterways connect both the maar itself and runoff from the foothills into Surt. The rooftop gardens seem to be struggling despite the addition of Sulphur, but the water is potable and the system actually seems to cool the buildings that it is placed in. Even fire elves can get heat exhaustion, after all.</p>
+<p>Surt gains the Aspect <strong>Aqua Via</strong> in its second slot. It&rsquo;s a primitive system for the moment, but it works and by the committee&rsquo;s estimate is a big step on the road to Surt becoming sustainable.</p>
+<ul>
+<li><strong>Aqua Via: Fresh potable water is brought into Surt through a long system of aqueducts, along with a clever ash filtration system.</strong></li>
+</ul>
+<h2><br />Furthermore, the Hunger&rsquo;s tireless research and development on the project means that the next <strong>Enrichment </strong>check to improve the Aqua Via will receive an <strong>Upgrade.</strong></h2>
+<p><strong>Political</strong></p>
+<p>The Ashwalker&rsquo;s speech is a triumphant success. Her speech at the Agora becomes the stuff of legend, perhaps repeated and analyzed generations later. The cheer it produces in the crowd is evidence enough for now, as well as the result.</p>
+<p>The Ashwalker&rsquo;s idea for a rapid response militia gets off the ground rapidly. Unlike last time, there is no shortage for volunteers. The lines for signing up extend into the streets. In a few days time, the <strong>Civile Praesidium</strong> is formed.</p>
+<p>The Praesidium, as Tsun imagined, is not a military unit. Members of the Praesidium are still involved in their civilian jobs. True to her plans, the Praesidium becomes an outlet for the rage of the civilian population. By channeling their energy with set schedules, orders, and command structures the Ashwalkers are able to turn the wolf hunting parties into something actually constructive. The Praesidium becomes a supplemental force able to be called up in times of need. They aren&rsquo;t soldiers, but they make good town guards and can be called up in an instant.</p>
+<p>To say that they are loyal to the Ashwalker is&hellip; an incomplete statement. Their command structure is tied to the fire elves&rsquo; normal military, but the Ashwalker gains an enormous amount of public support from their formation and receives much of the public goodwill for &ldquo;responding to the crisis&rdquo; while the Hunger is locked in his office and the Terror is well&hellip; the Terror. The Triumvirate is far from overthrown, but the Ashwalker is clearly on the rise.</p>
+<p>Any attempts to recruit an Ashwalker military unit next turn get a <strong>bonus die.</strong></p>
+<p>In addition, add the Cultural Aspect <strong>Civile Praesidium</strong> to the fire elves. This is a system that will be copied in basically any settlement the fire elves have, and as such is not a settlement or terrain aspect - and perhaps not completely under your control.</p>
+<p><strong>Military</strong></p>
+<p>Your Pathfinders return from the hills as scheduled, but they have had casualties. They were able to map the targeted areas. They don&rsquo;t find anything wildly exciting, but it seems these mountains are rich in common minerals such as iron, tin, zinc, and a few other mundane materials. While none of these are huge veins, mining is possible in any of the mountain squares. It is also quite promising of greater mineral riches further into the mountains.</p>
+<p>That&rsquo;s where the problems begin. Apparently, there was a rockslide during their return journey and several experienced Pathfinders were killed. It was a freak accident that no one could have anticipated, but the damage to the morale and the institutional experience of your Pathfinders is sharp.</p>
+<p>Any use of your Pathfinders next turn will be done with <strong>difficulty upgraded</strong>, as they are scrambling to replace experienced leadership.</p>
+<p><strong>Mental</strong></p>
+<p>The survey of the mineral wealth of the burning maar is interesting, to say the least. First of all, Sulphur itself isn&rsquo;t terribly valuable but when used carefully it can have many purposes. Indeed it can be a fertilizer but simply mixing it up as plant food isn&rsquo;t enough as in its raw state it does nothing for plants, but composting it with rich soil can make it far more nourishing.</p>
+<p>Perhaps more exciting to the fire elves, Suplhur is extremely flammable. It is used in quite a few alchemical compounds that burn, including alchemist&rsquo;s fire, and can also be used to make acids. Suplhur can also produce different color flames when it burns, depending on what it's mixed with, and could be useful for decoration.</p>
+<p>As far as other minerals, the geologists find that the Maar contains many minerals and salts common to volcanic areas. None in such abundance as the sulphur, but many of them could be valuable as spell components or in alchemy.</p>
+<p>Speaking of Sulphur being flammable&hellip; An experiment determining the flammability of Sulphur returns with the results of &ldquo;considerably more than we thought.&rdquo; Perhaps ironically, a fire breaks out in one of the geologist&rsquo;s central research laboratories - which at this point is his home. House fires aren&rsquo;t normally a problem for fire elves, but unfortunately they <em>are </em>a problem for delicate lab equipment, books, notes, and isolated samples.</p>
+<p>At this point they have to start over again and go collect more samples. This won&rsquo;t be a problem in the long term, but research checks next turn relating to minerals take a <strong>setback die.</strong></p>
+<p><strong>Event Resolved: Wolf Hunt</strong></p>
+<p>Ashwalker Tsun has essentially single handedly solved the crisis with the creation of the Civile Praesidium. Nearly overnight, the roving mobs of angry elves become an organized force. Over the weeks with no sign of werewolves, people calm down.</p>
+<p>Due to her rapid and effective response, Tsun is the one remembered for resolving this crisis. The fact that most of the Triumvirate were not seen directly addressing the issue is not lost on many. The Ashwalker political block grows in influence.</p>
+<p>The good news is that Tsun resolved the issue so rapidly that you do <em>not</em> take an economic stress this turn. Further, with the event resolved, your stress points will start recovering naturally next turn.</p>
+<p>Due to the creation of the Praesidium and Tsun&rsquo;s rousing leadership, select one of the following skills for a <strong>permanent upgrade</strong>:<strong> Recovery, Loyalty, or </strong><strong>Conventional Warfare</strong></p>
+<h2><strong>Status at start of Winter, Year 1</strong></h2>
+<table>
+<tbody>
+<tr>
+<td>
+<p><strong>Stress</strong></p>
+</td>
+<td>
+<p>Economic 0/5</p>
+</td>
+<td>
+<p>Morale 0/5</p>
+</td>
+<td>
+<p>Property 0/4</p>
+</td>
+<td>
+<p>Military 0/5</p>
+</td>
+</tr>
+<tr>
+<td>
+<p><strong>Fate Points</strong></p>
+</td>
+<td>
+<p>1/2 refresh</p>
+</td>
+<td>
+<p>? bonus</p>
+</td>
+</tr>
+</tbody>
+</table>
+<p>Settlement Surt has gained aspect <em>Aqua Via</em> in its 2nd slot</p>
     <?php
     include '../../settarget.php'; 
     settarget("Fire",3,null,null);
