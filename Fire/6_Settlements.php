2c2559f3a72ee20d096a5e3094bfa272390f54c4
wizzardman <58559951+wizzardman@users.noreply.github.com>
2019-12-18 13:08:06 -0600

    Update 6_Settlements.php

--- Fire/6_Settlements.php
+++ Fire/6_Settlements.php
@@ -15,18 +15,16 @@
 <p><strong>Description:</strong> The &ldquo;City of Bronze&rdquo; (appellate pending). Surt is the capital town of the Fire Elves, located in the northern corner of the Burning Maar, in a valley just lined on three sides by mountain peaks. Scattered with colorful decorations, alchemical fires, music, and its famous rooftop gardens, Surt is the center of Fire Elf culture.</p>
 <p><strong>Size: </strong>Town</p>
 <p><strong>Misc:</strong>&nbsp;</p>
-<ul>
+<ul><ul>
 <li>Post Office (in palace)</li>
-</ul>
+    </ul></ul>
 <p><strong>Territory Aspects:&nbsp;</strong></p>
-<ul>
 <ul>
 <ul>
 <li><strong>Burning Maar</strong><em>: Surt exists on the Burning Maar, a wide stretch of extremely volcanically active badlands.</em></li>
 <li><strong>Sulphur</strong><strong><em>:</em></strong><em> The volcanic terrain around Surt is a good source of sulphur</em></li>
 </ul>
 <ul>
-<li><em>First Invoked Fall Year 1</em></li>
 </ul>
 <ul>
 <li><strong>All Roads lead to Surt</strong><strong>:</strong> Clever road designed has insured easy connections between Surt and its adjacent settlements; all settlements within 3 pathfound squares of Surt share this aspect.</li>
@@ -42,11 +40,9 @@
 <p><strong>Settlement Aspects:</strong></p>
 <ul>
 <ol>
-<ol>
 <li><strong>Defensive Wall:</strong> Surt is currently surrounded by a defensive wall. It&rsquo;s a very simple structure, but still quite functional.</li>
 <li><strong>Verdant Rooftop Gardens:</strong> Vineyards, cropland, and parks, built into the very rooftops of Surt&rsquo;s buildings, providing a regular supply of produce to Surt&rsquo;s citizenry. <em>Once per year, provides 1 bonus die to a check relating to food or plant life</em>.</li>
 <li><strong>Fresh Mint -- Curiously Alchemical: </strong>A standard mint, primarily focused around gold coin production, featuring an unusually red gold medium (with some alchemical components added in) and some notoriously typoed coinage. The currency produced is very hard to counterfeit, and can be used in spellcasting and magic item creation.</li>
-</ol>
 </ol>
 </ul>
 
@@ -57,27 +53,25 @@
 <p><strong>Size: </strong>Village</p>
 <p><strong>Territory Aspects:&nbsp;</strong></p>
 <ul>
-<ul>
 <li><strong>Volcanic Badlands: </strong>The terrain around Gedr is the same as the rest of the dangerous, unstable terrain around the Maar.</li>
 <li><strong>Fortified Cove: </strong>Gerdr exists atop a very large slab of granite, surrounded by several granite ridges that extend into the Maar itself. This makes the ground that Gerdr is built on a bit more stable than the rest of the Maar, as well as providing some amount of shelter from the harsh air and water in the area. The cove has been surrounded by Gerdr&rsquo;s defensive wall, providing a safe area large enough to park a River Elf flotilla that can still be sealed off with heavy gates.</li>
-</ul>
-<ul>
+
 <li><strong>All Roads lead to Surt</strong><strong>:</strong> Clever road designed has insured easy connections between Surt and its adjacent settlements; all settlements within 3 pathfound squares of Surt share this aspect.</li>
 </ul>
-<ul>
 <li><strong>[Civ Aspect] Fortified Roadways:</strong> Guardposts, bunkers, and defensive emplacements along the roadways help defend all traffic from bandits, animals, and raiders. This provides 1 automatic setback die to any enemy attempting to attack Surtian infrastructure in territories with this aspect. Settlements connected by <strong>All Roads Lead to Surt</strong> and with a defensive aspect of their own automatically gain this aspect. This counts as All Roads Lead to Surt for invocation purposes.</li>
+</ul>
 </ul>
 <p><strong>Settlement Aspects:</strong></p>
 <ol>
 <li><strong>Defensive Wall:</strong> Gerdr is surrounded by a defensive wall, based around the settlement&rsquo;s granite foundation, and encircling the cove</li>
 <li><strong>Empty</strong></li>
 </ol>
+    
 <h2><strong>Disr</strong></h2>
 <p><strong>Name:</strong> Disr</p>
 <p><strong>Description:</strong> Named for a mythological, flying guardian of the dead, Disr is Surt&rsquo;s first &ldquo;mountainous&rdquo; village, built on the side of a mountain locally called &ldquo;Pegasus Back&rdquo;. Significant architectural effort has been put in to provide a stable foundation for the village&rsquo;s buildings, as well as the main thoroughfare that connects to the main Surtian road, which connects to ground level through a series of switchbacks arcing around the mountain&rsquo;s foot. Most of the buildings are built from the mountain&rsquo;s granite. Some are even carved directly into the mountain itself.</p>
 <p><strong>Size: </strong>Village</p>
 <p><strong>Territory Aspects:&nbsp;</strong></p>
-<ul>
 <ul>
 <li><strong>Gold Mine: </strong>Literally a gold mine. Nice!</li>
 <li><strong>Low Mountains: </strong>Disr is located just above the foothills, nestled in the lowest part of the northern mountain range that can actually be called &ldquo;mountains.&rdquo;</li>
@@ -87,7 +81,6 @@
 </ul>
 <ul>
 <li><strong>[Civ Aspect] Fortified Roadways:</strong> <em>Guardposts, bunkers, and defensive emplacements along the roadways help defend all traffic from bandits, animals, and raiders. This provides 1 automatic setback die to any enemy attempting to attack Surtian infrastructure in territories with this aspect. Settlements connected by </em><strong><em>All Roads Lead to Surt</em></strong><em> and with a defensive aspect of their own automatically gain this aspect. This counts as All Roads Lead to Surt for invocation purposes.</em></li>
-</ul>
 </ul>
 <p><strong>Settlement Aspects:</strong></p>
 <ol>
