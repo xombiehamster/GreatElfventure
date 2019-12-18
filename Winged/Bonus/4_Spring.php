0eae25779efa9d0e09c5021a42d65f79d971be8a
xombiehamster <58495678+xombiehamster@users.noreply.github.com>
2019-12-18 09:48:19 -0600

    Update 4_Spring.php

--- Winged/Bonus/4_Spring.php
+++ Winged/Bonus/4_Spring.php
@@ -9,7 +9,85 @@
 </head>
 
 <body>
-<p>A Placeholder</p>
+<h1>Spring Year 1: Turn 4</h1>
+
+<p>“You’re nervous; your feathers are fluffed.”</p>
+
+<p>Shadimon resettled his wings and grimaced. The table in front of him was scattered with small slates and waxed tablets, reports from the scouts and Harrier patrols. “On edge, yes. We haven’t found anyone else in this area at all.”</p>
+
+<p>Baijani sat down across from him, “And that worries you.”</p>
+
+<p>“We haven’t been exactly quiet and stealthy here. Anyone in the area should have come to see what’s going on.”</p>
+
+<p>“And we haven’t seen any such thing.”</p>
+
+<p>Shadimon squinted at her, “No, which is what worries me.”</p>
+
+<p>“Maybe there’s no one here anymore.” She raised her eyebrows at his scowl, “But that’s too easy.”</p>
+
+<p>“And we didn’t just survive the most vicious war in recorded history to rely on ‘maybe’. But we aren’t finding anything.”</p>
+
+<p>Baijani tilted her head, “Have you considered a magic approach? A large-scale ward around our area of the canyon, further if we can make it work.”</p>
+
+<p>“That would interfere with us, wouldn’t it?”</p>
+
+<p>“No, not a barricade. Just a spider web, say. Set it to detect people other than winged elves, maybe. But we have our three dreamdust elves, so it would be going off all the time.” She picked up one of the blank tablets and a stylus, “We’re looking for people who shouldn’t be here. But how do we define that? Do we care if people are just looking? Curious?” She looked up, “You’re the security head.”</p>
+
+<p>Shadimon drummed his fingers on the table, “No, we don’t have a reason to discourage any locals yet. Those are still potential allies or trade opportunities, and not automatically threatening. How specific can this kind of ward be? Can you set it to detect...I don’t know, intent?”</p>
+
+<p>Baijani rolled the stylus through her fingers, “Possibly. This kind of scale isn’t my area of expertise. I’ll have to check with our other mages. What do you have in mind?”</p>
+
+<p>“Malicious intent. Anyone, or thing I suppose, meaning harm to the village or any of our people.”</p>
+
+<p>She jotted a few scribbled notes on the tablet, “I’ll start the idea through the gossip mill and see what shakes out.</p>
+
+<hr/>
+
+<p>While establishing a cliffside garden was being difficult, they were all pleased at the progress the researchers had made on extracting and working copper. Shadimon turned over one of the pieces they had presented before passing it to Baijani. “That simple? Truly?”</p>
+
+<p>“I know! We were surprised too! But the copper here appears to be metallic, and all you have to do is chip it out of the rock. We have a good sized basket of bits that we’ve gotten out while testing tools.” The lead researcher brandished a sheaf of papers, “And we found records of many possible uses for it, and ways of treating and working it. It’ll take time to develop the veins into anything resembling an actual mine,” his wings twitched, “And well...being far underground isn’t exactly comfortable for us. We might need help to really explore what we have here.”</p>
+
+<p>Varna shivered dramatically, “Not for me, no thank you!”</p>
+
+<p>Baijani handed the piece of raw copper back to Shadimon, “But there’s enough near the surface that we can work with for now?”</p>
+
+<p>“Oh certainly!” He dipped his wings to Shadimon, “It’ll take some time to build up any kind of stock, but if you want copper as a trade item, we can have it any time.”</p>
+
+<p>Shadimon studied the piece of copper in his hands, tracing the irregular shapes. “Satri, how long is it until Year’s End?”</p>
+
+<p>“About two months. A little less.”</p>
+
+<p>He nodded, “We’ll be returning to the Fort for Year’s End. I’d like to set aside some of the copper to make tokens for the others. I don’t know what exactly…” He trailed off as Anasatri nearly bounced off of her stool. “Yes?”</p>
+
+<p>“I do! Oh and I know exactly who to talk to! I’ll take care of it, Shadi, and it’ll be ready to go when it’s time to leave!” She leaped up and darted out the door.</p>
+
+<p>“Well, one thing off my plate then,” Shadimon said. “Now about this ward Jani proposed…”</p>
+
+<hr/>
+
+<p>Shadimon stared at the copper feather sitting on the table. It was the size of a dagger and lovingly detailed, from the individual fibers to the sculpted puff of down at the base. Two polished beads had been fitted onto the shaft, one blue, one white. He went to pick it up and paused, looking at the play of light on one edge, “Is this <i>sharp?</i>”</p>
+
+<p>The artist grinned wider, “It is. We thought that was a good symbol.”</p>
+
+<p>“Goddess is it.” He picked it up, and noticed a small string of tiny metal beads. “What’s this?”?</p>
+
+<p>“For attaching a real feather.”</p>
+
+<p>“Ahh,” Shadimon drew the sound out, running the short chain through his fingers. “Yes, yes that’s important. This is tremendous. You’re up for doing more?”</p>
+
+<p>She folded her arms and grinned wider. “I already have the next couple started. That one was a successful prototype, and is meant to be ours,” Shadimon glanced up as she continued, “Wouldn’t be right to give them to everyone else and not have one ourselves.” She laughed, “And it was easier to practice, knowing it wasn’t meant as a gift.</p>
+
+<p>“Satri and I have been discussing ideas, but I also wanted to talk with you. Make sure I’m doing the right number, and see if you have any ideas on what might be important to the others.”</p>
+
+<p>Shadimon picked up a tablet and stylus, “Let’s see...us, the blade elves, the fire elves, dark elves, dreamdust elves, the cryptid elves, river elves, ah...oh, totem elves. I believe that’s everyone who stayed. So that’s eight.” He tapped the stylus, leaving small impressions in the wax, “I don’t know that I’d know more than anyone else does. Though...for the blade elves. One of the beads should be taken from a piece of armor, or a weapon. That’s how they remember people.” He grimaced, “Your guess is as good as mine for the cryptid elves.”</p>
+
+<p>“Yes, that one might be, ah, generic. But black and white goes well with anything.”</p>
+
+<p>“Maybe a cloth or leather wrapping, too. That’s what they seem to wear most often.”</p>
+
+<p>“Ohh, good idea…”</p>
+
+<p>They talked for a little longer about colors and styles, and the artist cheerfully returned to her makeshift studio, leaving the finished feather with Shadimon. He tilted one wing forward and ran his hand through the smaller covert feathers until he found one that was ready to come out. Plucking the feather free, he clipped it to the little chain and smoothed out the fibers. Lacking anything better for now, he set it in the center of the table. It could be hung up later to mark whatever the official town center ended up being.</p>
       <?php
     #Update bonus sequence
     include '../../settarget.php'; 
