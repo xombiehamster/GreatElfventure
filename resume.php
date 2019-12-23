a7e390f08f1b03b6bd73a644ee8652caf81d2bec
xombiehamster <58495678+xombiehamster@users.noreply.github.com>
2019-12-23 08:40:11 -0600

    Update resume.php

--- resume.php
+++ resume.php
@@ -1,15 +1,18 @@
-<?php session_start(); 
-function numfix($var){
-    if(is_numeric($var)){
-        if($var<0){
-            return null;
-        }else{
-            return $var;
-        }
-    }else {
-        return null;
+<?php session_start();
+include 'footer.php';
+$cookiename = "savestate";
+$curr = getstatus();
+if(!isset($_COOKIE[$cookiename])) {
+    setcookie($cookiename, $curr, time() + 3600, '/');
+}else{
+    if(statuscomp($_COOKIE[$cookiename],$curr)<0){
+        setcookie($cookiename,$curr,time() + 3600, '/');
+    }else{
+        setstatus($_COOKIE[$cookiename]);
     }
 }
+}
+
 ?>
 <!DOCTYPE html>
 
@@ -21,24 +24,28 @@ function numfix($var){
 </head>
 
 <body>
+    <p>Cookie saved status:
+        <php?
+              if(!isset($_COOKIE[$cookiename])) {
+                echo $cookie_name.' is not set';
+               } else {
+                    echo $_COOKIE[$cookiename];
+              }
+              ?></p>
+    <p>This page uses a cookie to save your current status.  You can override it using the form below.</p<
 <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
   Status: <input type="text" name="fname">
   <input type="submit">
 </form>
 
 <?php
-include 'footer.php';
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // collect value of input field
     $name = htmlspecialchars($_REQUEST['fname']);
     if (empty($name)) {
         echo "You have to enter something for this to work.";
     } else {
-        $arr = explode(" ",$name);
-        $ind = 0;
-        while ($ind<22){
-            $_SESSION[elford($ind)] = numfix($arr[$ind]);
-            $ind++;
+        setstatus($name);
         }
     }
 }
