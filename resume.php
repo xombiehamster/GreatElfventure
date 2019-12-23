a9bfd28ecd1c49a83b4f64128295085f9fcb91d2
xombiehamster <58495678+xombiehamster@users.noreply.github.com>
2019-12-23 13:39:58 -0600

    Update resume.php

--- resume.php
+++ resume.php
@@ -1,8 +1,6 @@
 <?php session_start();
 include 'footer.php';
 
-ini_set('display_errors', on);
-
 $cookiename = "savestate";
 $curr = getstatus();
 if(!isset($_COOKIE[$cookiename])) {
@@ -14,7 +12,7 @@ if(!isset($_COOKIE[$cookiename])) {
         setstatus($_COOKIE[$cookiename]);
     }
 }
-}
+
 
 ?>
 <!DOCTYPE html>
