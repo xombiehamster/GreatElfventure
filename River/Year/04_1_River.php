9076c671efa442662ba4b59f75ec83f90d64463f
xombiehamster <58495678+xombiehamster@users.noreply.github.com>
2019-12-23 21:30:41 -0600

    PLACEHOLDERS!

---
+++ River/Year/04_1_River.php
@@ -0,0 +1,96 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+
+<head>
+    <link rel="stylesheet" type="text/css" href="../../style.css">
+    <title>
+        River Elf Year 3 - Summer
+    </title>
+</head>
+
+<body>
+    <h1>Year 3 - Summer</h1>
+    <h2>Roleplay Event 2.0: <a href="../../Events/02_0.php">A New Year</a></h2>
+    <p>The Hylidae Family takes a vacation to Fort Alfyr for the traditional Elven New Year Potluck.  Litoria, Smilisca, and the apprentice, Acris ride downstream to the coast at the swiftest pace Sauver can manage.  They arrive just the day before the planned Potluck.  Smilisca immediately vanishes into the fort to find his beloved, while Litoria and Acris prep crabs for dinner.  They bring with them a selection of spices from the exotic terrain they’ve passed through.</p>
+    <p>At the Potluck, Litoria finds herself speaking up in defense of the <i>werewolf</i> Bryti, and keeping her confidences private.  It feels wrong to speak in defense of the enemy, but at the same time, her statements are true.  The enemy was the Lycan Empire, not its individual people.  And that enemy is dead.  The enemy remaining is the one who pretended to kindness in keeping the River Elves safely rebuilding his castle in the chaotic aftermath of Skaplyndi.  Litoria will not forget this.</p>
+    <p>Smilisca was able to find ample time to spend with his beaux, Terje, both before the potluck, and in Terje’s spontaneously teleported visit to Partager afterwards.  The relationship is starting to feel like a different kind of raft bond.  They’re together when they can be, and if that’s not always, that’s still okay.  Smilisca made a decent amount of money betting on the outcome of the <a href="../../Events/02_0b.php">Crag vs. Fire rap battle</a>, so it was overall a great time for him.</p>
+    <p>The evening of the Inknapping, Smilisca and Terje spent the evening at the river elves New Year bonfire celebrations, dancing and having a great time together.  Enthusiastic river elves even invited Ink to join the dance party, though they did not pressure him to get too close to the fire.  In the morning, the hosts offer Ink a cup of honey and lemon tea for that raspy throat of his, since it sounds very sore.  Ink stares at the tea for a few moments, before replying that though he cannot drink tea he likes the smell. He places the tea right next to his work area and spends the next couple of days enchanting in silence. He thanks you before he leaves, and tells Ny he is happy for her. It’s hard to tell, but you think he had a good time.</p>
+    <p>Acris, however, managed to get into Smilisca’s spellbook while he was away, and now thinks he really does know a spell.  He requested writing materials from Anasatri and with her help and harsh handwriting critique has begun his very own spellbook.  His goal of becoming a Real Wizard is one step closer.</p>
+    <h3>Litoria’s List of Questions for the Stranger</h3>
+    <ul>
+        <li>
+            Are you really a werewolf?
+        </li>
+        <li>
+            Why do you carry that sword?
+        </li>
+        <li>
+            Do you consider yourself an Elf, really?
+        </li>
+        <li>
+            Have you been killing humans and carving runes on them?
+        </li>
+        <li>
+            Where do you come from?
+        </li>
+        <li>
+            Why did you come here?
+        </li>
+        <li>
+            How can I help?
+        </li>
+    </ul>
+    <br>
+    <table style="width:100%">
+        <tr>
+            <th>Stress</th>
+            <td>Economic 0/5</td>
+            <td>Morale 0/5</td>
+            <td>Property 0/5</td>
+            <td>Military 0/6</td>
+        </tr>
+        <tr>
+            <th>Fate Points</th>
+            <td>3/3 Refresh</td>
+            <td>0 Bonus</td>
+        </tr>
+        <tr>
+            <th>Parager</th>
+            <td>(-12,11)</td>
+            <td>The Burning Maar</td>
+        </tr>
+    </table>
+    <p>Litoria and Sauver had previously planned to stay behind on the journey back to Partager, but with Smilisca having gone ahead, she can’t leave Acris to try and find it on his own.  Separated from his Master, the Apprentice becomes enmeshed with the Pathfinders they meet at the chosen location for Winter Quarters.</p>
+    <p>Partager, with Smilisca there to direct, manages to avoid the Crag Elf danger zone.  While traversing the swampy terrain, they keep an eye out for any signs that the Totem Elves had been in the area.</p>
+    <h3 id="jump">Roleplay Event 2.1.1: <a href="../Bonus/1_A Hitchhiker in Partager.php">A Hitchhiker in Partager</a></h3>
+    <p>Ink, returning with Smilisca’s Spellbook, also comes with Barrabus in tow.  Barrabus, of course, is absolutely adamant that Ink just randomly kidnapped him. Ink comments little on this before teleporting away, leaving poor Barry apparently stranded.  During his stay, Barrabus learns of Ny’s pregnancy and promises to keep it a secret for the time being.</p>
+    <p><b>Economic:</b> Some quick thinking and significant effort drive Smilisca and Staurois to reorganize the hauling capacity of the flotilla to accept the trade goods given in exchange by the Crag Elves for what food they can spare.  Doing this work without the effort of actually delivering their cargo seems odd, but they respect the Crag Elves’ privacy by meeting at the requested drop point and no closer.  Due to the dire circumstances of this request, they delay the construction of Winter Quarters to aid their fellow elves.</p>
+    <p>This uses the <i>Merchants</i> trapping of the <b>Trade</b> skill, and their shiny new stunt: Elf Express, and they roll 2 Ability and 1 Proficiency die: 3 advantage, 0 successes, 0 triumphs  Due to the dire nature of this need, they spend a Fate point to give that another try, and get: <kbd>2 advantage, 3 successes, 0 triumphs</kbd></p>
+    <p>The delay causes some grumbling among the Pathfinders who have been waiting on the coast.  They had hoped to be able to start putting down roots by now, and feel a bit bitter about coming to the aid of a group of people who had previously abandoned their fellow Elves in the Elven diaspora.</p>
+    <p><b>Political:</b> On edge after having heard from Surt about the camp full of massacred humans with strange Lycan runes carved into them, the Eyes in the Dark take extra precautions in these dense jungles.  Swamps are familiar, but jungles are strange and unknown terrain.  They’re on the lookout for Totem Elves, or whatever ate the Totem Elves.  This uses the <i>Cloak and Dagger</i> trapping of <b>Spycraft</b> and rolls 1 Ability die: <kbd>0 advantage, 1 successes, 0 triumphs</kbd></p>
+    <p><b>Military: </b>To work out some of their bitterness and keep them distracted, Litoria asks the pathfinders to scout the terrain to the North of the designated location for Winter Quarters.  This is a good distraction for her too.  The Crag Elves’ sitting in their homeland, comfortable or not, is a bitter pill to swallow when so much vast, uncrossable desert lies between herself and hers.  On Fort Alfyr’s map, they scout (-3, 4) and (-3, 3).  This uses the <i>Pathfinders</i> trapping of <b>Reconnaissance</b> with 2 Ability dice: 1 advantage, 1 successes, 0 triumphs</p>
+    <p><b>Mental:</b> Despite remembering her time as a young acolyte fondly, Eina thinks that if Calestros calls adults to the Service, she’s certainly willing to have their help.  But it’s an unusual situation to be developing a priesthood basically from scratch.  Eina wants to take into account the life experiences of the new volunteers and not treat them as children entering Calestros’s service in their youth as she did.  She opts to research how other elves handle religious devotion to see if there are any existing solutions to the question that they could adapt to their own uses.</p>
+    <p>With help from those who will be most affected by this, she uses the <i>Research</i> trapping of <b>Spirituality</b> with 1 Proficiency die: <kbd>0 advantage, 2 successes, 0 triumphs</kbd> + a Setback die to be rolled by GM.</p>
+    <h3 id="jump2">Roleplay Event 2.1.2: <a href="../Bonus/2_A Special Delivery.php">A Special Delivery</a></h3>
+    <h2>Results</h2>
+    <p><b>Economic:</b> <i>GOAL REWARD: Blood in the Elven body.</i> Due to the river elves’ absolute devotion to serving as the lifeblood of this new elven land, even at the cost of their own plans, the difficulty of this trade check has gone down by one.</p>
+    <p>Oh, those poor poor crag elves. They have managed to scrounge up some amount of iron ore and supplies, but it’s quite obvious it’s wildly unorganized. Different qualities and types of ore, all staggered around haphazardly with no measurements of type or kind, no real way to tell exactly what they have. And they’re so stiff, so completely unsure of how to handle themselves during a little friendly bartering.</p>
+    <p>Thankfully for them, you are the river elves. You are the blood in the elven body. This is what you are for. The river elves devote all of their skill and experience in trading to guide your new cousins through the process of correctly managing a trade. Some otherelf might have taken advantage of their inexperience and desperate situation. Not you. At least not much.</p>
+    <p>You have successfully traded your food shipment for the iron shipment that they have available, such as it is.</p>
+    <p><b>Political:</b> The Eyes take extra watches ‘round the clock after the events of the last season. Despite their patrols, they find no sign of any spies or interlopers.</p>
+    <p>No signs of any kind of wolves or unexpected guests. For some reason, Litoria feels slightly disappointed. She is not sure why.</p>
+    <p><b>Military:</b> The pathfinders are only able to scout one square, the one directly north of where winter quarters will be. The delay of the founding of Winter Quarters seems to have hurt the Pathfinders morale a bit. They report numerous unscheduled problems and delays.</p>
+    <p>The square north of the mangrove forest has the Tidal Marsh, Wetlands,  and River aspects. It’s a little more brackish than the wetlands around the mangrove forest, but it’s easily traversable to a flotilla.</p>
+    <p><b>Mental:</b>  Eina throws herself into forming her own priesthood. The budding clerics she has are an interesting group. They are not all from this flotilla originally. Many of them literally lost everything in the war, being the last of their families and the only ones who carry on their own family’s own religious beliefs and traditions. Eina asks them about all of them. She treats them as adults bringing her desperately needed knowledge, not children to be trained. When she’s done speaking to them, she looks at the breadth of knowledge and wisdom she has.</p>
+    <p>She realizes she doesn’t need to form a priesthood, she already has one.</p>
+    <p>River Elves now have the cultural aspect <i>Decentralized Religion</i> to symbolize that their organized religion is shared between all river elves on every level, like siblings in a family. It is not something declared from a central authority on high, but something that river elves figure out for themselves with the help of their close friends and family. The community and the laity decide the course of faith, the priesthood are merely there to help them along the way.</p>
+    <p><b>Misc:</b> And it is not a moment too soon. As the pathfinders return to the future site of Winter Quarters, they find that they are not alone. A group of river elves, riding boats patched from old wagons and scrap materials and pulled by their own arms, has arrived from the river to the west. A group from the old support columns. They have lost everything, including their hippos, but were recently able to make contact with the humans on the central continent. They were given supplies and told to head east, for the elven lands. They are extremely relieved to have found you after the arduous trek, and tell you more are coming.</p>
+    <p>Small groups of immigrants are now reaching this area, and you may use them to expand or build new settlements per the rules.</p>
+    
+    <?php
+        include '../../settarget.php'; 
+        settarget("River",14,null,null);
+        include '../footer.php';
+        echo getarrows("03_4_River.php","04_2_River.php");
+    ?>
+</body>
