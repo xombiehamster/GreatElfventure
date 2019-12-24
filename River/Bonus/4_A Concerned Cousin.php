c4e3782675c7317efb6dec445df4a188d7d0b9b3
xombiehamster <58495678+xombiehamster@users.noreply.github.com>
2019-12-23 20:34:02 -0600

    Update 4_A Concerned Cousin.php

--- River/Bonus/4_A Concerned Cousin.php
+++ River/Bonus/4_A Concerned Cousin.php
@@ -9,25 +9,44 @@
 </head>
 <body>
     <h1>A Concerned Cousin</h1>
-    <p>While at the future site of Winter Quarters, Litoria receives a message from Surt requesting that she locate and gather live samples of a very toxic flower called Azure Lilies.  It’s a Triumvirate request for medical supplies, with the stated goal of providing a long-term supply of sedatives for use in healing and surgical procedures (seeing as they don't have a lot of divine casters). She’s given the name of a merchant, Evander Bitterfruit, who says the  orders are from the Triumvirate, without specifics beyond that. Had their stamp and all.</p>
-    <p>“Tante, what’s this?”  While penning her response to ask for further detail, Acris spots the lilies in their little pots and cannot contain the urge to poke one.  The seed pod bursts and fills the room with a paralyzing inhaled toxin.  Litoria, still wearing her oiled cape for the outdoors is able to safely escape the range of the poison, but Acris immediately falls limp to the floor of the raft.  He lays there for a solid five hours before being able to move again.</p>
-    <p>There is no way this could be used as a sedative for a surgical procedure.  Acris was entirely conscious the whole time.  Litoria skips writing a second letter and opts to borrow the Feather.</p>
-    <p>"You've reached the Triumvirate chambers; scribe Titus speaking. How may I direct you?"  Litoria asks to speak with the Fury, specifically, because she seems the most reasonable of the three.</p>
-    <p>"Oh, the Fury? One second, I'll let her know you're on the... feather... for her."  A couple minutes pass.  "I'm sorry, it appears the Fury can't come to the feather right now. She's currently in the middle of blowing up her private chambers, it seems. May I take a message?"</p>
-    <p>"Weh. Tell'er we go' a reques' fer Azure Lily Pollen from some'un wi' yer station'ery.  I think it a strange one, me.  Pourquoi?  You have some kinda crisis ta be needin dang'rus stuff like this aroun?  It done knocked da 'prentise righ' out."  Litoria tries to explain things as simply as possible.</p>
-    <p>"Umm.... let me see if I see anything in the orders document. Hold on one second..."  There is a slight pause.  "Ah, here we go! Medical supply request! Azure lily at the bottom, right under something about Dreamdust. There's a stamp here, though no formal signature... but that's not terribly unusual, they often forget that. I've never seen the Terror sign anything honestly. Doesn't look like her handwriting, but its a formal request. Does that help? I can do some more digging if you like."</p>
-    <p>"Jus get the message to the Fury, weh?  We talk later."  Litoria is unimpressed by the functionary.</p>
-    <p>"All right. Happy to help!"  Several hours pass before she is contacted again.</p>
-    <p>"Litoria? It’s the Fury. I heard you tried to reach me earlier. Something about lily pollen?"</p>
-    <p>"Weh. Someone's sent us a reques' fer it, en claim it's a medical supply. Mais, now en da prentice is up 'gain, this woul be a horrible torture. I don' trust it, me."</p>
-    <p>"Hmm. Well, the request is in the Hunger's handwriting. As is the rest of the medical bill. ...He probably bought it for some kind of alchemy experiment he thinks we don't know about. And it doesn't look like he's requesting a lot of it, though there is mention of seeds here. I wonder if he's planning on growing it.</p>
-    <p>"I can ask him about it directly if you want, but my guess is it’s for some kind of alchemy, and he just didn't want to admit it."</p>
-    <p>"I'd apprecia' yer help makin’ sure this ain't a complex plot to assassinate the Terror or sommat. Merci Fury."</p>
-    <p>"Happily, dear!"</p>
-    <p>After five minutes, the Hunger himself responds.</p>
-    <p>"Litoria? Hunger here. I plan to shoot werewolves with it. Sound reasonable?"</p>
-    <p>"Weh."</p>
-    <p>She immediately contacts the winged elves to get one of their faster messengers to deliver the seeds to the Hunger as soon as elvenly possible, and they agree to the request without questioning it.  Pawprints among groups of footprints fill her thoughts - the Hunger’s fear may well be founded in reality.  There might be a resurgent werewolf problem near Surt.  And besides, Bryti said she wasn’t going back there, so it’s not like he’s going to be able to hurt her before Litoria gets a chance to have her list of questions answered.</p>
+    <p>Terje has looked over the after action report dozens of times, and here he sits reading it again. His office was always kind of sparse, but now he has moved any unnecessary furniture to the edges of the small room or removed it entirely. Just a couple of chairs and his filing system now. Got to have clear lines of sight.</p>
+    <p>They were good enough to get around the Cryptids, and they were looking for me…</p>
+    <p>He hasn't had the courage to go back to his apartment. He glances up to make sure his door is still locked. He glances down at his report again. He has lost his place. He starts reading it again, from the beginning. </p>
+    <p>Litoria is not used to proper doors anymore.  The raft she shares with her cousin has none.  What is the proper etiquette again?  Right.  Announce your presence.  Do not just walk right in.  She debates internally a moment.</p>
+    <p>And knocks.</p>
+    <p>Terje jumps slightly at the noise. He steps up to the door and places a hand on the latch. As he is standing to the side with a hand on his sword hilt, he realizes he is getting paranoid. He relaxes slightly, unlatches his door, and opens it.</p>
+    <p>"Hello?"</p>
+    <p>Litoria tries not to look too intimidating, but this effect is not particularly effective when she’s standing in her field armor having come directly from drills with the Eels.  She wishes Sauver could fit in the fort’s narrow hallways.</p>
+    <p>“Terje?”  She makes the effort to pronounce her cousin’s partner’s name correctly, and comes close.  “You’re not busy, non?”</p>
+    <p>"Litoria?" He blinks, briefly considering she might be another disguised spy, then shakes his head, "Uh, no. Come on in, please. Here…"</p>
+    <p>He pulls two chairs out from a corner, notably not sitting behind his desk, "Just… close the door behind you, please."</p>
+    <p>Litoria does as asked, and sits with a muffled clink of armor plates.  She leaves the spear at the door, and angles her chair to keep an eye on both.</p>
+    <p>“You ‘ave read the report, non?” she looks gravely at the blade elf.</p>
+    <p>He  glances at the report on his desk. The edges of the pages are already worn down from how many times he has flipped through it.</p>
+    <p>"Yes," he relents, "More than once."</p>
+    <p>“Mais la, you know then.”  Litoria nods, having decided something for herself.  “He trusts you, my cousin, your beau.”  She leans toward Terje, and places a webbed hand on his armored knee.  “We’ve all put <span class="tooltip">plein de<span class="tooltiptext">A lot</span></span> trust on you.  Ain’t fair, but it is what is.”</p>
+    <p>Terje slumps his shoulders, "I know. Smilisca told me that… when we first got together. You know the flotilla is my second home. I wouldn't do anything to put you in danger. That and Smilisca made me promise never to write anything down. I keep it all in my head. They wouldn't have found anything."</p>
+    <p>He takes a slow breath, "... Unless I had been home."</p>
+    <p>“Mais,” Litoria pats his knee again and leans back, “if’n they’d wanted ya dere, don’tcha think you woulda been?”  She gives him a halfway crooked grimace that can’t quite be called a smile.  “Tha Skulk shore thought em competen’ ‘nuff.  Would be en big big <span class="tooltip">honte<span class="tooltiptext">Embarrassing</span></span> minnow mistake, weh?”</p>
+    <p>“I…” Terje runs a hand through his hair, “Actually, yes, that is a very good point.”</p>
+    <p>He slumps in his chair and places a hand on Litoria’s, “I’m sorry, ami. It got to me. I always thought of the cryptids as invincible, you know? They were always the secret ghost in our back pocket, something we could always rely on. And… now I know they’re just elves, like the rest of us. There were four of them when they tried to catch the spy at my door. Four! And he got away! Not only that, one of them almost died! Siiri told me how bad it was. If she hadn’t happened to be up late working on her medicines in the Fort, that cryptid wouldn’t have made it.”</p>
+    <p>He pauses, “There was black blood on my door. Cryptids bleed black, did you know that?”</p>
+    <p>Litoria thought better of confessing that she did not know that they actually do bleed.</p>
+    <p>“Ami, mais - they’re elves, non?  All differen’ ways of bein’ en Elf er valid.”</p>
+    <p>Terje nods, “I suppose that’s true. Yeah, and I’d rather have an elf watching my back than some… weird elflike shadow horror, or ghost, or eldritch monsters. Knowing they’re elves like us does make me feel better. I am sorry, I was rambling. I am just… a bit shaken. Odd, isn’t it? After all we went through… maybe I was just finally starting to feel safe.” </p>
+    <p>Litoria had been all ready to complain and argue with this person who was targeted to potentially put her entire way of life and all the people who depend on her at risk.  Instead, she’s getting a pretty clear idea of what her cousin sees in the man.  She’s concerned for his well being.</p>
+    <p>“Terje, ami ami,” Litoria sighs quietly, “we’re safest when we ken rely on each other.”</p>
+    <p>“Weh,” Terje pronounces it like ‘whee,’ “Isn’t that the truth.”</p>
+    <p>He pauses for a few moments, “I love him. I really do. A little more each time I see him.”</p>
+    <p>Litoria looks at him seriously.</p>
+    <p>“<span class="tooltip">Qu'il est amoureux.<span class="tooltiptext">He's in love.</span></span>  My cousin, mais he’s damaged.  He knows it too.”  She folds her hands in her lap.  “You’re good fer him, en he - he shouldn’ be bad fer you.  Don’ break each other's hearts, Terje.  It’d hurt more’n you’d think.”</p>
+    <p>Terje gives a sly smile, “Well, then, the only way is forward. No way to go back now without the heartbreak. I don’t know where we’re going. I really don’t. Even a year ago, I didn’t know we were going to get this… serious. Ehra still needs me here, Katiin still needs me here… I can’t let my last family down. They’d be lost without me too. I don’t know what I’m going to do. I feel like I’m torn between two different  worlds and don’t want to give up either of them. I’m not going to give up, though. I’m going to keep trying. I promise. I swear on my terrible mutton chops.”</p>
+    <p>Litoria laughs, despite herself.  It’s a bit rusty.</p>
+    <p>“Luckily,” she gives him a sideways smile, “we’re <i>elves</i>.  Yall have plein de time.”</p>
+    <p>“Weh,” He mispronounces it <i>again</i> in a completely different way, “But maybe not so much that I can’t let myself have a <i>little</i> time now. I think I’ll tell Ehra I need a few days off. Y’know, for the stress. And hey… Thanks, Litoria. I feel better.”</p>
+    <p>“Le’ me know when you’ll be wantin’ to head over.  Sauver and I ken make it there right quick.”</p>
+    <p>Terje smiles again, “See? No maps at all.”</p>
+    <p class="center">-FIN-</p>
     <?php 
         include '../footer.php';
         echo getarrows("../Year/02_3_River.php#back2","../Year/02_3_River.php#back2");
