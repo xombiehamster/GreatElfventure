a67e83c5213bb058860ecd8d1e0ea65718a9e5c7
xombiehamster <58495678+xombiehamster@users.noreply.github.com>
2020-01-06 16:35:57 -0600

    special delivery

---
+++ Fire/Bonus/5_A Special Delivery.php
@@ -0,0 +1,39 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+
+<head>
+<?php include '../../header.php'; ?>
+    <title>
+        A Special Delivery
+    </title>
+    <link rel="stylesheet" type="text/css" href="../../style.css">
+</head>
+<body>
+    <h1>A Special Delivery</h1>
+    <p>While at the future site of Winter Quarters, Litoria receives a message from Surt requesting that she locate and gather live samples of a very toxic flower called Azure Lilies.  It’s a Triumvirate request for medical supplies, with the stated goal of providing a long-term supply of sedatives for use in healing and surgical procedures (seeing as they don't have a lot of divine casters). She’s given the name of a merchant, Evander Bitterfruit, who says the  orders are from the Triumvirate, without specifics beyond that. Had their stamp and all.</p>
+    <p>“Tante, what’s this?”  While penning her response to ask for further detail, Acris spots the lilies in their little pots and cannot contain the urge to poke one.  The seed pod bursts and fills the room with a paralyzing inhaled toxin.  Litoria, still wearing her oiled cape for the outdoors is able to safely escape the range of the poison, but Acris immediately falls limp to the floor of the raft.  He lays there for a solid five hours before being able to move again.</p>
+    <p>There is no way this could be used as a sedative for a surgical procedure.  Acris was entirely conscious the whole time.  Litoria skips writing a second letter and opts to borrow the Feather.</p>
+    <p>"You've reached the Triumvirate chambers; scribe Titus speaking. How may I direct you?"  Litoria asks to speak with the Fury, specifically, because she seems the most reasonable of the three.</p>
+    <p>"Oh, the Fury? One second, I'll let her know you're on the... feather... for her."  A couple minutes pass.  "I'm sorry, it appears the Fury can't come to the feather right now. She's currently in the middle of blowing up her private chambers, it seems. May I take a message?"</p>
+    <p>"Weh. Tell'er we go' a reques' fer Azure Lily Pollen from some'un wi' yer station'ery.  I think it a strange one, me.  Pourquoi?  You have some kinda crisis ta be needin dang'rus stuff like this aroun?  It done knocked da 'prentise righ' out."  Litoria tries to explain things as simply as possible.</p>
+    <p>"Umm.... let me see if I see anything in the orders document. Hold on one second..."  There is a slight pause.  "Ah, here we go! Medical supply request! Azure lily at the bottom, right under something about Dreamdust. There's a stamp here, though no formal signature... but that's not terribly unusual, they often forget that. I've never seen the Terror sign anything honestly. Doesn't look like her handwriting, but its a formal request. Does that help? I can do some more digging if you like."</p>
+    <p>"Jus get the message to the Fury, weh?  We talk later."  Litoria is unimpressed by the functionary.</p>
+    <p>"All right. Happy to help!"  Several hours pass before she is contacted again.</p>
+    <p>"Litoria? It’s the Fury. I heard you tried to reach me earlier. Something about lily pollen?"</p>
+    <p>"Weh. Someone's sent us a reques' fer it, en claim it's a medical supply. Mais, now en da prentice is up 'gain, this woul be a horrible torture. I don' trust it, me."</p>
+    <p>"Hmm. Well, the request is in the Hunger's handwriting. As is the rest of the medical bill. ...He probably bought it for some kind of alchemy experiment he thinks we don't know about. And it doesn't look like he's requesting a lot of it, though there is mention of seeds here. I wonder if he's planning on growing it.</p>
+    <p>"I can ask him about it directly if you want, but my guess is it’s for some kind of alchemy, and he just didn't want to admit it."</p>
+    <p>"I'd apprecia' yer help makin’ sure this ain't a complex plot to assassinate the Terror or sommat. Merci Fury."</p>
+    <p>"Happily, dear!"</p>
+    <p>After five minutes, the Hunger himself responds.</p>
+    <p>"Litoria? Hunger here. I plan to shoot werewolves with it. Sound reasonable?"</p>
+    <p>"Weh."</p>
+    <p>She immediately contacts the winged elves to get one of their faster messengers to deliver the seeds to the Hunger as soon as elvenly possible, and they agree to the request without questioning it.  Pawprints among groups of footprints fill her thoughts - the Hunger’s fear may well be founded in reality.  There might be a resurgent werewolf problem near Surt.  And besides, Bryti said she wasn’t going back there, so it’s not like he’s going to be able to hurt her before Litoria gets a chance to have her list of questions answered.</p>
+    <img class="center" src="../Images/02_1a_Summer_AzureLily.png">
+    <?php 
+        include '../footer.php';
+        echo getarrows("../Year/02_1_River.php#jump2","../Year/02_1_River.php#jump2");
+        setlast("Bonus/2_A Special Delivery.php");
+    ?>
+</body>
+</html>
