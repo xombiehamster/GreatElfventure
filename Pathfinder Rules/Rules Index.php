2f6722cf277a2ca6194e5b0e5cf65f69f4496a32
xombiehamster <58495678+xombiehamster@users.noreply.github.com>
2020-01-10 19:09:35 -0600

    rules

---
+++ Pathfinder Rules/Rules Index.php
@@ -0,0 +1,55 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html>
+<head>
+<?php include '../header.php'; ?>
+    <link rel="stylesheet" type="text/css" href="../style.css">
+    <title>
+        Great Elfventure Grand Rules Document
+    </title>
+</head>
+<body>
+
+<p>Below you'll see the document that includes the rules for the Great Elfventure campaign is a mostly-unedited state.</p>
+
+
+<iframe src="https://docs.google.com/document/d/e/2PACX-1vT7L3V34JReYxQYckTVjqCBM2ekTJOL1XzwVnA6Rkir0Ljr3j2RlPBRTgP6F1AZ1nAkDSkjz_wezCvs/pub?embedded=true"></iframe>
+
+<div style="overflow-x:auto;">
+    <table id="special">
+        <tr>
+        <!-- Headers -->
+            <th>Blade Elves</th>
+            <?php
+                #Crag elves don't appear until after the 2nd Potluck is read.
+                if($_SESSION["CRAG"]>0){
+                    echo '<th>Crag Elves</th>';
+                }?>
+            <th>Dark Elves</th>
+            <th>Dreamdust Elves</th>
+            <th>Fire Elves</th>
+            <th>River Elves</th>
+            <th>Winged Elves</th>
+        </tr>
+        <tr>
+        <!-- Stats -->
+            <td><a href="Blade.php">Pathfinder Stats</a></td>
+            <?php
+                #Crag elves don't appear until after the 2nd Potluck is read.
+                if($_SESSION["CRAG"]>0){
+                    echo '<td><a href="Crag.php">Pathfinder Stats</a></td>';
+                }?>
+            <td><a href="Dark.php">Pathfinder Stats</a></td>
+            <td><a href="Dreamdust.php">Pathfinder Stats</a></td>
+            <td><a href="Fire.php">Pathfinder Stats</a></td>
+            <td><a href="River.php">Pathfinder Stats</a></td>
+            <td><a href="Winged.php">Pathfinder Stats</a></td>
+        </tr>
+    </table>
+</div>
+
+    <?php 
+    include "../footer.php";
+    echo getarrows(null,null);
+?>
+</body>
