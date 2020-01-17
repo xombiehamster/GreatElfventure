70e5e93e6f20ed384bef9076a2f6269ca4db03a7
xombiehamster <58495678+xombiehamster@users.noreply.github.com>
2020-01-16 21:33:20 -0600

    Update 02_4_Dark.php

--- Dark/Year/02_4_Dark.php
+++ Dark/Year/02_4_Dark.php
@@ -16,26 +16,57 @@
         <tr>
             <th>Stress</th>
             <td>Economic 0/5</td>
-            <td>Morale 1/5</td>
-            <td>Property 0/4</td>
+            <td>Morale 0/5</td>
+            <td>Property 0/5</td>
             <td>Military 0/5</td>
         </tr>
         <tr>
             <th>Fate Points</th>
             <td>1/3 Refresh</td>
-            <td>0 Bonus</td>
+            <td>1 Bonus</td>
         </tr>
     </table>
-    <p>placeholder</p>
-    <p><b>Economic:</b> placeholder</p>
-    <p><b>Political:</b> placeholder</p>
-    <p><b>Military: </b>placeholder</p>
-    <p><b>Mental:</b> placeholder</p>
+    <h3 id="jump">Story 2.4.1: <a href="../12_A Stupid, Awkward Dream.php">A Stupid, Awkward Dream</a></h3>
+    <p><b>Economic:</b> Oh right we have small farms...let's expand them.  After a few days of considerable groaning about *not thinking of this sooner*, they all get to work and expanding the small geothermal farms that they currently had going for their own personal food source.  They had built the originals for their current small group of people, not really having expected for the population to … take on an exciting increase like it did with the refugees.  If they can expand the farms decently then hopefully that will give them some small room to work with until Summer arrives.</p>
+    <p><i>Investment</i> trapping of <b>Infrastructure</b></p>
+    <p>0 advantages, 1 success, 1 triumph</p>
+    <p><b>Political:</b> With the news coming through about the raid on the Dreamdust elves and the plans for dealing with that, Shyrendora decides to offer help in the only way that she knows her people can afford at the moment.  Sending a team of Spyhunters to Surt to help with their troubles of potential spys mucking about makes the most sense with the potluck right around the corner.  She honestly hopes she isn’t sending her team into a politically charged snake pit with the blade elves also offering military assistance, but she knows her hopes are more like wishful thinking.  Perhaps everyone will be so concerned with each other that they will leave her team in peace to do their jobs.  Kolya offers/demands to be a part of the team that goes.  She reluctantly lets him, if only because she has too much on her mind and not enough energy to argue with him.</p>
+    <p>Old favorite <i>Cloak and Dagger</i> trapping of Spycraft For <b>Surt-Surt</b></p>
+    <p>3 advantages, 1 success, 0 triumphs</p>
+    <p>Shyrendora - With the news of two spy incursions in Surt and the Fort, she decides to take some time out of her schedule to cover some shifts to check for potential tampering.  She doesn’t trust that the groups they are dealing with wouldn’t try again so soon and the news of potential water tampering at Surt has her on edge.  She is going to try to keep an eye on now known hotspots in the Hilt’Inn, but also pop in on the refugee camp to see how things are going there.</p>
+    <p><b>Military: </b>With Trischal being down and out, Shyrendora picks up some of her old Scoutmaster habits and gives the ranger teams an idea of what she wants to happen.  The pathfinding team had found a small rebounding population of fish.  From what she got out of Trischal once the Scoutmaster was awake enough to give an actual report, the population needs a few seasons to grow but wouldn’t be devastated by small expeditions to gather food.  With there being a growing fish population, she is also mildly hoping that there will be other animals in the area.  If they can do small bouts of fishing, great.  If there are other animals that they can hunt and bring back?  Also great.  She doubts it will work out with the best of both worlds but they have to do something.  Summer is very close but paradoxically still a long way away.</p>
+    <p><i>Rangers</i> trapping of <b>Reconnaissance</b></p>
+    <p>0 advantages, 6 successes, 0 triumphs</p>
+    <p><b>Mental:</b> Greg begrudgingly gives his notes to Vahn.  Vahn begrudgingly takes said notes and agrees to see what she and the others can do with it while he hovers around them like some kind of overprotective parent.  She knows that if the grump could cast it on his own he would do so in a heartbeat, and thus also knows that he is curious to see how this attuned glyph goes.  It takes some time and minor translating to parse his shorthand, but she gets the general idea of what he has figured out (without asking him thank you very much).  Her small group of “spiritually inclined”  (as he likes to sarcastically call them) are interested enough and fairly eager to give it a shot.</p>
+    <p><i>Creation</i> trapping of <b>Spirituality</b></p>
+    <p>2 advantages, 1 success, 0 triumphs</p>
     <h2>Results</h2>
-    <p><b>Economic:</b> placeholder</p>
-    <p><b>Political:</b> placeholder</p>
-    <p><b>Military:</b> placeholder</p>
-    <p><b>Mental:</b> placeholder</p>
+    <p><b>Economic:</b> It seems that dark elf engineers do their best work under pressure. The research and survey teams had already examined the geothermal heat at Nadezhda and considered them not warm enough to scale up for farming. Traditional dark elf geothermal farms require a small area of extremely high heat, like a thermal vent or even a lava flow, to work. The heat at Nadezhda is much lower, simply being a bit warmer than most cave systems.</p>
+    <p>When your engineers revisit the lower caves, however, they find that the locals have discovered a way to use it anyway. A local lichen variety favors warm, mildly damp conditions of these lower caves. While it is technically both edible and calorie dense, this lichen tastes like half cooked moldy pasta. What the locals have found, however, is that the local fauna love it.</p>
+    <p>As the dark elves have been cleaning out the skeletons, they have made space for the local wildlife to return. They are finding that these shallower caves near the rim of the Caldera have a surprisingly diverse ecosystem. Numerous types of small mammals, reptiles, and insects make their homes here, almost all of which are exploitable or edible. Your engineers adapt these small lichen farms into a large scale animal feed production operation, and locals begin capturing and breeding critters to be farmed for meat.</p>
+    <p>It turns out to be a major success. While gourmands from around the world won't be stacking up for "mole and cave cricket stew," refugees certainly will. Your researchers project that while the margins will be narrow, it will be enough for the current flow of refugees. There may be a few weeks of having to feed refugees lichen porridge, but they will be fed.</p>
+    <p>There is also an unexpected bonus to all of this. The boom in lichen population and aggressive breeding of local wildlife by farmers give them an idea. They spread the lichen and spare breeding stocks in the surrounding caves. This abundant food allows the natural wildlife to finally begin to return. The caves around Nadezhda have the <i>Rebounding Ecosystem</i> temporary aspect for 2 turns. Life is finally starting to reclaim its footing after the necromantic disaster.</p>
+    <p>(Note: if you have some cave critters you want the dark elves to have access to, now's a good time!)</p>
+    <p><b>Political:</b> It seems like Surt really did need your spyhunters' help this turn. While it does indeed turn out that Surt is a wild hotbed of chaotic vibrance that is really no place for an introvert, everyone is so focused on the controversial blade elf soldiers that most people don't give the dark elves a second look.</p>
+    <p>The big thing you learn is that Surt's security setup is a fucking mess. The main security force is the Constabulatory Guard. They're relatively professional full-time guardsmen. The second is the hot mess known as the Praesidium Civile. This loose paramilitary group is made up of civilians who volunteer or are paid for extra work as security professionals. They do not lack for motivation or numbers, but man they are a fractured mess. Different factions vie for power within the Praesidium, including Tsun, which can cause outright infighting. While there have been solid attempts to professionalize this force, they're still clearly a group that relies on enthusiasm and sheer numbers.</p>
+    <p>Now, neither of these groups are particularly bad at their jobs and between them there's quite a lot of coverage. The problem is that neither of these groups are particularly, well… subtle. They use fairly blunt means to accomplish their goals.</p>
+    <p>Your Spyhunters decide that they must use their smaller numbers and greater capacity for stealth to their advantage. They determine that the best way to exploit this defense would probably be to play the various factions against each other, then use the resulting gaps. Especially with the blade elves in town, this is a recipe for disaster. Your Spyhunters resolve to keep an eye on the factions that seem most at each other's throats. It turns out to be a good idea.</p>
+    <p>The CG have a pretty good idea on keeping the blade elves from getting involved in any messes, and it is to ensure a guard is stationed with them at all times. However, this gets dicey in crowded situations. It is on one such occasion during a particularly busy night at the Howl that someone tries to make their move.</p>
+    <p>Another bar fight distracts the guards. A few insulting words make it to the ears of a group of five blade elf soldiers. Before anyone can account for it, they're ready to take it outside. Right into the waiting arms of a more extremist Praesidium patrol, in a clear alley, with lookouts on the ends</p>
+    <p>They did not count on the dark elves. A Spyhunter recognizes a trap when he sees one. The dark elf can't make it inside because of the crowd, but manages to reach a passing CG patrol in time. The CG makes it to the alley in time, stopping the group before they ambush the blade elves. All are arrested.</p>
+    <p>The CG and Spyhunters agree that this group was intentionally looking to start an incident that would end up with dead blade elves (or maybe fire elves dead at blade elf hands) and perhaps put a permanent black mark on the two peoples' relationship. Thankfully this has been avoided. The blade elves spend a few days in lockup and under disciplinary action for getting drunk while armed and leaving their mandatory escort behind. The Praesidium members responsible for the plan are taken into Triumvirate custody.</p>
+    <p>You are not sure what happens to them after that.</p>
+    <p><b>Military:</b> The rangers deploy to the lakeside with a relatively decent plan of what to do. Most of their experience is in caves, but dark elves do cross training with surface trackers. Getting stuck outside a cave can happen to anyone. This, however, is something new.</p>
+    <p>Your rangers have a good idea: If there are fish, there are probably things that eat the fish. Especially since most of the lake is barren, the local ecosystem probably centers on this area. Half the team spreads out to search the surrounding area while the other half stays to slowly collect fish.</p>
+    <p>It turns out to be a good idea. The local wildlife is relatively abundant for an area that's otherwise badlands. While dark elves don't turn out to be particularly good hunters, they do turn out to be particularly good trappers.</p>
+    <p>Small to medium lakeside mammals that pray on fish like beavers and otters turn out to be most of the haul. With this supplement, the rangers are able to carry back as much food as they can safely carry without needlessly stressing the healing ecosystem.</p>
+    <p>They find that these food supplies aren't strictly immediately necessary. With the cave farms going well, the foot shortage is mostly addressed. You actually gain a <i>Food Surplus</i> Aspect that will last for one turn.</p>
+    <p>In addition, many people seem to like the fine furs that the dark elves brought back from their trapping. That may turn out to be an exploitable resource.</p>
+    <p><b>Mental:</b> Fucking <i>Greg</i>, man. This is what happens when an alchemist fiddles with divine magic. Divine casters don't even really <i>research</i> spells, they just kind of get them. Coming up with your own divine spells is kinda like telling Mother Echo you know better than she does. It's not impossible or anything, it's just vaguely rude.</p>
+    <p>Vahn initially struggles to adapt Greg's notes. They are done with the brute empiricism of an Alchemist, and since he isn't a divine caster they are largely untested and theoretical. She has to adapt large portions of it with her own research to make it a practical, usable spell by any stretch.</p>
+    <p>While Vahn does learn the spell (and can add it to her spell list,) she does not have enough time remaining to actually develop it into an aspect. If she has more time, she can teach it to her fellow casters and work out a system with the Spyhunters to deploy it in sensitive areas and attune it against wolf elves. She'll just need more time.</p>
+    <p>You will have an Upgrade on your next attempt to develop that Aspect.</p>
+    <p><b>Congratulations!</b> You have survived another year in this strange and chaotic land! As a reward for your perseverance, you receive one skill point to distribute as you wish!</p>
     <h3><a href="../../Events/03_0.php">The Elves Gather for Their Annual Potluck</a></h3>
     <?php
     include '../../settarget.php'; 
