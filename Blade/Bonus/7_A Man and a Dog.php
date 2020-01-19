5e8ffd871dd61809770fa4f7f3f8a0736a5a9d15
xombiehamster <58495678+xombiehamster@users.noreply.github.com>
2020-01-19 14:26:48 -0600

    Update 7_A Man and a Dog.php

--- Blade/Bonus/7_A Man and a Dog.php
+++ Blade/Bonus/7_A Man and a Dog.php
@@ -4,6 +4,7 @@
 <head>
 <?php include '../header.php'; ?>
     <link rel="stylesheet" type="text/css" href="../../style.css">
+    <link rel="stylesheet" type="text/css" href="../NPC.css">
     <title>
         A Man and a Dog
     </title>
@@ -11,7 +12,119 @@
 
 <body>
 <h1>A Man and a Dog</h1>
-<p>PLACEHOLDER</p>
+<p>The time has come.</p>
+<p>Allophryne's Abeyance has given one day of peace - one day to rest and prepare. That day is now ending.</p>
+<p>Bryti has selected a large clearing in the nearby sparse woods, a considerable distance from the flotilla. She has spent the better part of the day preparing the space. The clearing is surrounded by several cairns of stones, arranged in a circle. At the center, another arrangement of stones that she has pounded into the dirt in a circle as a basic holy symbol of Luna.</p>
+<p>The sun is just beginning to set. It is a warm night. The sky is perfectly clear, and the wind is calm. It would be nice, if not for the obvious. As the sun sets, he can feel an almost electric tingle of tension. The Dog is pacing in his mind, waiting to get out of its cage. It knows what is happening. Even though it cannot communicate with him, he knows it his there.</p>
+<p>The Hunger has none of his tools. Only a simple chiton and his mask. He sits cross-legged across the holy symbol from Bryti. She is in her hybrid form. Her sword, still in its sheath, sits in her lap. She appears to have been meditating. Allophryne and Blinkin wait at the edge of the clearing, ready to stop the Dog if it tries to escape.</p>
+<p>As the sunlight slowly fades, the tension in Hunger’s mind grows stronger. Bryti opens her eyes and looks up from her sword to him. She takes a calming breath.</p>
+<p>“The time for masks has passed,” she tells him. </p>
+<p>The Hunger sighs, and nods, then reaches for the straps along the back of his head. </p>
+<p>The mask detaches with a gentle pop, revealing a face almost identical to the one depicted on the mask, and oddly unblemished, despite hours of pressure and metal. Without it, he is an elf in the middle of his 200s, with a dour face and a sharp aquiline nose.</p>
+<p>For the first time in several years, the elf feels a warm breeze on his face. He can smell the fresh air without the tinge of bronze. It is surprisingly refreshing.</p>
+<p>The werewolf stands. She pulls the sheath from the blade and tosses it aside. Now that it is not being swung at him, he can see the detailed engravings clearly. Two wolves chasing a moon, Lycan runes meant to contain the ‘evil’ of the silver, and oddly, a short poem in Skaplyndi:</p>
+<p id="BrytiLetter">Through my wrath and pain</p>
+<p id="BrytiLetter">I shall purge this wretched curse</p>
+<p id="BrytiLetter">The last trace of us.</p>
+<p>She plants the tip of the blade in the dirt and stares at him over the hilt. Her fingers tense on the grip.</p>
+<p>“The Dog will not leave this circle. That is not a threat. That is not a promise. That is a <i>fact</i>,” she declares to him. </p>
+<p>"So what happens now?" he asks, meeting her gaze. </p>
+<p>Her face twitches as she suppresses a surge of emotion, "Now I will give you a fair fight." </p>
+<p>He only nods in reply. There is no turning back and no second chances. He knows what he has to do.</p>
+<p>The sun has set. He does not see the full moon beginning to rise over the treeline behind him, but he does not need to. He can feel its light on his back, feel the surging energy it provides. The Dog crashes against its cage. The spell begins to fracture.</p>
+<p>The werewolf before him prepares to carry out her sacred duty. Pale moonlight shrouds her and the symbols on her sword begin to glow. Her eyes take on that blue glow. She takes a hand of the hilt of the sword and makes a gesture. A rune lights up on her palm. She waits.</p>
+<p>The spell ends. The Dog surges forth, more powerful than ever before. It is cornered, terrified, and enraged. The elf gasps as the Dog begins to take hold and he begins to change.</p>
+<p><i>If I die here we both do!</i> It screams at him. </p>
+<p>The elf very nearly is enough to slow the dog, but not quite. It forces its way through the defenses he had been preparing and wrests control. The elf's consciousness manages to hang on.</p>
+<p>He feels the dog pivot to try to run, but it is too late. The Purifier is already surging forwards. She grabs the Dog by the neck with her free hand. He can feel the rune burning into its skin. She lifts it bodily off the ground.</p>
+<p>She snarls, "<b>Hefja bl-</b>"</p>
+<p>With a crackle, an arc of red energy leaps from the Dog and strikes her in the chest. It hits her with a smell of burning ozone and singed fur. She barks in shock as the spell is ripped from her hand.</p>
+<p>"What in <i>Hel!?</i>" She sputters.</p>
+<p>The Dog sinks its teeth into her wrist and twists. Still reeling, she loses her grip. The Dog drops to the ground and tries to scramble away. Smoke rises from Bryti's chest and hand as she struggles to cast the spell again.</p>
+<p>The elf struggles to stay conscious within the dog. No, he struggles to <i>think</i>. That wasn't an effect of the affliction. He forces himself to remember, to analyze. He feels the affliction within him, a black ball of hate. Around that, a crackling red haze of hateful energy. Hateful, he realizes, but not divine. He suddenly realizes what is happening.</p>
+<p><i>But they don't,</i> he recognizes the look of confusion on Bryti's face, sees Allophryne lunging towards him set with determination, <i>I have to warn them.</i></p>
+<p><i>Shut up, coward, you lose!</i> The Dog growls.</p>
+<p>The elf ignores it. He tries to force control over the Dog's body, to reach out and assert his will, but it is too much. There is too much rage and hate, fear and panic, trapped in its form. His mind begins to fade.</p>
+<p><i>Not its form,</i> he realizes suddenly, <i>mine.</i></p>
+<p>The elf digs his fingers into the dirt. He bites down on his own tongue. The dog reels in surprise.</p>
+<p><i>You are me,</i> he tells it, "And I am you," he manages to gasp aloud. </p>
+<p>The Hunger -- no, Gaius -- tests his vocal cords. “There’s a curse effect,” he says, his words tripping over the strange shape of his mouth, and his uncomfortably long tongue. “Arcane, not divine.” </p>
+<p>He forces his legs to shuffle him back into the center of the circle. “Proof… this couldn’t have been an accident. I don’t… the affliction cannot be removed while it is present. I think. Whatever it is, this is not a normal spell. <i>Remove Curse</i> would have trouble with it. <i>Abeyance…</i> might not.” He kneels, trying to force his body to relax. </p>
+<p>It has been 25 hours since Allophryne cast the first Abeyance he has ever cast.  It’s not the first time he’s cast a spell.  He’s been flipping Detect Shapechangers on and off at random ever since he realized he could do that.  He’s not had access to particularly useful magic before now.</p>
+<p>But he’s worked with Blinkin plenty.  The obese alligator trundles forward to assist while Allophryne grabs the fire elf’s shoulder to begin the spell. The stripes that line his throat stand out in stark relief as the spell begins.</p>
+<p>Bryti shakes her burned hand and frowns and the ensuing literal dogpile.</p>
+<p>"I suppose that's one way to do it."</p>
+<p>Without any further hesitation, she jumps into the grapple. She leaves the sword behind. She grabs him by the scruff of his neck, then pulls him into a headlock to control his teeth.</p>
+<p>"Hold on as long as you can, Innkeeper. We have you."</p>
+<p>The longest minute of Gaius' life begins as Allophryne begins his spell and the Dog surges back to take control. Fueled by blind animalistic panic, the Dog hits his mind like a tidal wave.</p>
+<p>The glib rational being that the Dog pretended to be is gone. It surges to control, lashing out wildly. It snaps its teeth, snarls, and howls wildly. It captors do not budge.</p>
+<p>While the Dog is far stronger than the elf, it is no match for the powerful warriors that hold it - or one fat alligator. The Dog thrashes and writhes, trying to slip free. At one point during Allophryne's casting, it slips free of Bryti's headlock and lunges to bite the river elf.</p>
+<p>Blinkin has other ideas. The obese alligator simply rears up and flops down on the offending beast like a giant scaly blanket. The Dog wheezes as Blinkin's bulk knocks the breath out of it. Bryti quickly regains her grasp.</p>
+<p>Allophryne finishes his spell and splashes the Dog with the holy water he has been preparing. There is a crackling of energy as the curse that was protecting the affliction is grounded.</p>
+<p>Bryti grins fiercely and raises a hand. She chants quietly. The rune lights up on her hand again and she looks the Dog dead in the eyes. </p>
+<p>"<span class="tooltip"><b>Hefja blót!</b><span class="tooltiptext">To sacrifice a heathen in the name of Luna</span></span>" She slams her hand into the Dog's face.</p>
+<p>...</p>
+<p>The elf is adrift. He sees a writhing black mass all around him, clinging to him and dragging him down somewhere from which there is no escape. He has to fight it, but what is it?</p>
+<p>He focuses his mind. He remembers his long, grueling struggle. He searches for the answer, for the truth of what the Dog really is.</p>
+<p><i>You never cared,</i> the Dog had said, <i>but I always wanted to.</i></p>
+<p>He knows what the Dog is now. He can see it, crystal clear in its totality. He reaches out to destroy it… but he can't.</p>
+<p>His mind falters. He sees a mask in his vision. He is confused. He knows what the Dog is, but a more important question is eluding him.</p>
+<p id="i">Gaius, Guy, a Mask, a tax assessor, an apprentice, an elf, Innkeeper, the Hunger…</p>
+<p id="i">Who am I? </p>
+<p><i>The first step in any field of study is to ask a question,</i> Cantia Brighteye had said. <i>The second is to study what is observable about the subject.</i></p>
+<p>Fact one: he is an elf. Fact two: he is the Hunger. Fact three: he is Gaius Freshleaf, tax assessor and alchemist’s apprentice and an elf in a mask and…</p>
+<p id="i">And I am all of those things too.</p>
+<p>The darkness presses down on him.</p>
+<p>Fact four: all of those identities were him. Facets of him, carved from the events of his life. Even the dog, in its way, was simply another part of him. A shard, containing his darkest and most arrogant elements. </p>
+<p>But those elements weren’t the whole of him. </p>
+<p><i>The third step,</i> Cantia continued, somewhere in his memories, </i>is to form a hypothesis. A testable explanation for the observations you recorded, that can be proven or disproven in a series of experiments.</i></p>
+<p>Hypothesis: the curse copies the darker, or more selfish, aspects of the personality, and elevates them to control the overall being. The dog disregards his fellows’ abilities because Hunger, deep down, believes he is smarter than them. The dog is certain Surt can only survive with its help, but also willing to burn Surt down around it, because the elf worries that Surt can’t survive without him. </p>
+<p>Subclause: but those worries, those fears, that arrogant certainty isn’t what defines him. Arrogant certainty isn’t what created the Surtian gardens; that was hard work, dedication, and willingness to work with and share the glory with others. It was Sita who developed the water filter into a useable format, and the CPC that designed the schematics, and the people of Surt who built it.</p>
+<p>Fear isn’t what made him the most successful tax assessor in Muspelham. That was determination, dedication, and… empathy. He’d rarely admitted that to himself, but the joy of slipping a few coppers out under the nose of the Governor <i>wasn’t</i> from ‘sticking it to the man’. The elf had never cared a wit about the Governor. It was about the people who came to him -- who needed those coppers far more.</p>
+<p id="i">No.</p>
+<p>Had he wanted recognition? Maybe. Definitely. His name had gotten him attention, success, wealth… and eventually an apprenticeship. The chance to pursue real alchemy. Thaumaturgical exchange. Lead into golems. The power of dragons.</p>
+<p>The burning cellar. Bodies on the street of Muspelham. Recognition had lead him to that, too. Recognition, a desire for power, the fear of death and aging… and the arrogance to think that he somehow <i>deserved</i> that power. Deserved it enough to put the whole city at risk. To allow for -- or worse, <i>not even think of </i>-- the deaths his ‘research’ might cause.</p>
+<p id="i">No!</p>
+<p>And the hours afterward. Pulling victims from their burning homes. Healing them with scattered tinctures. Listening to their screams and sorrow and horror at the death and loss surrounding them. At their bodies, forever tainted and changed by power that turned their city to ash. <i>His</i> power.</p>
+<p>Had he been hiding from that all this time?</p>
+<p id="i">They never mattered! It doesn't matter! STOP!</p>
+<p>That, too, was a part of his thoughts, but now, within the flames, he could recognize what that was. That wasn’t arrogance; that was fear. A shield of callousness, built to shade the rest of him from recognition of what he’d done. From acknowledging that this was his fault. That the Conflagration was a part of him.</p>
+<p id="i">Please stop remembering them. It hurts too much.</p>
+<p>And that is why he fought so tirelessly, so <i>obsessively</i> for Surt. Why it had to survive. To ‘pay his debts’. To bury them.</p>
+<p>But forgetting them had been what killed them in the first place. What sort of payment is that? How is that saving them? They didn’t need a new city in their name; they needed to be remembered.</p>
+<p>He thought of Iolas, the boy who had been apprenticed to him by his thankful parents. He thought of that same family, and the remnants he found in their tiny house on the city’s edge. The street vendor and his daughter, near the office. </p>
+<p id="i">NO!</p>
+<p>The elf finally understands. Here, in this darkness, he is alone. There is nothing here that is not him. There is nothing to reject, nothing to cast out, nothing to fight against. There is only something to remember.</p>
+<p>The darkness is part of him, but it is him incomplete. The pressure weakens and begins to break. He feels it back away, the tension eases. It is not strong enough to remember.</p>
+<p id="i">Please stop.</p>
+<p>The bards of the Governor’s Court, playing at the Summer Fair. The youths training for service in the academy’s amphitheater. The bakers, smiths, artisans, and brewers in the Supply Quarter. His neighbors, their homes too close to the site of the explosion. All ash.</p>
+<p id="i">I can't.</p>
+<p>The tension pulls away. It disintegrates. Slowly, almost gently, it is burned away. There is one last thing that clings to him. It is tiny, a piece of string as thin as a spider's web. It hangs onto him.</p>
+<p id="i">I have always been here. I will always be here.</p>
+<p>“Congrats on your eternal irrelevancy,” the elf says. “Now get out; I’m busy.” </p>
+<p>The string snaps. It barely makes a sound.</p>
+<p>…</p>
+<p>The elf opens his eyes. There is a warm breeze of night air on his face. He hears the soft noises of night life. He can see the bright full moon above him. There is nothing lurking in the back of his head. It is quiet. It is peaceful.</p>
+<p>There are also about four hundred pounds of alligator sitting on his chest.</p>
+<p>"Guy?" Bryti's cautious voice calls out. </p>
+<p>Guy coughs. “Definitely,” he says, squeezing his voice out from a ribcage actively being compressed like a bagpipe. </p>
+<p>Allophryne blinks a few times, checking to be sure that the spell is working by looking back and forth between the triple-shape of the natural lycanthrope and the man who is no longer Afflicted.  There is no trace of anything but an elf being squashed beneath the flotilla’s chubbiest alligator.</p>
+<p>Blinkin, you absolute u n i t.  It’s time to get off.  Allophryne grabs the gator by the tail and gives a not-so-gentle yank.  They release the elf with a mumbled apology.</p>
+<p>“Don’t apologize,” the elf says, crawling back to his feet. “He saved my life. That’s worth a few moments of compression.” </p>
+<p>"You did it," Bryti leans back to a kneeling position as she looks at the elf.</p>
+<p>She turns to look at Allophryne and Blinkin, "We did it."</p>
+<p>She sounds genuinely bewildered. </p>
+<p>Guy picks up his mask, from where he dropped it beside the ritual circle, and wipes a bit of stray mud from the rim. His eyes trace across the bronze face. </p>
+<p>"I had never succeeded in saving someone after the first transformation before," Bryti says to no one in particular. </p>
+<p>After a second, Guy flips the mask back over, and slaps it back onto his face. He turns back to Bryti. "That was… not exactly what I expected. And not one I'd care to repeat, honestly. But… thank you." </p>
+<p>"Anytime," she says rather blithely.</p>
+<p>She focuses slightly, then looks at the Hunger, "Actually, I have a request." </p>
+<p>He nods. “By all means, anything.”</p>
+<p>"I know that the Tourist is banned in Surt, but what about Bryti the blade elf?" </p>
+<p>The Hunger taps a few fingers on his mask. “Hmm. I think we can work with that -- with two conditions. One -- convince Ehra to give you some believable paperwork citing your origins; that way, if Tsun gets too suspicious, I can justify my response as preventing an international incident. Two -- try not to scare Captain Doukas again.” </p>
+<p>"Should probably go by Katya again, then. I am losing track of my secret identities, to be honest," she nods, "Agreed." </p>
+<p>Hunger nods. “I can’t help you on the ‘secret identities’ part, at least -- I just spent all night fighting the dog for mine -- but at the very least, you’re welcome to visit Surt by whatever clearly-a-Blade-Elf-don’t-ask-questions name you wish.” He takes a step forward, and stumbles. “...And now I think I probably need to lie down.” </p>
+<p>"Agreed," she sighs, "I need a vacation."</p>
 <p class="center">-FIN-</p>
 
 <?php
