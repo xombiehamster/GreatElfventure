bd0bd64d03269510f8ab41fc13cf1a4ff128e6a4
xombiehamster <58495678+xombiehamster@users.noreply.github.com>
2020-01-23 20:26:12 -0600

    ambush predators

--- Fire/Bonus/13_Ambush Predators.php
+++ Fire/Bonus/13_Ambush Predators.php
@@ -6,7 +6,7 @@
         Ambush Predators
     </title>
     <link rel="stylesheet" type="text/css" href="../../style.css">
-    <link rel="stylesheet" type="text/css" href="../River.css">
+    <link rel="stylesheet" type="text/css" href="../Fire.css">
 </head>
 <body>
     <h1>Ambush Predators</h1>
@@ -215,8 +215,8 @@
     <p class="center">-ACTUAL FIN-</p>
     <?php 
         include '../footer.php';
-        echo getarrows("../Year/04_1_River.php#back","../Year/04_1_River.php#back");
-        setlast("Bonus/12_Ambush Predators.php");
+        echo getarrows("../Year/04_1_Fire.php#back","../Year/04_1_Fire.php#back");
+        setlast("Bonus/13_Ambush Predators.php");
     ?>
 </body>
 </html>
