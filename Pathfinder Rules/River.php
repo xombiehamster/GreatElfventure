618574eece35edf9908771f086866248b9740619
XombieHamster <58495678+xombiehamster@users.noreply.github.com>
2020-02-05 11:43:44 -0600

    Update River.php

--- Pathfinder Rules/River.php
+++ Pathfinder Rules/River.php
@@ -1,1 +1,39 @@
-
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html>
+<head>
+<?php include '../header.php'; ?>
+    <link rel="stylesheet" type="text/css" href="../style.css">
+    <title>
+        River Elf Pathfinder Race Statistics
+    </title>
+</head>
+<body>
+<H1> River Elves </H1>
+<p><strong>Type</strong><strong><br /></strong>Humanoid (elf) 0 RP</p>
+<p><strong>Size</strong><br />Medium 0 RP&nbsp;</p>
+<p><strong>Base Speed</strong><strong><br /></strong>30 ft move speed 0 RP <br/>
+30 ft swim speed 2 RP
+</p>
+<p><strong>Ability Score Modifiers</strong><br />Standard (+2 Str, &ndash;2 Dex, +2 Int) 0 RP&nbsp;</p>
+<p><strong>Languages</strong><strong><br /></strong>Standard (Elven and Common, bonus language: Celestial, Draconic, Gnoll, Gnome, Goblin, Orc, and Sylvan) 0 RP&nbsp;</p>
+<p><strong>Defensive Racial Traits</strong><br /><em>Elven Immunities</em> 2 RP&nbsp;</p>
+<p>Elves are immune to magic sleep effects and gain a +2 racial saving throw bonus against enchantment spells and effects.</p>
+<p><em>Skin breathing vulnerability</em>-1 RP</p>
+<p>Because a river elf’s skin aids in their breathing, they takes a –2 penalty on Fortitude saving throws against gases, contact and inhaled poisons, and all other inhaled effects.</p>
+<p><strong>Feat and Skill Racial Traits</strong><em>Keen Senses</em> 2 RP</p>
+<p>Elves receive a +2 racial bonus on Perception checks.</p>
+<p><em>Amphibious</em>2 RP</p>
+<p>River elves are just as capable on land as in water.</p>
+<p><em>Swamp Stealth</em>2 RP</p>
+<p>River elves receive a +4 bonus to stealth if in water or in swamps or wetlands</p>
+<p><strong>Offense Racial Traits</strong><br /><em>Weapon familiarity</em> 2 RP&nbsp;</p>
+<p>Elves are proficient with longbows (including composite longbows), longswords, rapiers, and shortbows (including composite shortbows), and treat any weapon with the word “Elven” in its name as a martial weapon.</p>
+<p><strong>Senses Racial Traits</strong><br /><em>Low-light vision</em> 1 RP&nbsp;</p>
+  <p><em>Watersense</em> 1 RP</p>
+<p>Members of this race can sense vibrations in water, granting them blindsense 30 feet against creatures that are touching the same body of water.</p>
+<?php 
+    include "../footer.php";
+    echo getarrows(null,null);
+?>
+</body>
