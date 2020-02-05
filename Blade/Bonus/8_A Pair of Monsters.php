9c170b758429c8065ba8d64d75de0c37c40c28be
XombieHamster <58495678+xombiehamster@users.noreply.github.com>
2020-02-05 11:21:47 -0600

    Update 8_A Pair of Monsters.php

--- Blade/Bonus/8_A Pair of Monsters.php
+++ Blade/Bonus/8_A Pair of Monsters.php
@@ -11,7 +11,70 @@
 
 <body>
 <h1>A Pair of Monsters</h1>
-<p>PLACEHOLDER</p>
+    <p>Terror hauls herself up the canyon wall. She’s thirty feet above the path, and climbing fast, aiming for an isolated ledge she spotted from well below.</p>
+    <p>She sets her elbows, and hauls herself onto the ledge, twisting into a sitting position in the same motion.</p>
+    <p>The paths along the cliff face fan out below her, like fracture lines on glass, criss-crossing from one side to the other. One of the paths traces right up to the ledge itself, along a side of the cliff tucked away from where she’d made her climb. </p>
+    <p>Terror snorts. She turns back around, scanning the cliff above for an even more isolated ledge.</p>
+    <p>"I have not mauled any local politicians," a voice speaks out from nearby, "If that is your concern."</p>
+    <p>Terror turns to see the Tourist. She is sitting cross legged on a ledge nearby, overlooking the city. She regards Terror with a tired stare. </p>
+    <p>Terror glares back, before getting up, and pointedly examining the cliff face above the ledge for more handholds.</p>
+    <p>Bryti follows her gaze for a moment, then gives her a contemplative look. She makes a small grunt, then turns back to look over the canyon again. She holds her two holy symbols in her lap, gently running her thumbs over both the Calestros and Luna symbols as she watches the lights in the city. </p>
+    <p>"We got nothing from him," Terror said, testing a handhold. "So if that is your concern, rest assured, there's nothing more that can be done."</p>
+    <p>"Typical," Bryti's face tenses up for a moment, "At least I was stopped before I beheaded him out of spite." </p>
+    <p>"Would have made no difference," Terror said. "Cal's grace, he might have been better off." She jumps up, grabbing for a crevice a few feet above her head, and missing it by several inches.</p>
+    <p>"What happened?" Bryti asks, turning back to look at Terror. </p>
+    <p>"The curse triggered on his first answer," Terror says. She glances back. "Emptied his memories. Not a lot left, and nothing that can be recovered post-mortem."</p>
+    <p>"Vicious, but effective," Bryti grumbles. She continues to fiddle with her holy symbols. When Terror looks back, she sees something odd. Scratched into the rock next to Bryti is the same symbol that Terror found on the overlook near Surt: Forað, the Lycan rune for monster. </p>
+    <p>Terror gestures to the carving.</p>
+    <p>"Hmph," Bryri looks at it, then back to Terror and sighs, "Fine. There you have it. The Tourist is off sulking like a surly teenager. Feel free to gloat." </p>
+    <p>“I don’t gloat,” Terror says. “Do you  mark every Elven settlement with one of those?”</p>
+    <p>"No," Bryti gives Terror a curious look, "You… found my camp near Surt? That's the only place I remember leaving one." </p>
+    <p>“You’re not the only one who gets bored and climbs mountains,” Terror says. “So… why ‘monster’?” </p>
+    <p>"Isn't it obvious?" Bryti raises an eyebrow. </p>
+    <p>Terror stares at her.</p>
+    <p>"I come down from the hills, savagely attack an innocent man, and terrorize your populace, only to be driven back by one of your great heroes in a vicious battle. What am I supposed to consider myself?" Bryti's voice is tense. </p>
+    <p>Terror rolls her eyes. “Stupid.”</p>
+    <p>"I…" Bryti sighs, "... have no evidence with which to argue that at the moment." </p>
+    <p>“Don’t take it personally; I don’t think anyone really does. Myself least of all-- not much, I mean,” Terror corrects quickly. She walks over and sits on the boulder above Bryti’s carving. “You saved the Hunger’s life -- and a lot of other people, too, to make up for it. Not exactly monstrous behavior.”</p>
+    <p>"I was doing my sacred duty," she explains, "Nothing more." </p>
+    <p>“<small>How’s that working out for you,</small>” Terror mutters to herself.</p>
+    <p>"... Isolating," Bryti chooses the word carefully.</p>
+    <p>“Hence why you’re treating yourself like a monster?” Terror sighs. “I’ve met monsters, Tourist. If you were one, you wouldn’t ask yourself if you are.”</p>
+    <p>"Would I?" She asks as she taps her fingers on her holy symbol, "Isn't it becoming clear? We can't coexist, us and other people. We are too violent, too prideful. Even if we mean well, we will come to blows with others, and when we do the outcome is always the same. We always destroy ourselves in the end." </p>
+    <p>"Yet people keep expecting us to run cities, fulfill divine commandments, and interrogate spies without detonating them, right?" Terror says, with a metallic edge to her voice. "When will they learn?"</p>
+    <p>Bryti tilts her head curiously at Terror.</p>
+    <p>"By 'us,' I meant Lycans," she explains. </p>
+    <p>Terror tilts her head. "My apologies," she says sarcastically. "I thought we were still talking about monsters." </p>
+    <p>"<i>You?</i>" Bryti snorts, "You're joking. You're a hero, a champion, not a monster. I've heard stories that speak of you as a legend." </p>
+    <p>"Not one of those is mutually exclusive," Terror says. "And you're not the one that just scared an elf into brain-death. I can keep going; I've got a long list of…" She pauses. "And if 'monster's is too exclusive a title for me, then sorry, soldier, but you're way out." </p>
+    <p>"I never was a soldier," Bryti responds quietly, "And if you were truly a monster, you would have killed me in Surt. You had every reason to, but in even in your wrath and pain, you chose to avoid a killing blow. I have been wondering why for some time." </p>
+    <p>Terror sighed. "You didn't deserve to die just for wandering into the wrong town. Hunger was right, <i>again</i>, thrice-damn the reasons. We're -- <i>I'm</i> just lucky my anger didn't get the better of me."</p>
+    <p>"<i>You're</i> lucky…" Bryti grumbles, "There are hundreds who are called hero who would have killed me on the spot without a second's hesitation. I have my answer, then. Hero, champion, monster, whatever else you may be… you are a good person." </p>
+    <p>Terror stares at her. “...You want to know something? I helped Fury toss my old mentor off a cliff this past winter. Helped her nearly burn <i>my ex</i> to death, at the same time. In fact, let’s go back a few years -- you want to know why Muspelham caught fire? Why I’ve got a city on a volcano full of trauma-filled burning Cal-blessed <i>jerks?</i> Seven hells, lady, <i>I burnt it.</i> Saving a <i>million</i> lives won’t turn that around.”</p>
+    <p>She stands up, grabs the boulder Bryti had scrawled Forað onto, and tries to huck it off the cliff, over the spot least likely to hit anything occupied. </p>
+    <p>The boulder slips from her grip, and crashes directly onto her foot.</p>
+    <p>“Calsblessedtitsow!” Terror yells, her shout rapidly descending into the shrieks of twisting metal.</p>
+    <p>Bryti casually reaches over and touches her with a healing spell, easing the pain in her foot. She sits back, giving Terror some space.</p>
+    <p>"I know what happened to Muspelham," Bryti says quietly, "Dreyrugr came to my family asking for military aid a few weeks before it happened. He told my mother he required our forces' assistance in eliminating a rebel threat hiding in the city. She refused, of course. She knew it would have turned into a bloodbath, just… that was extreme, even for <i>him.</i>"</p>
+    <p>"You were in that rebel cell?" She asks, "I… I'm sorry. I should have… I don't know. I should have done… something." </p>
+    <p>“There was <i>nothing</i> you could,” Terror says, glaring at the rip on her sandals. Her tone softens. “You’re not the one who pissed him off; I am. And don’t get insulted by this, but he would have wrecked you. It took way more than just me to end him, and even then it was close. Fighting him alone would have been a death sentence -- and trust me, I know, because it <i>was</i> mine once.”</p>
+    <p>"He tried to kill me when we met," Bryti speaks quietly, "Tossed me around like a pup. He only stopped because my family intervened. I <i>did</i> piss him off, and he eventually came back and visited his hate on my family."</p>
+    <p>Bryti shudders and lowers her head, "You were right to fight us. You had no choice, not if you wanted a real future, no matter what beasts like him thought of it. Don't blame yourself for the actions of a <i>true</i> monster." </p>
+    <p>Terror pauses. “Likewise, then,” she says eventually. Bryti doesn't answer that immediately.</p>
+    <p>"I take it you disagree with me," she looks at the dropped boulder still bearing the rune. </p>
+    <p>“I don’t think violence and pride are enough to exclude you from society,” Terror says. “I really can’t, without being even more of a hypocrite than I already am. So… no. You want an answer? Do I think Lycans can make it in a civilized world? Yes -- because they’re no worse than the rest of us, so far as I can tell.</p>
+    <p>“Cal’s grace, you want to know something that makes this weirder? That mentor I mentioned? <i>Turned into a bear.</i> Helped raise me, insisted throughout my whole life,” her voice deepens to match a male baritone, “ ‘Lycans are heretical animals, set on us by demons, and turning into animals at all is unnatural,’ And he turns into a bear. Insists this was an ancient elven ritual or something. ...Okay, it’s far less relevant than I thought it was, but at the time it seemed really thrice-damned hypocritical.”</p>
+    <p>"Perhaps tossing him off a cliff was not quite so monstrous, then," Bryti comments in a slightly more relaxed tone, "What is the saying? He sounds like 'a piece of work.'" </p>
+    <p>Terror nods. “ ‘Sir’ Phoeberon wasn’t exactly… consistent,” she says. “Plus, he’d tried to drown Fury a bit before that. I don’t know if he’d changed since I’d last seen him… or if he was already like that, and I just never knew.” </p>
+    <p>"Regardless," Bryti nods sagely, "As they say in elven, 'Fuckem.'" </p>
+    <p>“ ‘Fuckem.’ “ Terror says. She glances at the symbol of Calaestros, still in Bryti’s hand. “...I’ll let you get back to communing,” she says. “I think I’ve worked my sulk out, at this point, and Cal and I don’t talk these days. Keep your chin up, Tourist; we’re not letting you leave just yet.” </p>
+    <p>"One thing, before you go…" Bryti looks around for a moment, then stands up. As she stands, she shifts to her hybrid form. She stares at Terror for a few seconds before giving her a half smile.</p>
+    <p>"Help me with this," she tilts her head to the boulder. </p>
+    <p>Terror stares for a second, and then nods.</p>
+    <hr/>
+    <p>The next day, Shadimon receives a baffling report. According to several Skywatchers on night duty, a modestly large boulder was witnessed hurtling across the canyon and smashing to bits on the ground a few hundred feet away from the canyon wall. The Skywatchers were unable to determine where, exactly, it came from.</p>
+    <p>Shadimon sighs and adds the report to his "miscellaneous bullshit" pile.</p>
+    
 <p class="center">-FIN-</p>
 
 <?php
