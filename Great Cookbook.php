e4cf53ab8c04ff1f16676545b81047c71c32f607
XombieHamster <58495678+xombiehamster@users.noreply.github.com>
2020-02-07 10:16:18 -0600

    Create Great Cookbook.php

---
+++ Great Cookbook.php
@@ -0,0 +1,66 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html>
+<head>
+<?php include '../header.php'; ?>
+    <link rel="stylesheet" type="text/css" href="../style.css">
+    <title>
+        Great Elfventure Great Cookbook
+    </title>
+</head>
+<body>
+<H1> Great Elfventure Great Cookbook </H1>
+<h2>Blade Elf Efficient Potato Discs</h2>
+<p>Blade elves are not terribly well known for their cooking poweress.  They are, however, quite proud of this recipe.</p>
+<p>Ingredients:</p>
+<ul>
+  <li>Several potatoes</li>
+  <li>1/3 stick butter</li>
+  <li>1 tbsp thyme</li>
+  <li>1 tsp rough salt</li>
+  <li>1 quantity pepper</li>
+  <li>1 cupish chicken stock or broth</li>
+  <li>4-5 cloves of garlic</li>
+</ul>
+<p>Peel the potatoes and slice them into discs about a thumbprint wide.  Melt the butter and mix the salt, thyme, and pepper with it.  Toss the potato discs in the butter mixture.  If your potatoes are cold, they may chill the butter on contact.  Just make sure there is seasoned butter on the potato discs.  Preheat your oven to 450, 500, or the highest number between the two you’re comfortable with.</p>
+<p>Place your discs into a metal pan with high sides and roast for 15 minutes.  Turn the discs over carefully and roast for another 15 minutes.  Flip them one more time, and pour the stock carefully into the pan.  The stock will bubble a lot right away, so beware the splatter.  Crush garlic cloves directly into pan around potato discs.  Roast another 15 minutes.</p>
+<p>Remove from pan and eat warm.</p>
+<h2>Lycan Drunken Honey Chicken</h2>
+<p>Measurements are not even remotely exact, but Lycans don’t have standard measuring cup sizes or mass production now do they?</p>
+<p>Ingredients:</p>
+<ul>
+  <li>Boneless skinless chicken of some variety</li>
+  <li>Mead</li>
+  <li>Apple cider vinegar</li>
+  <li>A few whole cloves</li>
+  <li>At least one whole bay leaf</li>
+  <li>A good handful of flour</li>
+  <li>1 Egg</li>
+  <li>A pinch ground nutmeg</li>
+  <li>Some pepper</li>
+  <li>A little salt</li>
+  <li>A generous little pile of paprika</li>
+  <li>Some oil</li>
+</ul>
+<p>Place chicken in a container with a lid, cover with a roughly 1:1 mixture of mead and apple cider vinegar.  Include a few cloves and a minimum of one bay leaf.  Allow to marinate for 4-24 hours.</p>
+<p>Cut the chicken into small chunks, about half the length of a finger.</p>
+<p>Mix flour, nutmeg, salt, pepper and paprika in a small dish.  Beat the egg and a small amount of water in a separate bowl.  Dip each piece of chicken first in the flour mixture, then the egg, then the flour mixture again.  Place on a clean plate and let rest.</p>
+<p>Heat about a thumbnail’s depth of oil in a high-sided skillet until shimmery.  Add chicken chunks to oil and cook, turning infrequently, about 10 minutes or until all sides are golden.</p>
+<p>Serve with honey to dip.</p>
+<h2>Lycan Dipping Honey</h2>
+<p>This may be made with vinegar that has had hot peppers soaking in it for several months.  Or just add a pinch of cayenne.</p>
+<p>Ingredients:</p>
+<ul>
+  <li>1/2 cup honey</li>
+  <li>Several spoonfuls apple cider vinegar</li>
+  <li>A pinch of nutmeg, black pepper, allspice</li>
+  <li>A good bit of chili powder</li>
+  <li>A tiny bit of salt</li>
+</ul>
+<p>Mix ingredients in dish.  Dip whatever is desired.  Apples are great.  Or above chicken recipe.  Or bread.  Whatever you want really, it’s all good.</p>
+
+<?php 
+    include "../footer.php";
+    echo getarrows(null,null);
+?>
+</body>
