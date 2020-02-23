cce5c0e48be440c5722dfadebda49451d040a8cb
xombiehamster <58495678+xombiehamster@users.noreply.github.com>
2020-02-22 19:57:32 -0600

    river year 3 summer

--- River/Year/03_1_River.php
+++ River/Year/03_1_River.php
@@ -32,25 +32,59 @@
             <td>Goldfall River</td>
         </tr>
     </table>
-    <p>placeholder</p>
+    <p>Smilisca grabs the feather from where it was hidden in his kayak and sends a bleary message consisting of only one word, Success, to whomever may hear it.  They have won the day, but at what costs, they will not fully know until the moon is full.  And then he tumbles into the little boat and falls right to sleep.</p>
     <h3 id="back">Roleplay Event 3.1.1: <a href="../Bonus/7_A Course Is Changed.php">A Course Is Changed</a></h3>
-    <p>placeholder</p>
+    <p>Left unattended with his Master’s notes on the River Augury ritual, Acris secretly spends his time learning a new cantrip by sheer brute force.  He learns Read Magic.  It was the only spell he could figure out without causing any noticeable damage.</p>
+    <img src="../Images/03_1_Summer Acris_Wizarding.gif" class="center">
     <h3 id="back2">Roleplay Event 3.1.2: <a href="../Bonus/8_A Moonlit Flight.php">A Moonlit Flight</a></h3>
     <h3 id="back3">Roleplay Event 3.1.3: <a href="../Bonus/9_A Conversation with Heaven.php">A Conversation with Heaven</a></h3>
-    <p><b>Economic:</b> placeholder</p>
-    <p><b>Political:</b> placeholder</p>
-    <p><b>Military: </b>placeholder</p>
-    <p><b>Mental:</b>placeholder</p>
+    <p>Partager waits for its garrison to return before moving out.  They don’t have far to travel this season, so it does not damage the schedule overmuch to delay.  Eina’s fingernails may never recover from having been bitten down so much waiting for the results of the military action.</p>
+    <p><b>Economic:</b> Having failed to put together a most excellent aid package, Staurois returns from the potluck to instead help deliver the lumber they prepared.  In the process, the river elves are stricken by the plight of the refugees at Hilt’Inn, and offer to also share the food they have.  This uses the <i>Merchants</i> trapping of <b>Trade</b>.</p>
+    <p>We’re pretty good at this.  Rolling 2 Ability 1 Proficiency dice: <kbd>4 advantage, 2 successes, 0 triumphs</kbd>.</p>
+    <p><b>Political:</b> Spies, spies, everywhere the spies.  Cloggin’ up the scenery wastin’ my time.  Sayin’ do this don’t do that, can’t you see there’s spi-ies?  Paranoia about all these wolf elves and coyote elves and who knows what the sea elves are up to down there in the icky salt water drives the river elves to place their yearly skill point into the Spycraft skill.  The Eyes in the Dark use the <i>Cloak and Dagger</i> trapping of <b>Spycraft</b> to be on the lookout for any more strangers.</p>
+	<p>We’re learning to be better at this.  Rolling 2 Ability dice: <kbd>1 advantage, 1 successes, 0 triumphs</kbd>.</p>
+    <p><b>Military: </b>The Angry Alligators limp back to Partager, valiantly successful, but wounded nonetheless.  They’ll attempt a Heal check to recovery from their casualties in the field, and then seek the shelter of their garrison.</p>
+	<p>The river elf scouts will take the opportunity granted by having the military stationary for a few weeks to thoroughly scout the area around the battlefield.  They can catch back up with the flotilla when they’re done using the <i>Pathfinders</i> trapping of <b>Reconnaissance</b>.</p>
+    <p>This rolls 2 Ability: <kbd>0 advantage, 1 successes, 0 triumphs</kbd>.</p>
+    <p><b>Mental:</b> With their limitations in mind, Eina discusses the results of the previous season’s research with Smilisca when he arrives.  Together, they come to the conclusion that creating a ritual they’re not actually able to cast would be about as useful as that sounds.  Maybe in the future, when we’ve learned more about performing magic on this massive scale we can improve the ritual.  For now, though, it is best to work on developing the most simple version of the spell.  This uses the <i>Magic</i> trapping of <b>Spirituality</b> to try and dodge the interference of the water by using its connection to their Patron Deity, Calestros.</p>
+    <p>This is still rolling one shiny Proficiency die: <kbd>1 advantage, 1 successes, 0 triumphs</kbd>.</p>
+    <p>Partager arrives at Hilt’Inn, flanked by its garrison of walking wounded soldiers.  They’re a little less enthusiastic than usual, but still happy to have successfully arrived at the destination with wares to trade.</p>
     <h3 id="back4">Roleplay Event 3.1.4: <a href="../../Blade/Bonus/2_A Plan Comes Together.php">A Plan Comes Together</a></h3>
     <h3 id="back5">Play By Post 3.1.5: <a href="../../Blade/Bonus/3_Operation Birdcage.php">Operation Birdcage</a></h3>
     <h2>Results</h2>
-    <p><b>Economic:</b> placeholder</p>
-    <p><b>Political:</b> placeholder</p>
-    <p><b>Military:</b> placeholder</p>
-    <p><b>Mental:</b> placeholder</p>
+    <p><b>Economic:</b> Shyrendora expresses her deep regrets that they did not get the silver to you in time for the battle. Indeed, there was a shipment of silver ore prepared for trade with you sitting here before the battle but they chose not to tell you about it. She explains that with all the spies and unknown factions acting in the background, the dark elves were not comfortable in revealing the existence of such a critical asset. They had not secured it at the time, and are still working to do so. She hopes that the silver vein can be a major asset to all allied elves in time, but for now wants to make sure it stays safe. By the time they knew about the horde, it was too late to do anything.</p>
+    <p>While the logic makes sense, it still hurts that it’s one more thing the river elves weren’t told. </p>
+    <p>Despite all this, they’re still cousins and you’re still traders. The trade goes off without a hitch, and you’re even able to gather an amount of food along the river before you arrive. It’s less than you were hoping, but the dark elves report that their food shortage problems have been greatly lessened.</p>
+    <p>Either way, you have a Silver Shipment Aspect. It can be invoked to craft silver weapons for a division.</p>
+    <p><b>Military:</b> The actual Pathfinding part is pretty easy. This square has of course the River Aspect. Interestingly, it is also Fertile Land so that's nice, but the Burned Battlefield Aspect you left behind might put a damper on the property values.</p>
+    <p>With the basic Pathfinding out of the way, your Pathfinders decide to give their favorite werewolf a hand. Many of them were either at or around the battle and are used to her presence. She's looking for stragglers, and sure enough there are a few in the area. Not more than a handful, but even one can cause an outbreak. The Pathfinders decide to help their Wolf scout the area for more afflicted, increasing her range.</p>
+    <p>That's when things get interesting.</p>
+    <p><b>Political:</b> Pathfinder groups have started keeping an Eye around as a trained observer, and it is from one of them that you get a quite interesting report.</p>
+    <p>Your Pathfinders had tracked an Afflicted to the edge of the Goldfall. Just when they were sure that they had pinned down its location, one of the trackers spots some very unusual wolf tracks. These are the same kind of tracks they saw near Surt. Concerned, the Pathfinders carefully examine the area.</p>
+    <p>They spot riders waiting in ambush. Cloaked figures that appear to be elves are waiting in concealment around this Afflicted's campsite. They are riding what appear to be massive wolves. Outnumbered and outgunned, the Pathfinders have to pull back before they are detected.</p>
+    <p>It takes some significant arguing, but your Eye is able to convince Bryti to leave the area and keep herself hidden from those riders. Your Eye believes they were intending to kill or capture her. She loathes to leave the Afflicted there, but the Eye convinces her that her life is too valuable to risk that way. She promises to keep her trail covered.</p>
+    <p>However they were tracking her, the trail is broken. The riders are not able to keep following her and she is able to slip away undetected. Your wolf is safe, for now.</p>
+    <p>Your Pathfinders wait a few days for the mystery riders to move on. Once they are gone, a quick ambush from the river handily dispatches the remaining afflicted.</p>
+    <p>Your Eyes also keep a close watch over the flotilla and on your leaders at the Hilt'Inn. Fortunately, things seem to go pretty quietly there.</p>
+    <img src="../Images/03_1b_Summer_WolfRider.png" class="center">
+    <p><b>Mental:</b> Eina decided to take her time in getting this spell right, which turned out to be an issue when she was unexpectedly tapped for Operation Birdcage. Her staff attempted to keep the research going while she was busy, but without her guidance they didn't get too far.</p>
+    <p>Still, the team is not discouraged. This is going to be an extremely important ritual, and they are fine with taking their time getting it right. Progress has been made, a few kinks have been worked out, and the ritual does seem attainable. It's just going to take more time.</p>
     <h3 id="back6">Story 3.1.6: <a href="../../Blade/Bonus/4_A Broken Promise.php">A Broken Promise</a></h3>
-    <p><b>News Arrives:</b> placeholder</p>
-    <p><b>Additionally:</b> placeholder</p>
+    <p><b>News Arrives:</b> Coyote Refugees</p>
+    <p>Breaker arrives at Riverhaven, battered and weary. She bears numerous injuries, including several severe burns. She's not the only one. Her Cazadores are in rough shape, and they're down many of their numbers.</p>
+    <p>"We had to fight our way out," she explains, "The hijueputa knew. I don't know how, but he fucking knew I was going to betray him. The bastard turned the horse into a banquet 'in my honor' for ‘valiantly defeating the trap and bringing back the loot.’ The son of a bitch was using it as a setup to kill me and the ones most loyal to me. He had to have figured out I was going to backstab him and decided to do me in first."</p>
+    <p>"Well whatever he figured out, the bastard didn't know about the bomb. We set up the thing right in the middle of the training grounds inside the fort. He was planning on surrounding us and cutting us down when we got drunk. Almost did it."</p>
+    <p>"I had to set the fucking thing off myself. Only my people were expecting it. I got burned bad, and so did a lot of my people, but it fucking worked. Coyote's people didn't know how to react. We had to fight our way out of the fort with hell at our backs, but we fucking did it. They were so distracted by the bomb we even broke open the stables and stole as many horses as we could."</p>
+    <p>"The whole fort looked like a torch going up. It was loud as hell, too. Lit up half the town like it was daylight. I don't know what that thing was packed with, but without that we never would have made it. The guards were distracted enough that we were able to get our families out safe. We had to charge the gates to make an exit, but we made it. We ran, and we could see the damned thing burning all the way to the horizon. It must have kept Coyote busy, because no one came after us."</p>
+    <p>"We're torn up pretty bad. We wouldn't have made it much further, civilians or no. We've been through worse, but not by a lot. I'll stand by my agreement that the Cazadores will stay out of Riverhaven, but they could sure use a chance to recover…"</p>
+    <p>Breaker has arrived at Riverhaven with 2 units of Cazadores. Unfortunately they took losses fleeing Coldwater. One unit has Light Casualties, one has Heavy Casualties. Both have Low Morale, though that will likely recover on its own so long as their families are safe.</p>
+    <p>Speaking of, Riverhaven now has the Coyote Refugees Aspect. They fill in the Extra Housing, making that Aspect unusable for the duration of their stay. Thankfully, Coyotes are resilient and resourceful people. They brought a great deal of their own supplies and apply their skills to supporting their presence in Riverhaven, meaning their presence does not inflict any kind of Stress. The dreamdust elves ask them to dress in dreamdust clothing, making it less immediately apparent that Riverhaven took in the refugees. Successful Spying will still find them.</p>
+    <p>In addition, they brought a Coyote Horses Aspect with them. These are their youngest horses that were being bred for Cavalry. They are light war horses with the Racer archetype. Invoking them to create a Cavalry unit will provide an extra proficiency die. This Aspect can be given to any faction, though it is currently up to the dreamdust elves who gets it.</p>
+    <p>In return for the economic losses the fire elves suffered, Breaker reports that the bomb caused the Crippled Barracks consequence on the Fort. The garrison at Coldwater is unusable next season, and his forces will not be able to recover from their own casualties or recruit any new units.</p>
+    <p><b>Additionally:</b> The dark elves have done something quite helpful! They have created a program of giving maps, travel paths, survival equipment, and vital supplies to refugees leaving the Hilt'Inn for their new homes.</p>
+    <p>This means that everyone who has refugees coming through there now has the <i>Well Equipped Refugees</i> Aspect which can be invoked to grow settlements or create new ones, as your refugees are arriving in larger numbers and are far more able to support themselves</p>
+    <p>This doesn't really have an expiration date and will last as long as they are able to keep up the program and refugees are still coming in</p>
+    <p>Does not cost a fate point to invoke the first time.</p>
     
     <?php
         include '../../settarget.php'; 
